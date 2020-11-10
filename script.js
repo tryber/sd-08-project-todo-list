64c871b801d049bdc940bb37c3db545b117b668d
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-10 16:10:34 -0400

    Codeclimate

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = function () {
   // Add conteudo na lista
   const texto = document.getElementById('texto-tarefa');
-  let adicionaTarefa = document.getElementById('criar-tarefa');
-  let lista = document.getElementById('lista-tarefas');
+  const adicionaTarefa = document.getElementById('criar-tarefa');
+  const lista = document.getElementById('lista-tarefas');
 
   adicionaTarefa.addEventListener('click', function () {
-    let tarefa = document.createElement('li');
+    const tarefa = document.createElement('li');
     tarefa.innerText = texto.value;
     lista.appendChild(tarefa);
     texto.value = '';
   });
-};
+}
