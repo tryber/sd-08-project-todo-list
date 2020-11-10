4c6f3d125636e0dad3a960ac5bf6a53cc3bb3033
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-10 14:48:28 -0300

    Requisitos (1 a 5)_2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Definindo variáveis básicas
 let buttonsContainer = document.querySelector('.buttons-container');
-let list = document.getElementById('lista-tarefa');
+let list = document.getElementById('lista-tarefas');
 let textByUser = document.getElementById('texto-tarefa');
 
 // Criando função para facilitar criação de botões
-function createButton(buttonText, typeOfButton) {
+function createButton(buttonText, typeOfButton, idOfButton ) {
   let button = document.createElement('button');
   button.setAttribute('type','button');
   button.innerHTML = buttonText;
   button.className = 'button';
+  button.id = idOfButton
   button.classList.add(typeOfButton);
   buttonsContainer.appendChild(button);
 }
@@ -20,10 +21,10 @@ textByUser.addEventListener('keyup',function () {
 })*/
 
 //Requisito 5 -Criando botão de input e adicionando evento de input
-createButton('input', 'input');
+createButton('input', 'input', 'criar-tarefa');
 let inputbutton = document.querySelector('.input')
 inputbutton.addEventListener('click', function () {
-  let list = document.getElementById('lista-tarefa');
+  let list = document.getElementById('lista-tarefas');
   let textByUser = document.getElementById('texto-tarefa');
   let item = document.createElement('li');
   item.className = 'item';
