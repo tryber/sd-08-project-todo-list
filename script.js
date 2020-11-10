dec040039cf238c45e82ef8e7917f71e9f972f11
Ruben Albertassi <albertassi88@gmail.com>
2020-11-10 16:54:04 -0300

    Inclusao da tag p com o titulo

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let body = document.querySelector("body");
 let header = document.createElement("header");
 header.innerHTML = "Minha Lista de Tarefas";
 body.append(header);
+
+let p = document.createElement("p");
+p.id = "funcionamento";
+p.innerHTML = "Clique duas vezes em um item para marcá-lo como completo";
+header.appendChild(p);
