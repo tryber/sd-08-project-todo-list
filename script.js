8750030041c4646c58108fdc7632d869e9c18159
Thiago Ferreira <thiagocristhian@gmail.com>
2020-11-10 16:37:02 -0300

    Requisito 5

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let selButtom = document.querySelector('#criar-tarefa');
+let selInput = document.querySelector('#texto-tarefa');
+let selLista = document.querySelector('#lista-tarefas');
+
+selButtom.addEventListener('click', function() {  
+  let textoInput = selInput.value;
+  let adiciona = document.createElement('li');
+  adiciona.innerText = textoInput;
+  selLista.appendChild(adiciona);
+  selInput.value = ''
+});
