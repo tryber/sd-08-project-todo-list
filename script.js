df1d10ca8267fbc16dea8638d6a2228cabb3d34e
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2020-11-10 18:49:38 -0300

    requisito 6 - lista por ordem de insercao, requisito 7 - cor de fundo da tarefa, requisito 8 - uma cor de tarefa pr vez

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function addTask() {
   const buttonAddTask = document.querySelector('#criar-tarefa');
-  buttonAddTask.addEventListener('click', function (task) {
+  buttonAddTask.addEventListener('click', function () {
     const taskElement = document.createElement('li');
+    taskElement.className = 'tarefa'
+    taskElement.style.backgroundColor = 'white';
     const inputTask = document.querySelector('#texto-tarefa');
     taskElement.innerHTML = inputTask.value;
     const listTask = document.querySelector('#lista-tarefas');  
@@ -15,4 +17,26 @@ addTask();
 function clearInput() {
   const inputTask = document.querySelector('#texto-tarefa');
   inputTask.value = '';  
+}
+
+function taskColor() {
+  const listTask = document.querySelector('#lista-tarefas');
+  listTask.addEventListener('click', function (task) {
+    if (task.target.style.backgroundColor === 'rgb(128, 128, 128)') {
+    task.target.style.backgroundColor = 'white';
+    } else {
+      clearColorTask();
+      task.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  });
+}
+
+taskColor();
+
+function clearColorTask() {
+  const listTask = document.querySelectorAll('.tarefa');
+  console.log(listTask)
+  for (let tag of listTask) {
+    tag.style.backgroundColor = 'white';
+  }
 }
