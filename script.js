cc76adf5ba56af1707b62bdfe2f9005f0a792246
Igor Nascimento <igorln96@gmail.com>
2020-11-10 17:44:46 -0300

    req9: inserindo dois cliques para tarefas completas

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 // Botão para add tarefa
 let buttonCriar = document.getElementById("criar-tarefa");
 let lista = document.getElementById("lista-tarefas");
-buttonCriar.addEventListener("click", function(){
-    let text = document.getElementById("texto-tarefa");
-    let itemLista = document.createElement("li");
-    itemLista.innerHTML = text.value;
-    lista.appendChild(itemLista);
-    text.value = ""    
-})
+buttonCriar.addEventListener("click", function () {
+  let text = document.getElementById("texto-tarefa");
+  let itemLista = document.createElement("li");
+  itemLista.innerHTML = text.value;
+  lista.appendChild(itemLista);
+  text.value = "";
+});
 
+// Selecionar item na lista
 let listItem = document.getElementsByTagName("li");
-lista.addEventListener("click", function(){
-for (let i = 0; i < listItem.length; i += 1) {
-listItem[i].addEventListener("click", function(event){
-    for (let i = 0; i < listItem.length; i += 1) {
-        listItem[i].className = "";
-    }
-    event.target.className = "selected";
-})
-}
-})
+lista.addEventListener("click", function () {
+  for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].addEventListener("click", function (event) {
+      for (let i = 0; i < listItem.length; i += 1) {
+          if (listItem[i].className === "selected completed") {
+              listItem[i].className = "completed"
+          } else if (listItem[i].className === "selected") {
+              listItem[i].className = "";
+          }
+      } 
+      event.target.className = "selected";
+    });
+  }
+});
 
-// lista.addEventListener("click", function(event){
-//     let listItem = document.getElementsByTagName("li");
-//     for (let i = 0; i < listItem.length; i += 1) {
-//         listItem[i].className = "";
-//     }
-//     event.target.className = "selected"
-// })
+// Indicar item completo
+      lista.addEventListener("dblclick", function (event) {
+          if (event.target.className === "selected") {
+              event.target.className = "selected completed"
+          } else {
+              event.target.className = "selected";
+          }
+      });
