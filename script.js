6bae2318664d8f477c1018cb74e3654e04c6d45e
enicio <jessandro42@gmail.com>
2020-11-10 17:10:10 -0300

    Setimo requisito

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-
-
-
 function newTask () {
   let newTask = document.createElement('Li')
   newTask.innerText = document.querySelector('#texto-tarefa').value
   document.querySelector('#lista-tarefas').appendChild(newTask)
   document.querySelector('#texto-tarefa').value = ""
-
 }
-
 
 let buttonPushed = document.querySelector('#criar-tarefa')
 buttonPushed.addEventListener("click", newTask)
+
+let getTasks = document.getElementById('lista-tarefas')
+getTasks.addEventListener('click', changeBackground)
+
+function changeBackground (e) {
+  console.log(e.target)
+  e.target.style.backgroundColor = "rgb(128, 128, 128)"
+}
 
