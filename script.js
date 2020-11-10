d4cf6d1c1d0e4bfd343f2d0b754234527208613e
Felipe Franca <felipefrancafj@gmail.com>
2020-11-10 17:24:16 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ document.getElementById("criar-tarefa").addEventListener("click", () => {
   let textoDaTarefa = document.getElementById("texto-tarefa"),
     listaOrdenada = document.getElementById("lista-tarefas"),
     itemDaLista = document.createElement("li");
-    itemDaLista.innerHTML = textoDaTarefa.value;
-    listaOrdenada.appendChild(itemDaLista)
-    textoDaTarefa.value = ''
+  itemDaLista.innerHTML = textoDaTarefa.value;
+  listaOrdenada.appendChild(itemDaLista);
+  textoDaTarefa.value = "";
 });
 
+document.getElementById("lista-tarefas").addEventListener("click", () => {
+  let object = document.querySelectorAll("li");
+  for (let index = 0; index < object.length; index += 1) {
+    object[index].style.backgroundColor = "white";
+  }
+  event.target.style.backgroundColor = "rgb(128,128,128)";
+});
