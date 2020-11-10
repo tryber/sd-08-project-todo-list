f22c27ce74d47316d091263328ae772970d85bc2
momentts <marcosd3wow@gmail.com>
2020-11-10 19:42:15 -0300

    requisitos 1 ao 4

---
+++ script.js
@@ -0,0 +1,15 @@
+var botado = false;
+
+function botar() {
+    p = document.getElementById(‘lista’);
+    var li = document.createElement(ol);
+    ol.innerHTML = ‘tres’;
+    ol.setAttribute(‘id’, ‘meuId’);
+    p.appendChild(ol);
+}
+
+function tirar() {
+    i = document.getElementById(‘meuId’);
+    alert(‘Conteudo: ‘+i.innerHTML);
+    document.getElementById(‘lista’).removeChild(i);
+    }
