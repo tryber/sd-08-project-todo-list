77f252279988ac02602c89c3c3332548210e0cc2
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-10 15:00:57 -0300

    adicionando requisito 8

--- script.js
+++ script.js
@@ -3,31 +3,37 @@ function limpaCampoInput() {
   inputCampo.value = '';
 }
 
-
-function trocaCorElementoLista(elemento){
-    console.log(elemento.target)
-    elemento.target.classList.add('colore-elemento-lista');
+function removerCorElementoLista() {
+  const lista = document.querySelectorAll('.elemento-lista');
+  for (let index = 0; index < lista.length; index++) {
+    if (lista[index].classList.contains('colore-elemento-lista')) {
+      lista[index].classList.remove('colore-elemento-lista');
+    }
+  }
 }
 
+function trocaCorElementoLista(elemento) {
+  removerCorElementoLista();
+  elemento.target.classList.add('colore-elemento-lista');
+}
 
 function verificaCliqueLista() {
-    const lista = document.querySelectorAll('.elemento-lista');
-    for (let index = 0; index < lista.length; index += 1) {
-        lista[index].addEventListener('click', trocaCorElementoLista)
-    }
+  const lista = document.querySelectorAll('.elemento-lista');
+  for (let index = 0; index < lista.length; index += 1) {
+    lista[index].addEventListener('click', trocaCorElementoLista);
+  }
 }
 
 function adicionaTarefaLista() {
-    const tarefaInput = document.querySelector('#texto-tarefa').value;
-    limpaCampoInput();
-    const listaOrdenada = document.querySelector('#lista-tarefas');
-    const elementoLista = document.createElement('li');
-    elementoLista.className = 'elemento-lista'
-    elementoLista.innerText = tarefaInput;
-    listaOrdenada.appendChild(elementoLista);
-    verificaCliqueLista();
-  }
-
+  const tarefaInput = document.querySelector('#texto-tarefa').value;
+  limpaCampoInput();
+  const listaOrdenada = document.querySelector('#lista-tarefas');
+  const elementoLista = document.createElement('li');
+  elementoLista.className = 'elemento-lista';
+  elementoLista.innerText = tarefaInput;
+  listaOrdenada.appendChild(elementoLista);
+  verificaCliqueLista();
+}
 
 window.onload = function () {
   const botaoAdicionar = document.querySelector('#criar-tarefa');
