14b10adda20de9c1452381e69e6fba3bdda2a281
Tandy <tandyfw@gmail.com>
2020-11-10 15:54:01 -0300

    Requisito 5 concluido

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+  let textBox = document.querySelector('#texto-tarefa');
+  let btntask = document.querySelector('#criar-tarefa');
+  let lista = document.querySelector('#lista-tarefas');
+
+  btntask.addEventListener('click', function(){
+    let li = document.createElement('li');
+    li.innerHTML = textBox.value;
+    lista.appendChild(li);
+    textBox.value = '';
+  });
+}
+addTask();
