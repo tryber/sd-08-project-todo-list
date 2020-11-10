d593f4ed77750783a7204b7f9354ca4f82504d2d
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-10 17:18:15 -0300

    R10 - Adicionado o requisito 10

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let texto_tarefa_cxa = document.querySelector('#texto-tarefa');
 let criar_tarefa_btn = document.querySelector('#criar-tarefa');
 let lista_tarefas_ol = document.querySelector('#lista-tarefas');
+let apaga_tudo_btn = document.querySelector('#apaga-tudo');
 
 criar_tarefa_btn.addEventListener('click', criaTarefa);
+apaga_tudo_btn.addEventListener('click', apagaLista);
 
 function criaTarefa() { // Função que adiciona uma nova tarefa na lista
     let tarefa = document.createElement('li');
@@ -24,4 +26,8 @@ function alteraCor(event) { // Funcão que altera a cor da tarefa da lista
 function concluiTarefa(event) { // Função que adiciona um risco à tarefa da lista, indicando que ela foi concluída
     if (event.target.className != 'completed') { event.target.className = 'completed'; }
     else { event.target.className = null; }
+}
+
+function apagaLista() { // Limpa a lista apagando todas as tarefas
+    lista_tarefas_ol.innerHTML = '';
 }
