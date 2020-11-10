d992389bbae2dfe93132891a6a96989a7199ed1b
Leandro Lachini <leandrolachini@gmail.com>
2020-11-10 20:17:59 -0300

    requisito 2_1

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Parametros de carregamento da pagina
+window.onload = function () {
+           
+   }
+
 
 // link.: https://www.w3schools.com/js/tryit.asp?filename=tryjs_events_onselect
-function myFunction() {
-    document.getElementById("demo").innerHTML = "You selected some text";
-  }
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "You selected some text";
+//   }
+
