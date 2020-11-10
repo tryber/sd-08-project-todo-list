4e0543d45d2bba9b379a66b81ecf77d7323821ca
Leandro Lachini <leandrolachini@gmail.com>
2020-11-10 19:19:01 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// link.: https://www.w3schools.com/js/tryit.asp?filename=tryjs_events_onselect
+function myFunction() {
+    document.getElementById("demo").innerHTML = "You selected some text";
+  }
