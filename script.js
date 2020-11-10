5eda8534b5c509aee567bab161382bc973c81df4
victor Farias <victorfarias.tv@gmail.com>
2020-11-10 17:09:22 -0300

    Requisito 7: Alterando cor de fundo da tarefa clicada

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ function adicionaTarefa() {
   let textoTarefa = document.getElementById('texto-tarefa');
   buttonAdicionar.addEventListener('click', function () {
     let tarefa = document.createElement('li');
+    tarefa.className = 'tarefa';
     tarefa.innerText = textoTarefa.value;
     listaOrdenada.appendChild(tarefa);
     textoTarefa.value = '';
   });
 }
 adicionaTarefa();
+
+function removeColor() {
+  let listaTarefas = document.querySelector('#lista-tarefas');
+  let tarefas = listaTarefas.children;
+  for (let i = 0; i < tarefas.length; i += 1) {
+    tarefas[i].classList.remove('selected');
+  }
+}
+
+function adicionaColor() {
+  let listaTarefas = document.querySelector('#lista-tarefas');
+  listaTarefas.addEventListener('click', function(event) {
+    removeColor();
+    event.target.classList.add('selected');
+  });
+}
+adicionaColor();
