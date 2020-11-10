b43130f3db890fe08991c07db24c076648064c4f
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-10 16:37:45 -0300

    R07 - Adicionado o requisito 07

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ function criaTarefa() { // Função que adiciona uma nova tarefa na lista
     tarefa.innerText = texto_tarefa_cxa.value;
     texto_tarefa_cxa.value = '';
     lista_tarefas_ol.appendChild(tarefa);
+    tarefa.addEventListener('click', alteraCor);
+}
+
+function alteraCor(event) { // Funcão que altera a cor da tarefa da lista
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
