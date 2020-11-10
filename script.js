f8d8951bd07ab72e9fb985dcc9d54c35fc34f745
MuriloBatista <git@github.com:MuriloBatista>
2020-11-10 19:22:21 -0300

    Adiciona 5º requisito

--- script.js
+++ script.js
@@ -10,8 +10,23 @@ textInf.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 //Cria caixa de texto para 'imput' - 3º requisito
 const txtBox = document.createElement('input');
 txtBox.id = 'texto-tarefa';
+txtBox.type = 'text';
 document.querySelector('body').appendChild(txtBox);
 //Cria lista ordenada - 4º requisito
 const taskList = document.createElement('ol');
 taskList.id = 'lista-tarefas';
 document.querySelector('body').appendChild(taskList);
+//Cria Botão e Lista valor do imput - 5º requisito
+const btnCriar = document.createElement('button');
+btnCriar.id = 'criar-tarefa';
+btnCriar.innerText = 'Nova Tarefa';
+document.querySelector('body').appendChild(btnCriar);
+btnCriar.addEventListener('click', makeList);
+
+function makeList(){
+    const conteudo = document.getElementById('texto-tarefa').value;
+    const listaTarefas = document.createElement('li');
+    listaTarefas.innerText = conteudo;
+    document.getElementById('texto-tarefa').value = '';
+    document.querySelector('#lista-tarefas').appendChild(listaTarefas);
+}
