7f3bc08f5940e112655024169e16a791ce503b20
mateuscoury <72469989+mateuscoury@users.noreply.github.com>
2020-11-10 16:24:11 -0300

    corrigido botao de adicionar elementos

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function criabotao(){
-
     let getposi = document.querySelector("#botao")
     let criabot =document.createElement("button")
-    criabot.tagName = "criar-tarefa"
+    criabot.id = "criar-tarefa"
     criabot.innerHTML = "Adicionar"
     getposi.appendChild(criabot)
-
-  let getposi2 = document.querySelector("#texto-tarefa")
+    let getposi2 = document.getElementById("texto-tarefa")
     let getposi1 = document.querySelector("#lista-tarefas")
-    let textoinput = getposi2.innerHTML
-
-criabot.addEventListener("click", function(){
-
     
-    let criaelista = document.createElement("li")
-    criaelista.innerHTML =  textoinput
-    getposi1.appendChild(criaelista)
+
+    criabot.addEventListener("click", function(){
+        let textoinput = getposi2.value
+
+        
+        let criaelista = document.createElement("li")
+        criaelista.innerText =  textoinput
+        getposi1.appendChild(criaelista)
 
 
 
 
-})
+    })
 
 
 
