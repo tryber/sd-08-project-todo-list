417aa3011b9441ced927531bc28b27c2ca3ffa04
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-10 13:36:21 -0300

    Dando uma geral no code climate

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let addTaskButton = document.getElementById('criar-tarefa');
-let taskList = document.getElementById('lista-tarefas')
+const addTaskButton = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 
 addTaskButton.addEventListener('click', function () {
-  let input = document.getElementById('texto-tarefa');
-  let createLi = document.createElement('li');
+  const input = document.getElementById('texto-tarefa');
+  const createLi = document.createElement('li');
   createLi.innerHTML = input.value;
   taskList.appendChild(createLi);
-  input.value = ""
+  input.value = ''
 });
