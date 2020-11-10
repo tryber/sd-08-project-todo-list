cbcaa438b742d660d6ba8452a2482d7353d34253
Samuel <samuelvanguarda@gmail.com>
2020-11-10 18:02:06 -0300

    Requisito 5

---
+++ script.js
@@ -0,0 +1,15 @@
+// Botão Adiciona Tarefa
+function adicionaTarefa(){
+    let input = document.getElementById('texto-tarefa');
+    const button = document.getElementById('criar-tarefa');
+    const ol = document.getElementById('lista-tarefas');
+
+    button.addEventListener('click', function() {
+        const li = document.createElement("li");
+        li.innerHTML = input.value;
+        ol.appendChild(li); 
+        input.value = '';
+        })    
+}
+
+adicionaTarefa();
