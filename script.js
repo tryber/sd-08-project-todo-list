d22087c7631d7ac762f3dcaf0c88d74e5f94e79b
Ruben Albertassi <albertassi88@gmail.com>
2020-11-10 17:14:03 -0300

    Inclusao da funcao addListTodo

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ divInput.appendChild(input);
 
 let olOrdered = document.createElement("ol");
 olOrdered.id = "lista-tarefas";
+
 divInput.appendChild(olOrdered);
 
 let button = document.createElement("button");
@@ -23,5 +24,16 @@ button.id = "criar-tarefa";
 button.innerHTML = "Adicionar";
 divInput.appendChild(button);
 
+function addListTodo(){
 
+    button.addEventListener("click", function(){
 
+        let valueInput = input.value;
+        let liOrdered = document.createElement("li");
+        liOrdered.innerHTML = valueInput;
+        olOrdered.appendChild(liOrdered);
+        input.value = "";
+    });
+
+}
+addListTodo();
