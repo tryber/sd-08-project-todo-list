7f4778da38ba725aed1e114813a41ad1b77ceb30
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-10 17:47:37 -0300

    feat: question 6, 7, 8

--- script.js
+++ script.js
@@ -4,15 +4,28 @@ const todoList = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
 const colorLi = 'rgb(128,128,128)';
 
+function backgroundColor(target, color) {
+  target.style.backgroundColor = color;
+}
+function removeSelectedAll() {
+  document.querySelectorAll('li').forEach((element) => {
+    removeSelected(element);
+  });
+}
+function removeSelected(target) {
+  return target.classList.remove('selected');
+}
+function addedSelected(target) {
+  removeSelectedAll();
+  // backgroundColor(target, colorLi);
+  target.classList.add('selected');
+}
 function createElementHTML(value) {
   for (let index = 0; index < value.length; index += 1) {
     let createdLi = document.createElement('li');
     createdLi.innerText = value[index];
     todoList.appendChild(createdLi);
   }
-}
-function backgroundColor(target, color) {
-  target.style.backgroundColor = color;
 }
 function removeElementsAll() {
   const elementTarget = document.querySelectorAll('li');
@@ -30,8 +43,8 @@ function capturedElementEvents(target, event, callback) {
   const currentElement = document.getElementById(target);
   currentElement.addEventListener(event, (element) => {
     const elementEvent = element.target;
-    callback(elementEvent, colorLi);
+    callback(elementEvent);
   });
 }
 capturedElementEvents('criar-tarefa', 'click', push);
-capturedElementEvents('lista-tarefas', 'click', backgroundColor);
+capturedElementEvents('lista-tarefas', 'click', addedSelected);
