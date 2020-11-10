1370d972c37bbbd84dbbc1c34e9ed317f768429f
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-10 18:15:59 -0300

    Ate Requisito 9

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ function criaTarefa () {
     entradaTarefa.value = "";
 }
 document.getElementById("criar-tarefa").addEventListener("click",criaTarefa);
+
+function selecionaItem (event) {
+    let itens = document.getElementsByTagName("li");
+    for (let i=0; i<itens.length; i+=1){
+        if (itens[i].style.backgroundColor === "rgb(128, 128, 128)"){
+            itens[i].style.backgroundColor = "";
+        }
+    }
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
+}
+listaTarefas.addEventListener("click",selecionaItem);
+
+function tarefaCompleta (event) {
+    if (event.target.classList.item(0) === "completed") {
+        event.target.classList.remove("completed");
+    } else {
+        event.target.classList.add("completed");
+    }
+}
+listaTarefas.addEventListener("dblclick",tarefaCompleta);
