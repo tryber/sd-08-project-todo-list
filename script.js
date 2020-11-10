d0b0c7765ea959877b6a1759f3d0d49c8ab0cf6b
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-11-10 17:56:09 -0300

    Create complete item & erase button

--- script.js
+++ script.js
@@ -14,9 +14,33 @@ createButton.addEventListener("click", function () {
 taskList.addEventListener("click", function (event) {
   let taskItem = document.querySelectorAll("li");
   for (let index = 0; index < taskItem.length; index += 1) {
-    taskItem[index].classList.remove("selected");
     taskItem[index].style.backgroundColor = "rgb(256, 256, 256)";
   }
-  event.target.className = "selected";
   event.target.style.backgroundColor = "rgb(128, 128, 128)";
 });
+
+taskList.addEventListener("dblclick", function (event) {
+    if (event.target.className == "completed"){
+        event.target.style.textDecoration = "none";
+        event.target.classList.remove("completed")
+    } else {
+    event.target.style.textDecoration = "line-through";
+    event.target.className = "completed";
+}
+});
+
+eraseAllButton.addEventListener("click", function () {
+    let listSize = document.querySelectorAll("li").length;
+    for (let index = 0; index < listSize; index += 1) {
+    let lastItem = taskList.lastElementChild;
+    taskList.removeChild(lastItem);
+    }
+})
+
+eraseFinishedButton.addEventListener("click", function () {
+    let listSize = document.querySelectorAll(".completed").length;
+    for (let index = 0; index < listSize; index += 1) {
+        let firstItem = document.querySelector('.completed');
+        taskList.removeChild(firstItem);
+        }
+})
