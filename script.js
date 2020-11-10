fb8fa3d7b935f9465d5fd83ba7eed109c24a3639
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-10 17:48:51 -0300

    CodeClimate requisitos (1-9)_1

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Definindo variáveis básicas
-let buttonsContainer = document.querySelector('.buttons-container');
-let list = document.getElementById('lista-tarefas');
-let textByUser = document.getElementById('texto-tarefa');
+const buttonsContainer = document.querySelector('.buttons-container');
+const list = document.getElementById('lista-tarefas');
 let itemLength = 0;
+let item = document.getElementsByClassName('item');
 //
 // Criando função para facilitar criação de botões
 //
-function createButton(buttonText, typeOfButton, idOfButton ) {
-  let button = document.createElement('button');
-  button.setAttribute('type','button');
+function createButton(buttonText, typeOfButton, idOfButton) {
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
   button.innerHTML = buttonText;
   button.className = 'button';
-  button.id = idOfButton
+  button.id = idOfButton;
   button.classList.add(typeOfButton);
   buttonsContainer.appendChild(button);
 }
@@ -19,51 +19,51 @@ function createButton(buttonText, typeOfButton, idOfButton ) {
 // Requisito 5 e 6 - Criando botão de input e adicionando evento de input
 //
 createButton('input', 'input', 'criar-tarefa');
-let inputbutton = document.querySelector('.input')
+const inputbutton = document.querySelector('.input');
 inputbutton.addEventListener('click', function () {
-  let list = document.getElementById('lista-tarefas');
-  let textByUser = document.getElementById('texto-tarefa');
-  let item = document.createElement('li');
+  const textByUser = document.getElementById('texto-tarefa');
+  const item = document.createElement('li');
   item.className = 'item';
   item.innerHTML = textByUser.value;
   list.appendChild(item);
-  textByUser.value = "";
+  textByUser.value = '';
   itemLength += 1;
-})
-// Requisito 7 - Adicionando evento para mudança de style para o item selecionado
+});
 //
-// Adicionando evento para chamar função com tamanho de lista de items itemLength atualizado
-let item = document.getElementsByClassName('item');
-list.addEventListener('mouseenter',function () {
-  clickOnList();
-  doubleClickOnList()
-})
+// Requisito 7- Adicionando evento para mudança de background
 //
-// Adicionando evento para mudança de background
 function clickOnList() {
   for (let i = 0; i < itemLength; i += 1) {
-    item[i].addEventListener('click', function() {
+    item[i].addEventListener('click', function () {
       item[i].style.backgroundColor = 'rgb(128,128,128)';
-      index = i
-      for(let j = 0; j < itemLength; j += 1){
+      let index = i;
+      for (let j = 0; j < itemLength; j += 1){
         if (j !== index) {
-          item[j].style.backgroundColor ='';
+          item[j].style.backgroundColor = '';
         }
       }
-    })
+    });
   }
-} 
+}
+//
 // Requisitos 9 - Adicionando evento de double click para alterar classe do item selecionado
+//
 function doubleClickOnList() {
   for (let i = 0; i < itemLength; i += 1) {
     item[i].addEventListener('dblclick', function() {
-      let classOfItemDoubleClicked = document.getElementsByClassName('item')[i].className;
-      if (classOfItemDoubleClicked == 'item') {
+      const classOfItemDoubleClicked = document.getElementsByClassName('item')[i].className;
+      if (classOfItemDoubleClicked === 'item') {
         item[i].classList.add('completed');
       }
-      if(classOfItemDoubleClicked == 'item completed'){
+      if (classOfItemDoubleClicked === 'item completed'){
         item[i].className = 'item';
       }
-    })
+    });
   }
-} 
+}
+//
+// Adicionando evento para chamar funções com tamanho de lista de items itemLength atualizado
+list.addEventListener('mouseenter', function () {
+  clickOnList();
+  doubleClickOnList();
+});
