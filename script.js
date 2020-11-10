a9b1b66257b656ed9bc12164790ef1f595dc6c26
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-10 17:52:45 -0300

    add li dinamicamente

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const listaTarefa = document.createElement('ol')
+
 // criar li dinamicamente
-// dinamizar o clique do button desaparecer o input.value e mostrar o input.value como li
+window.onload = function(){  
+  document.body.appendChild(listaTarefa)
+  listaTarefa.id = 'lista-tarefa'  
+}
+
+function addTask(){  
+  let novaTarefa = document.createElement('li')
+  novaTarefa.innerText = document.getElementById('texto-tarefa').value
+  listaTarefa.appendChild(novaTarefa)
+  document.getElementById('texto-tarefa').value = ""
+
+  console.log(listaTarefa)
+}
+
+
+
+
 // dblclick no li faz text-decoration line-through solid rgb (0,0,0)
