3723a0c35d0092a8f302c7b853c92c659df9cde0
Victor Breno <victor.brns2@gmail.com>
2020-11-10 17:53:36 -0300

    requisitos 6,7 e 8

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-//Função para adicionar elemento a lista
+//Função para adicionar elemento a lista e apagar o que está dentro do input.
 function adcLista (){
   let elementoLista = document.createElement('li');
+  elementoLista.className = 'elemento-lista';
   elementoLista.textContent = tarefaParaAdc.value;
   let lista = document.getElementById('lista-tarefas');
   lista.appendChild(elementoLista);
-
   tarefaParaAdc.value = '';
 }
-
-
-
-
 
 //Botão para adicionar elemento a lista.
 let tarefaParaAdc = document.getElementById('texto-tarefa');
 let btnAdc = document.getElementById('criar-tarefa');
 btnAdc.addEventListener('click', adcLista)
+
+
+//Função para alterar o fundo de elemento da lista.
+function altFundo (element) {
+  let listaDeElementos = document.getElementsByClassName('elemento-lista');
+  for (i = 0; i < listaDeElementos.length; i += 1) {
+    if (listaDeElementos[i].classList.contains('elemento-cinza')) {
+      listaDeElementos[i].classList.remove('elemento-cinza');
+      listaDeElementos[i].style.removeProperty('backgroudColor');
+    }
+  }
+  element.className += ' elemento-cinza';
+}
+
+
+//Teste para ver se o elemeno clicado é um li da lista ordenada.
+document.addEventListener('click', function (clique) {
+  if ( clique.target.classList.contains( 'elemento-lista' ) ) {
+      altFundo(clique.target);
+  }
+}, false);
+
+
