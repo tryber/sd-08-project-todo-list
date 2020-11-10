5b1dec000a4e4f4371d0b95aa4c6682936e7064d
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-10 14:45:56 -0300

    adicionando requisito 7

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ function limpaCampoInput() {
   inputCampo.value = '';
 }
 
-function adicionaTarefaLista() {
-  const tarefaInput = document.querySelector('#texto-tarefa').value;
-  limpaCampoInput();
-  const listaOrdenada = document.querySelector('#lista-tarefas');
-  const elementoLista = document.createElement('li');
-  elementoLista.innerText = tarefaInput;
-  listaOrdenada.appendChild(elementoLista);
+
+function trocaCorElementoLista(elemento){
+    console.log(elemento.target)
+    elemento.target.classList.add('colore-elemento-lista');
 }
+
+
+function verificaCliqueLista() {
+    const lista = document.querySelectorAll('.elemento-lista');
+    for (let index = 0; index < lista.length; index += 1) {
+        lista[index].addEventListener('click', trocaCorElementoLista)
+    }
+}
+
+function adicionaTarefaLista() {
+    const tarefaInput = document.querySelector('#texto-tarefa').value;
+    limpaCampoInput();
+    const listaOrdenada = document.querySelector('#lista-tarefas');
+    const elementoLista = document.createElement('li');
+    elementoLista.className = 'elemento-lista'
+    elementoLista.innerText = tarefaInput;
+    listaOrdenada.appendChild(elementoLista);
+    verificaCliqueLista();
+  }
+
 
 window.onload = function () {
   const botaoAdicionar = document.querySelector('#criar-tarefa');
   botaoAdicionar.addEventListener('click', adicionaTarefaLista);
+  verificaCliqueLista();
 };
