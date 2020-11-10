2cd8ad4c50b232f1769204fe086781da415dc091
MuriloBatista <git@github.com:MuriloBatista>
2020-11-10 18:30:37 -0300

    Adiciona 3º requisito

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ const textInf = document.createElement('p');
 document.querySelector('body').appendChild(textInf);
 textInf.id = 'funcionamento';
 textInf.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+//Cria caixa de texto para 'imput' - 3º requisito
+const txtBox = document.createElement('input');
+txtBox.id = 'texto-tarefa';
+document.querySelector('body').appendChild(txtBox);
