15bf04a79436ceaad759ee921a0b0632e2765021
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-10 15:24:38 -0300

    Requisito 5 concluido

---
+++ script.js
@@ -0,0 +1,19 @@
+// Cria uma tarefa
+function createTask(description) {
+  const task = document.createElement('li');
+  task.className = 'task';
+  task.textContent = description;
+  return task;
+}
+
+const olTasks = document.getElementById('lista-tarefas');
+const taskTextInput = document.getElementById('texto-tarefa');
+const createTaskButton = document.getElementById('criar-tarefa');
+
+// Cria uma nova tarefa e a adiciona a ol quando clicamos no botao
+createTaskButton.addEventListener('click', function() {
+  const taskDescription = taskTextInput.value;
+  const task = createTask(taskDescription);
+  olTasks.appendChild(task);
+  taskTextInput.value = "";
+});
