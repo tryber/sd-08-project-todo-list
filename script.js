4c1c19654bc59d531ccd5fbdf9ee027bf7bbd1b4
emassaki <massakihirayama@gmail.com>
2020-11-10 19:37:53 -0300

    Adicionado requisito 13 - botao para subir ou descer um item na lista

--- script.js
+++ script.js
@@ -1,29 +1,73 @@
-// Adicionar tarefa à lista
-const addTaskButton = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-addTaskButton.addEventListener('click', () => {
-  let input = document.getElementById('texto-tarefa');
-  const task = document.createElement('li');
-  task.innerHTML = input.value;
-  task.classList.add('task');
-  input.value = '';
-  taskList.appendChild(task);
-});
+taskList.innerHTML = localStorage.getItem('List')
 
-// Pinta a tarefa clicada
-taskList.addEventListener('click', (event) => {
-  const tasks = document.querySelector('.selected');
-  if (tasks === null) {
-    event.target.classList.add('selected');
-    event.target.style.background = 'rgb(128, 128, 128)';
-  } else {
-    tasks.removeAttribute('style');
-    tasks.classList.remove('selected');
-    event.target.classList.add('selected');
-    event.target.style.background = 'rgb(128, 128, 128)';
+// Eventos click
+document.addEventListener('click', (event) => {
+  // Adicionar tarefa à lista
+  if (event.target.id === 'criar-tarefa') {
+    let input = document.getElementById('texto-tarefa');
+    const task = document.createElement('li');
+    task.innerHTML = input.value;
+    task.classList.add('task');
+    input.value = '';
+    taskList.appendChild(task);
   }
-});
+  // Pinta a tarefa clicada
+  if (event.target.classList.contains('task')) {
+    const tasks = document.querySelector('.selected');
+    if (tasks === null) {
+      event.target.classList.add('selected');
+    } else {
+      tasks.removeAttribute('style');
+      tasks.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  }
+  // Apaga todas as tarefas
+  if (event.target.id === 'apaga-tudo') {
+    taskList.innerHTML = '';
+  }
+  // Apaga tarefas completas
+  if (event.target.id === 'remover-finalizados') {
+    const completedTasks = document.getElementsByClassName('completed');
+    const length = completedTasks.length;
+    for (let index = 0; index < length; index += 1) {
+      completedTasks[0].parentElement.removeChild(completedTasks[0]);
+    }
+  }
+  // Salva a lista de tarefas
+  if (event.target.id === 'salvar-tarefas') {
+    localStorage.setItem('List', taskList.innerHTML);
+  }
+  // Mover para cima o item selecionado
+  if (event.target.id === 'mover-cima') {
+    const childNode = document.querySelectorAll('.task');
+    let selected;
+    for (let index = 0; index < childNode.length; index += 1) {
+      if (childNode[index].classList.contains('selected')) {
+        selected = index;
+      }
+    }
+    if (selected !== 0) {
+      childNode[selected].parentElement.insertBefore(childNode[selected], childNode[selected - 1]);
+      // Solução encontrada no site 'https://stackoverflow.com/questions/9732624/how-to-swap-dom-child-nodes-in-javascript'
+    }
+  }
+  if (event.target.id === 'mover-baixo') {
+    const childNode = document.querySelectorAll('.task');
+    let selected;
+    for (let index = 0; index < childNode.length; index += 1) {
+      if (childNode[index].classList.contains('selected')) {
+        selected = index + 1;
+      }
+    }
+    if (selected < childNode.length) {
+      childNode[selected].parentElement.insertBefore(childNode[selected], childNode[selected - 1]);
+    }
+  }
+})
 
+// Eventos dblclick
 // Torna a tarefa completa
 taskList.addEventListener('dblclick', (event) => {
   if (event.target.classList.contains('completed')) {
@@ -32,26 +76,3 @@ taskList.addEventListener('dblclick', (event) => {
     event.target.classList.add('completed');
   }
 });
-
-// Apaga todas as tarefas
-const removeAll = document.getElementById('apaga-tudo');
-removeAll.addEventListener('click', () => {
-  taskList.innerHTML = '';
-})
-
-// Apaga tarefas completas
-const removeCompleted = document.getElementById('remover-finalizados');
-removeCompleted.addEventListener('click', () => {
-  const completedTasks = document.getElementsByClassName('completed');
-  const length = completedTasks.length;
-  for (let index = 0; index < length; index += 1) {
-    completedTasks[0].parentElement.removeChild(completedTasks[0]);
-  }
-})
-
-// Salva a lista de tarefas
-const saveTasks = document.getElementById('salvar-tarefas');
-saveTasks.addEventListener('click', () => {
-  localStorage.setItem('List', taskList.innerHTML);
-})
-taskList.innerHTML = localStorage.getItem('List')
