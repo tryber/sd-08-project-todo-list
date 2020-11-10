14d256aeba204da4c15c568a47578eb1a5b54b45
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-10 20:46:20 -0300

    Req7: Adiciona cor de fundo ao clicar

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const body = document.querySelector('body');
 
 //Cria o header e o texto que explica
-createHeader();
-createInput();
-createList();
+createBasics();
 
 
-function createHeader() {
+function createBasics() {
+    //Cria o header
     let header = document.createElement('header');
     header.innerHTML = 'Minha Lista de Tarefas';
     body.appendChild(header);
 
+    //Cria o parágrafo
     let instruction = document.createElement('p');
     instruction.id = 'funcionamento';
     instruction.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
     body.appendChild(instruction);
+
+    //Chama a função que cria o input e botão de add
+    createInput();
+
+    //Cria a lista ordenada
+    let list = document.createElement('ol');
+    list.id = 'lista-tarefas';
+    body.appendChild(list);
 }
 
 function createInput() {
@@ -29,23 +37,26 @@ function createInput() {
     button.innerHTML = 'Criar';
 
     //Adiciona ao botão a função de adicionar itens a lista e limpar o input
-    button.addEventListener('click', function(event){
+    button.addEventListener('click', function(){
         let lista = document.querySelector('#lista-tarefas');
         let novaTarefa = document.querySelector('#texto-tarefa').value;
 
+        //Cria a nova linha, adiciona o texto a ela e associa a lista
         let novaLinha = document.createElement('li');
         novaLinha.innerHTML = novaTarefa;
         lista.appendChild(novaLinha);
+        //Adiciona um listener a essa nova linha para que quando for clicado troque a cor de fundo
+        novaLinha.addEventListener('click', function(event){
+            let getAllListItems = document.querySelectorAll('li');
+            for (let i = 0; i < getAllListItems.length; i += 1){
+                getAllListItems[i].style.backgroundColor = 'white';                
+            }
+            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        })
 
+        //Limpa o campo input
         document.querySelector('#texto-tarefa').value = '';
     })
 
     body.appendChild(button);
-}
-
-function createList() {
-    let list = document.createElement('ol');
-    list.id = 'lista-tarefas';
-    body.appendChild(list);
-
 }
