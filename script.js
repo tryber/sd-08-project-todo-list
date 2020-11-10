c1a11b1d21bb14f115f099c81f4e2890653edf9a
Tandy <tandyfw@gmail.com>
2020-11-10 16:20:09 -0300

    Requisito 8 concluido

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Requisito 05 - Deve haver um botão com id="criar-tarefa" e ao clicar nesse botão,
+// um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
+// Requisito 06 - Os itens da lista de tarefas devem ser ordenados por ordem de criação.
+
 function addTask() {
   let textBox = document.querySelector('#texto-tarefa');
   let btntask = document.querySelector('#criar-tarefa');
@@ -12,10 +16,16 @@ function addTask() {
 }
 addTask();
 
+// Requisito 07 - Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
 function addTaskColor() {
   let lista = document.querySelector('#lista-tarefas');
   lista.addEventListener('click', function(event){
     event.target.style.backgroundColor = 'rgb(128,128,128)';
+    for(let i = 0; i < lista.children.length; i += 1) {
+      if(event.target !== lista.children[i]) {
+        lista.children[i].style.backgroundColor = 'white';
+      }
+    }
   });
 }
 addTaskColor();
