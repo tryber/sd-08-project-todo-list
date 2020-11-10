d40d69cb279b02c9469273026413467817463e18
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-10 15:53:23 -0300

    Requisito 01 e 02 e 03 com javaScript

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-// Requisito 01 e Requisito 02
+// Requisito 01 e Requisito 02 Requisito 03
+
 function addHeader() {
     let body = document.getElementsByTagName('body');
     let header = document.createElement('header');
     let h3 = document.createElement('h3');
     let paragraph = document.createElement('p');
+    let boxText = document.createElement('input');
+    boxText.id = 'texto-tarefa';
+    boxText.type = 'text'
     h3.innerText = 'Minha Lista de Tarefas';
-    paragraph.id = 'funcionamento'
     paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+    paragraph.id = 'funcionamento'
     for (let index = 0; index < body.length; index += 1) {
         body[index].appendChild(header).appendChild(h3);
         body[index].appendChild(paragraph)
+        body[index].appendChild(boxText)
     }
 }
 
