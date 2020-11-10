66dc9b03fbbd2a557a3cb446e79032af25732886
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-11-10 17:21:56 -0300

    Requisito 13: Bônus

--- script.js
+++ script.js
@@ -12,35 +12,22 @@ botaoAdd.addEventListener('click', function () {
 // Realizei esse requisito com a ajuda de:
 // https://css-tricks.com/snippets/javascript/bind-different-events-to-click-and-double-click/
 // https://www.w3schools.com/jsref/met_win_settimeout.asp
-// Ainda não entendi o uso do booleano
-// tô quase entendendo, o evento de 2 clicks irá limpar o timer, e tornar prevent verdadeiro...ainda tá complicado
-
-
 tarefas.addEventListener('click', function (event) {
   // condição para um clique
   if (event.detail === 1) {
     // se o elemento não estiver selecionado...
     if (event.target.style.backgroundColor !== 'rgb(128 , 128 , 128)') {
-      
-        // Condição para gerar delay e o evento distinguir entre duplo clique e único clique
-        
-            // Aqui vou colocar um for para tirar o cinza dos outro cinzas, preciso e uma lista
-            listaTarefas = document.querySelectorAll('.tarefasLi')
-            console.log(listaTarefas)
-            for (let i = 0; i < listaTarefas.length; i++ ) {
-              let element = listaTarefas[i];
-              if (element.style.backgroundColor === 'rgb(128, 128, 128)' ) {
-                element.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-              }
-            }
-          event.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
-        
-        prevent = false;
-      
+      let listaTarefas = document.querySelectorAll('.tarefasLi')
+      console.log(listaTarefas)
+      for (let i = 0; i < listaTarefas.length; i++ ) {
+        let element = listaTarefas[i];
+        if (element.style.backgroundColor === 'rgb(128, 128, 128)' ) {
+          element.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        }
+      }
+      event.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
     }
   } else if (event.detail === 2) {
-    
-    
     if (event.target.className !== 'tarefasLi completed') {
       event.target.className = 'tarefasLi completed';
     } else {
@@ -73,3 +60,31 @@ botaoCompletados.addEventListener('click', function (){
 
 //elementoPai.insertBefore(novoElemento, elemento referência)
 //
+
+let botaoUp = document.querySelector("#mover-cima");
+botaoUp.addEventListener('click', function () {
+let listaTarefas = document.querySelectorAll('.tarefasLi');
+let paiTarefas = document.querySelector("#lista-tarefas");
+  for (let i = 0; i < listaTarefas.length; i++ ) {
+    let element = listaTarefas[i];
+    if (element.style.backgroundColor === 'rgb(128, 128, 128)' ) {
+      if (element.previousElementSibling !== null){
+        paiTarefas.insertBefore(element, element.previousElementSibling)
+      }
+    }
+  }
+})
+
+let botaoDown = document.querySelector("#mover-baixo");
+botaoDown.addEventListener('click', function () {
+let listaTarefas = document.querySelectorAll('.tarefasLi');
+let paiTarefas = document.querySelector("#lista-tarefas");
+  for (let i = 0; i < listaTarefas.length; i++ ) {
+    let element = listaTarefas[i];
+    if (element.style.backgroundColor === 'rgb(128, 128, 128)' ) {
+      if (element.nextElementSibling !== null){
+        paiTarefas.insertBefore(element.nextElementSibling, element)
+      }
+    }
+  }
+})
