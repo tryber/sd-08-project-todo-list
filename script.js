fcb8576f89d8096576edf7f23cbe203dde06181b
Bruno <brunocmoraes97@gmail.com>
2020-11-10 18:17:09 -0300

    Requisito 5. Adiciona listas com o conteúdo que tiver no input.

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('criar-tarefa');
+let ol = document.getElementById('lista-tarefas');
+let input = document.getElementById('texto-tarefa');
+
+function createList(){
+    button.addEventListener('click', function () {
+        let li = document.createElement('li')
+        li.innerText += input.value;
+        ol.appendChild(li);
+        input.value = "";
+    })
+}
+createList();
