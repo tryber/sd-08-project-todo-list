4978a5faa765bff5c98112ade860c6818503b0c8
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-10 16:24:24 -0300

    R05 - Adicionado o requisito 05

---
+++ script.js
@@ -0,0 +1,12 @@
+let texto_tarefa_cxa = document.querySelector('#texto-tarefa');
+let criar_tarefa_btn = document.querySelector('#criar-tarefa');
+let lista_tarefas_ol = document.querySelector('#lista-tarefas');
+
+criar_tarefa_btn.addEventListener('click', criaTarefa);
+
+function criaTarefa() { // Função que adiciona uma nova tarefa na lista
+    let tarefa = document.createElement('li');
+    tarefa.innerText = texto_tarefa_cxa.value;
+    texto_tarefa_cxa.value = '';
+    lista_tarefas_ol.appendChild(tarefa);
+}
