479e646be90bde98377461276b7b0c9b517fe5a8
emassaki <massakihirayama@gmail.com>
2020-11-10 17:15:10 -0300

    Adicionado requisito 9 - risca as tarefas completas ao clicar duas vezes

--- script.js
+++ script.js
@@ -6,20 +6,29 @@ addTaskButton.addEventListener('click', () => {
   const task = document.createElement('li');
   task.innerHTML = input.value;
   task.classList.add('task');
-  input.value = "";
+  input.value = '';
   taskList.appendChild(task);
-})
+});
 
 // Pinta a tarefa clicada
 taskList.addEventListener('click', (event) => {
   const tasks = document.querySelector('.selected');
   if (tasks === null) {
-    event.target.classList.add('selected')
-    event.target.style.background = 'rgb(128, 128, 128)'
+    event.target.classList.add('selected');
+    event.target.style.background = 'rgb(128, 128, 128)';
   } else {
     tasks.removeAttribute('style');
     tasks.classList.remove('selected');
-    event.target.classList.add('selected')
-    event.target.style.background = 'rgb(128, 128, 128)'  
+    event.target.classList.add('selected');
+    event.target.style.background = 'rgb(128, 128, 128)';
   }
-})
+});
+
+// Torna a tarefa completa
+taskList.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
