33ab88af905d709201dd1d9822c6e4ce1212289d
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-11-10 18:25:28 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const criarTarefa = document.querySelector('#criar-tarefa');
+
 criarTarefa.addEventListener('click', tarefa);
+addEventListener('click', corLista)
 
 function tarefa (){
   const listaDeTarefas = document.querySelector('#lista-tarefas');
   const tarefa = document.getElementById('texto-tarefa');
   const lista = document.createElement('li');
+  lista.className = 'lista';
   lista.innerText = tarefa.value;
   listaDeTarefas.appendChild(lista);
   tarefa.value = "";
+}
+
+function corLista (event) {
+  const evento = event.target;
+  const estilo = event.target.style;
+  const lista = document.querySelectorAll('.lista');
+  if(evento.className == 'lista') {
+    if(estilo.backgroundColor == 'rgb(128, 128, 128)'){
+      estilo.backgroundColor = 'rgb(255, 255, 255)'
+    } else {
+      estilo.backgroundColor = 'rgb(128, 128, 128)';
+    } 
+  }
 }
