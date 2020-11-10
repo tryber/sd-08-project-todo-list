c6e30e5008bf5a81df03033740d703f820436e9b
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-10 15:33:18 -0300

    Requisito 01 com javaScript

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const body = document.querySelectorAll('body');
-console.log(body);
+
+// Requisito 01
+function addHeader() {
+    let body = document.getElementsByTagName('body');
+    let header = document.createElement('header');
+    let h3 = document.createElement('h3');
+    h3.innerText = 'Minha Lista de Tarefas';
+    for (let index = 0; index < body.length; index += 1) {
+        body[index].appendChild(header).appendChild(h3)
+    }
+}
+
+addHeader();
+
+// Requisito 02
