c4266c0f1db3e09b1ff4cfab2d42f0ebab982c9e
Vinicius <viniciussilvaribeiro@gmail.com>
2020-11-10 19:06:23 -0300

    adicionando 7

--- script.js
+++ script.js
@@ -8,7 +8,29 @@ function botao() {
         pai.appendChild(linha);
         linha.innerText = texto.value;
         texto.value = "";
+
+        //Colorindo de cinza
+        linha.addEventListener("click", (event) => {
+                const marcado = document.querySelector(".marcado");
+                if (marcado !== null) {
+                    marcado.classList.remove("marcado");
+                }
+
+                event.target.classList.add("marcado");
+
+            //linha.style.backgroundColor = "rgb(128, 128, 128)";
+        })
+
+        //Riscando texto
+        linha.addEventListener("dblclick", (event) => {
+            if (linha.className === "") 
+            event.target.classList.add("completed");
+            else event.target.classList.remove("completed");
+        })
     })
     
 }
 botao();
+
+
+
