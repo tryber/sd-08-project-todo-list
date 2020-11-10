12c2afc94e20989758ccd6eb6b8aaa4c97bee52b
Antônio Schappo <antonioschappo@gmail.com>
2020-11-10 20:07:07 -0300

    First try ex. 7

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ let tasksListed = document.getElementsByTagName('li');
 
 
 taskButton.addEventListener('click', function(event){
+
 let task = document.getElementById('texto-tarefa');
 let taskListItem = document.createElement('li');
-let taskList = document.getElementById('lista-tarefas')
+let taskList = document.getElementById('lista-tarefas');
+let selectedTasks = document.body.getElementsByClassName('colorless taskSelected')
 taskListItem.innerText = task.value;
-taskList.appendChild(taskListItem)
-taskListItem.classList.add('colorless')
+
+taskList.appendChild(taskListItem);
+taskListItem.classList.add('colorless');
 task.value = '';
+
 for (let i = 0; i< tasksListed.length; i+=1 ){
     let taskListed = tasksListed[i];
-    taskListed.addEventListener('dblclick', function(event){
+    taskListed.addEventListener('click', function(event){
     let task = event.target;
-    task.classList.toggle('taskSelected');
+    
+for(let i = 0; i < selectedTasks.length; i+=1){
+    let selectedTask = selectedTasks[i];
+    if(selectedTask){
+        selectedTask.classList.toggle('taskSelected')
+    }
+}
+task.classList.toggle('taskSelected');
 })}
 });
 
