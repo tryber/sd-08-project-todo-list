f3e2dc52b59d0c0224ac9d3df9029f8a96e76821
emassaki <massakihirayama@gmail.com>
2020-11-10 16:58:36 -0300

    Adicionados requisitos 7 e 8 - Destacar uma unica tarefa selecionada

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // Adicionar tarefa à lista
 const addTaskButton = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 addTaskButton.addEventListener('click', () => {
-  const taskList = document.getElementById('lista-tarefas');
   let input = document.getElementById('texto-tarefa');
   const task = document.createElement('li');
   task.innerHTML = input.value;
+  task.classList.add('task');
   input.value = "";
   taskList.appendChild(task);
+})
+
+// Pinta a tarefa clicada
+taskList.addEventListener('click', (event) => {
+  const tasks = document.querySelector('.selected');
+  if (tasks === null) {
+    event.target.classList.add('selected')
+    event.target.style.background = 'rgb(128, 128, 128)'
+  } else {
+    tasks.removeAttribute('style');
+    tasks.classList.remove('selected');
+    event.target.classList.add('selected')
+    event.target.style.background = 'rgb(128, 128, 128)'  
+  }
 })
