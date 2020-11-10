40b948368d1eb399454ad3d2420a2cb6efc1c5df
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-10 15:52:39 -0300

    esqueleto html inicial

---
+++ script.js
@@ -0,0 +1,3 @@
+// criar li dinamicamente
+// dinamizar o clique do button desaparecer o input.value e mostrar o input.value como li
+// dblclick no li faz text-decoration line-through solid rgb (0,0,0)
