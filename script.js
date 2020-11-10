cc1530cf40b38ee619fd01a336a18651e31aa815
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-10 16:01:24 -0300

    Refatorando requisitos 01,02 e 03 e colocando em funçoes separadas

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 
-// Requisito 01 e Requisito 02 Requisito 03
+// Requisito 01
 
+let body = document.getElementsByTagName('body');
 function addHeader() {
-    let body = document.getElementsByTagName('body');
     let header = document.createElement('header');
     let h3 = document.createElement('h3');
-    let paragraph = document.createElement('p');
-    let boxText = document.createElement('input');
-    boxText.id = 'texto-tarefa';
-    boxText.type = 'text'
     h3.innerText = 'Minha Lista de Tarefas';
-    paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
-    paragraph.id = 'funcionamento'
+
     for (let index = 0; index < body.length; index += 1) {
         body[index].appendChild(header).appendChild(h3);
-        body[index].appendChild(paragraph)
-        body[index].appendChild(boxText)
     }
 }
 
 addHeader();
 
+// Requisito 02
+
+function addParagraph (){
+    let paragraph = document.createElement('p');
+    paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+    paragraph.id = 'funcionamento'
+
+    for (let index = 0; index < body.length; index += 1) {
+        body[index].appendChild(paragraph)
+    }
+}
+addParagraph();
+
+// Requisito 03
+
+function addInput(){
+    let boxText = document.createElement('input');
+    boxText.id = 'texto-tarefa';
+    boxText.type = 'text'
+    for (let index = 0; index < body.length; index += 1) {
+        body[index].appendChild(boxText);
+    }
+}
+
+addInput();
+
+// Requisito 04
