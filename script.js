770b1855fab52dc4bda4fb0853387978e277bcb4
Vitor Nunes <vitornunes200@gmail.com>
2020-11-10 18:35:10 -0300

    Requisitos Obrigatorios

--- script.js
+++ script.js
@@ -4,25 +4,42 @@ function adicionaTexto(){
     let list = document.getElementById('lista-tarefas');
     let itemList = document.createElement('li');
     itemList.innerText = texto.value;
+    //Requisito 7
+    itemList.addEventListener("click", function(){
+        verificaListItem();
+        itemList.style.backgroundColor = 'rgb(' + [128,128,128].join(',') + ')';
+       });
+    // Requisito 9
+    itemList.addEventListener("dblclick", function(){
+        itemList.classList.toggle('completed');
+       });
     list.appendChild(itemList);
     texto.value = "";
-    selectItemList();
-}
-function selectItemList(){
-    
-    let box = document.getElementsByTagName('li');
-        Array.from(box).forEach(function(box){
-            box.addEventListener("click", function(){
-            verificaListItem();
-            box.style.backgroundColor = 'rgb(' + [128,128,128].join(',') + ')';
-           })
-        });
 }
 
+//Requisito 8
 function verificaListItem(){
 
     let box = document.getElementsByTagName('li');
         Array.from(box).forEach(function(box){
-            box.style.backgroundColor = 'white';
+            box.style.removeProperty('background-color');
         });
+}
+//Requisito 10
+function clearAll(){
+
+    let list = document.getElementById('lista-tarefas');
+    list.innerHTML = '';
+}
+//Requisito 11
+function clearSelected(){
+    
+    let list = document.getElementById('lista-tarefas');
+    let box = document.getElementsByTagName('li');
+        Array.from(box).forEach(function(box){
+            if(box.className == "completed"){
+                list.removeChild(box);
+            }
+        });
+
 }
