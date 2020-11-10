598b84678b4694925db8d13fbf99dbeb3192ee67
enicio <jessandro42@gmail.com>
2020-11-10 18:32:52 -0300

    Nono requisito

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let buttonPushed = document.querySelector('#criar-tarefa')
+buttonPushed.addEventListener("click", newTask)
+
 function newTask () {
   let newTask = document.createElement('Li')
   newTask.innerText = document.querySelector('#texto-tarefa').value
@@ -5,17 +9,12 @@ function newTask () {
   document.querySelector('#texto-tarefa').value = ""
 }
 
-let buttonPushed = document.querySelector('#criar-tarefa')
-buttonPushed.addEventListener("click", newTask)
 
 let getTasks = document.getElementById('lista-tarefas')
 getTasks.addEventListener('click', changeBackground)
 
 function changeBackground (e) {
-  //console.log(e.target)
-
-
-  let allNodes = document.querySelectorAll('li').length
+   let allNodes = document.querySelectorAll('li').length
 
   for (let index = 0; index < allNodes; index += 1) {
 
@@ -23,8 +22,28 @@ function changeBackground (e) {
        document.querySelectorAll('li')[index].style.removeProperty('background-color')
     }
   }
-
-  e.target.style.backgroundColor = "rgb(128, 128, 128)"
-
+   e.target.style.backgroundColor = "rgb(128, 128, 128)"
 }
 
+
+getTasks.addEventListener("dblclick", setLineThrough);
+
+function setLineThrough(e) {
+  // let allNodes = document.querySelectorAll('li').length
+
+  // for (let index = 0; index < allNodes; index += 1) {
+
+  //   document.querySelectorAll('li')[index].classList.remove('completed')
+
+  // }
+  //console.log(e.target)
+
+  if (e.target.className == 'completed') {
+    e.target.classList.remove('completed')
+    } else {
+    e.target.className = 'completed'
+  }
+}
+
+// let colorSelected = document.getElementById(e.target.id)
+// 	colorSelected.className = "color selected"
