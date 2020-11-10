97f961bc0cf90b42336ae8ecf34241b829870c4d
Vinicius <viniciussilvaribeiro@gmail.com>
2020-11-10 16:02:01 -0300

    tentando a 5

---
+++ script.js
@@ -0,0 +1,13 @@
+//Requisito 5
+const pai = document.getElementById("lista-tarefas")
+const texto = document.getElementById("texto-tarefa")
+function botao() {
+    const botao1 = document.getElementById("criar-tarefa");
+    botao1.addEventListener("click", (event) => {
+        console.log("aa");
+        const linha = document.createElement("li");
+        pai.appendChild(linha);
+        linha.innerText = texto.value;
+    })
+}
+botao();
