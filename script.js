32556082c1f280a6729a1b601687bed172fa5dbd
Samuel <samuelvanguarda@gmail.com>
2020-11-10 19:11:52 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Botão Adiciona Tarefa
-function adicionaTarefa(){
+function adicionaTarefa() {
     let input = document.getElementById('texto-tarefa');
     const button = document.getElementById('criar-tarefa');
     const ol = document.getElementById('lista-tarefas');
@@ -11,5 +11,13 @@ function adicionaTarefa(){
         input.value = '';
     })    
 }
-
 adicionaTarefa();
+
+// ALtera Cor da Lista
+function alteraCor() {
+    const ol = document.getElementById('lista-tarefas');
+    ol.addEventListener("click", function(evento){
+        evento.target.style.backgroundColor = "rgb(128, 128, 128)";
+    })
+}
+alteraCor();
