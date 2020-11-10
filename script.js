4e59a42bde89bb4e2c7270f89ea00294459dbb71
Daniel <danieldsrd@gmail.com>
2020-11-10 16:10:24 -0300

    Mudando cor de fundo ao selecionar

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ document.querySelector('#criar-tarefa').addEventListener('click', function(){
     let listElement = document.createElement('li');
     document.querySelector('#lista-tarefas').appendChild(listElement);
     listElement.innerHTML = document.querySelector('#texto-tarefa').value;
-    document.querySelector('#texto-tarefa').value = '';   
+    document.querySelector('#texto-tarefa').value = '';
+    changeBackColor();   
 });
+
+//MUDAR COR DE FUNDO
+function changeBackColor(){
+    for(let index = 0; index < document.querySelectorAll('li').length; index+=1) {
+        document.querySelectorAll('li')[index].addEventListener('click', function (event){
+            event.target.style.background = 'rgb(128,128,128)';
+        });
+    }
+}
+
