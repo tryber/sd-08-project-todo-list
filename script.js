157c00b2223acce8208c962689d4df5ab88fe6c1
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-10 18:46:47 -0300

    req-10 postergast no meu item 10 naosei oque estpa roalndo  6-v

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const button  = document.getElementById('criar-tarefa');
-button.addEventListener('click', function() {
+const buttonCreat  = document.getElementById('criar-tarefa');
+buttonCreat.addEventListener('click', function() {
   let task = document.createElement('li');
   let taskText = document.getElementById('texto-tarefa');
   let text =document.createTextNode(taskText.value);
   taskText.value = "";
   task.appendChild(text);
   document.getElementById('lista-tarefas').appendChild(task)
+});
+
+
+const buttonRemove  = document.getElementById('apaga-tudo');
+buttonRemove.addEventListener('click', function() {
+  let task = document.getElementsByTagName('li');
+  for(let i=0; i<task.length; i++){
+    // let apaga = ;
+    task[i].remove();
+  }
 });
