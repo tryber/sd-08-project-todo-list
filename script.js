ca3b36b8ed4a6acc1a39a10ac0506849d0d84162
MuriloBatista <git@github.com:MuriloBatista>
2020-11-10 18:25:26 -0300

    Adiciona 2º requisito

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Cria titulo - 1º requisito
 const title = document.createElement('h1');
 document.querySelector('header').appendChild(title);
 title.innerText = 'Minha Lista de Tarefas';
+//Cria paragrafo e adiciona ID - 2º requisito
+const textInf = document.createElement('p');
+document.querySelector('body').appendChild(textInf);
+textInf.id = 'funcionamento';
+textInf.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
