e43c10d404b33607e987e64f96caf263d68af602
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-10 17:52:49 -0300

    REQ-7: Added option to select tasks

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const btnCreateTask = document.getElementById('criar-tarefa');
 const tasksList = document.getElementById('lista-tarefas');
 let taskInput = document.getElementById('texto-tarefa');
+const colors = {
+  gray: 'rgb(128, 128, 128)',
+};
 
 // Setting the button to add a new task and clean the input
-btnCreateTask.addEventListener('click', function () {
+btnCreateTask.addEventListener('click', addTask);
+
+// Adding a new task
+function addTask(event) {
   const newTask = document.createElement('li');
+  newTask.className = 'task';
   newTask.innerText = taskInput.value;
   taskInput.value = '';
   tasksList.appendChild(newTask);
-});
+  const allTasks = document.querySelectorAll('.task');
+  for (let task = 0; task < allTasks.length; task += 1) {
+    allTasks[task].addEventListener('click', selectTask);
+  }
+}
+
+// Selecting the clicked task and deselecting the other ones
+function selectTask(event) {
+  let taskSelected = document.querySelector('.selected');
+  if (taskSelected) {
+    taskSelected.classList.remove('selected');
+    taskSelected.style.backgroundColor = '';
+  }
+  event.target.classList.add('selected');
+  event.target.style.backgroundColor = colors.gray;
+}
