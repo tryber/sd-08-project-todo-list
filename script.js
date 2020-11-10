91a102b93697b163b3d4cae8655508bf05bb048b
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-10 17:14:43 -0300

    O requisito 6 foi feito junto com o anterior - resolve codeclimate

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let butonTarefaLocal = document.querySelector('#criar-tarefa');
-let listLocal = document.querySelector('#lista-tarefas');
-let inputLocal = document.querySelector('#texto-tarefa');
+const butonTarefaLocal = document.querySelector('#criar-tarefa');
+const listLocal = document.querySelector('#lista-tarefas');
+const inputLocal = document.querySelector('#texto-tarefa');
 
-function insertElement (){ 
-    let newElement = document.createElement('li');
-    newElement.innerHTML = inputLocal.value;
-    listLocal.appendChild(newElement);
-    inputLocal.value = '';
+function insertElement() {
+  const newElement = document.createElement('li');
+  newElement.innerHTML = inputLocal.value;
+  listLocal.appendChild(newElement);
+  inputLocal.value = '';
 }
 
-butonTarefaLocal.addEventListener("click", insertElement);
+butonTarefaLocal.addEventListener('click', insertElement);
