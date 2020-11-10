19870992eb8918edd1d70c136c7d0c53b8c13905
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-10 16:07:58 -0400

    Codeclimate correcoes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-    // Add conteudo na lista
-    let texto = document.getElementById("texto-tarefa");
-    let adicionaTarefa = document.getElementById("criar-tarefa");
-    let lista = document.getElementById("lista-tarefas");
+  // Add conteudo na lista
+  const texto = document.getElementById('texto-tarefa');
+  let adicionaTarefa = document.getElementById('criar-tarefa');
+  let lista = document.getElementById('lista-tarefas');
 
-    adicionaTarefa.addEventListener("click", function () {
-        let tarefa = document.createElement("li");
-        tarefa.innerText = texto.value;
-        lista.appendChild(tarefa);
-        texto.value = "";
-    })
-}
+  adicionaTarefa.addEventListener('click', function () {
+    let tarefa = document.createElement('li');
+    tarefa.innerText = texto.value;
+    lista.appendChild(tarefa);
+    texto.value = '';
+  });
+};
