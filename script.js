b248cd4d7bfe0d1f00815d83cad136614f00d7a0
Fabio Ferreira <fabio35547@gmail.com>
2020-11-10 20:54:04 -0300

    Requisito 13

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ window.onload = function () {
     const btnClearTasksList = document.getElementById('apaga-tudo');
     const btnRemoveTaskCompleted = document.getElementById('remover-finalizados');
     const btnSaveTasksList = document.getElementById('salvar-tarefas');
+    const btnMoveUpTask = document.getElementById('mover-cima');
+    const btnMoveBottomTask = document.getElementById('mover-baixo');
     const olTasksList = document.getElementById('lista-tarefas');
     const listTasks = document.querySelector('.task-list');
 
@@ -15,10 +17,32 @@ window.onload = function () {
     btnClearTasksList.addEventListener('click', clearListTasks);
     btnRemoveTaskCompleted.addEventListener('click', removeTaskCompleted);
     btnSaveTasksList.addEventListener('click', saveTaskList);
+    btnMoveUpTask.addEventListener('click', moveUpTask);
+    btnMoveBottomTask.addEventListener('click', moveBottomTask);
     olTasksList.addEventListener('click', setBackGroundColor);
     olTasksList.addEventListener('dblclick', setTaskCompleted);
 
     // Funções dos eventos dos objetos
+    function moveUpTask(event) {        
+        const childNode = document.querySelectorAll('.task');            
+        for (let index = 0; index < childNode.length; index += 1) {
+            if (childNode[index].classList.contains('selected')) {
+                childNode[index].parentElement.insertBefore(childNode[index], childNode[index - 1]);
+            }
+        }
+    }
+
+    function moveBottomTask(event) {
+        const childNode = document.querySelectorAll('.task');
+        for (let index = 0; index < childNode.length; index += 1) {
+            if (childNode[index].classList.contains('selected')) {
+            if (index + 1 < childNode.length) {
+                childNode[index + 1].parentElement.insertBefore(childNode[index + 1], childNode[index]);
+                }
+            }
+        }
+    }
+
     function saveTaskList() {
         localStorage.clear();
         localStorage.setItem('List', listTasks.innerHTML);
