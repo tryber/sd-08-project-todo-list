12880787599c1be69dc3c5d594f569cd1994ffcd
Igor Nascimento <igorln96@gmail.com>
2020-11-10 16:46:56 -0300

    req7 e 8: mudando o background do item selecionado

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+// Botão para add tarefa
 let buttonCriar = document.getElementById("criar-tarefa");
+let lista = document.getElementById("lista-tarefas");
 buttonCriar.addEventListener("click", function(){
     let text = document.getElementById("texto-tarefa");
-    let lista = document.getElementById("lista-tarefas");
     let itemLista = document.createElement("li");
     itemLista.innerHTML = text.value;
     lista.appendChild(itemLista);
     text.value = ""    
 })
+
+let listItem = document.getElementsByTagName("li");
+lista.addEventListener("click", function(){
+for (let i = 0; i < listItem.length; i += 1) {
+listItem[i].addEventListener("click", function(event){
+    for (let i = 0; i < listItem.length; i += 1) {
+        listItem[i].className = "";
+    }
+    event.target.className = "selected";
+})
+}
+})
+
+// lista.addEventListener("click", function(event){
+//     let listItem = document.getElementsByTagName("li");
+//     for (let i = 0; i < listItem.length; i += 1) {
+//         listItem[i].className = "";
+//     }
+//     event.target.className = "selected"
+// })
