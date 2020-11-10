2fdd46fa7b955890823a1ed054c2a63de94db36d
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-10 18:17:48 -0300

    First shipping requirement 7

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function actionAddTask() {
     txtTask.value = '';
   });
 }
+function setBackgroudItem(element) {
+  const itens = document.getElementsByTagName('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    itens[i].style.backgroundColor = '';
+  }
+  element.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function selectItem() {
+  const itens = document.getElementById('lista-tarefas');
+  itens.addEventListener('click', function (event) {
+    setBackgroudItem(event.target);
+  });
+}
+
 window.onload = function () {
+  selectItem();
   actionAddTask();
 };
