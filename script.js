65a4868f7dbe40622d521c362341838d1c444b69
Fabio Ferreira <fabio35547@gmail.com>
2020-11-10 19:50:01 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,73 +1,92 @@
-// Lista de objetos
-const txtTask = document.getElementById('texto-tarefa');
-const btnCreateTask = document.getElementById('criar-tarefa');
-const btnClearListTasks = document.getElementById('apaga-tudo');
-const btnRemoveTaskCompleted = document.getElementById('remover-finalizados');
-const olListTasks = document.getElementById('lista-tarefas');
+window.onload = function () {
 
-// Lista de eventos de objetos
-txtTask.addEventListener('keyup', checkEnterEvent);
-btnCreateTask.addEventListener('click', createTask);
-btnClearListTasks.addEventListener('click', clearListTasks);
-btnRemoveTaskCompleted.addEventListener('click', removeTaskCompleted);
-olListTasks.addEventListener('click', setBackGroundColor);
-olListTasks.addEventListener('dblclick', setTaskCompleted);
+    // Lista de objetos
+    const txtTask = document.getElementById('texto-tarefa');
+    const btnCreateTask = document.getElementById('criar-tarefa');
+    const btnClearTasksList = document.getElementById('apaga-tudo');
+    const btnRemoveTaskCompleted = document.getElementById('remover-finalizados');
+    const btnSaveTasksList = document.getElementById('salvar-tarefas');
+    const olTasksList = document.getElementById('lista-tarefas');
+    const listTasks = document.querySelector('.task-list');
 
-// Funções dos eventos dos objetos
-function setBackGroundColor(event) {    
-    if (document.querySelector('.selected') !== null){
-        document.querySelector('.selected').style.backgroundColor = '';
-        document.querySelector('.selected').classList.remove('selected');
+    // Lista de eventos de objetos
+    txtTask.addEventListener('keyup', checkEnterEvent);
+    btnCreateTask.addEventListener('click', createTask);
+    btnClearTasksList.addEventListener('click', clearListTasks);
+    btnRemoveTaskCompleted.addEventListener('click', removeTaskCompleted);
+    btnSaveTasksList.addEventListener('click', saveTaskList);
+    olTasksList.addEventListener('click', setBackGroundColor);
+    olTasksList.addEventListener('dblclick', setTaskCompleted);
+
+    // Funções dos eventos dos objetos
+    function saveTaskList() {
+        localStorage.clear();
+        localStorage.setItem('List', listTasks.innerHTML);
     }
-    event.target.classList.add('selected');
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';       
-}
 
-function setTaskCompleted(event) {
-    if (event.target.classList.contains('completed')) {
-        event.target.classList.remove('completed');
-    } else {
-    event.target.classList.add('completed');
-    }      
-}
+    loadLocalStorageItems();
 
-function removeTaskCompleted() {
-    for (let index = 0; index < olListTasks.childElementCount; index += 1) {
-      const task = olListTasks.children[index];
-      if (task.classList.contains('completed')) {
-        olListTasks.removeChild(task);
-        index -= 1;
+    function loadLocalStorageItems() {
+        if(localStorage.length > 0) {
+            listTasks.innerHTML = localStorage.getItem('List');
+        }
       }
-    }
-  }
 
-function checkEnterEvent(event) {
-    if (event.type === 'keyup')
-    {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            btnCreateTask.click();
+    function setBackGroundColor(event) {    
+        if (document.querySelector('.selected') !== null){
+            document.querySelector('.selected').style.backgroundColor = '';
+            document.querySelector('.selected').classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';       
+    }
+
+    function setTaskCompleted(event) {
+        if (event.target.classList.contains('completed')) {
+            event.target.classList.remove('completed');
+        } else {
+        event.target.classList.add('completed');
+        }      
+    }
+
+    function removeTaskCompleted() {
+        for (let index = 0; index < olTasksList.childElementCount; index += 1) {
+            const task = olTasksList.children[index];
+            if (task.classList.contains('completed')) {
+                olTasksList.removeChild(task);
+                index -= 1;
+            }
         }
     }
-}
 
-function createTask() {
-    if (txtTask.value == '') {
-        alert('Digite uma tarefa');
+    function checkEnterEvent(event) {
+        if (event.type === 'keyup')
+        {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                btnCreateTask.click();
+            }
+        }
     }
-    else {
-        let listTasks = document.querySelector('.task-list');
-        let liTask = document.createElement('li');
-        
-        liTask.className = "task";
-        liTask.innerHTML = txtTask.value;
-        listTasks.appendChild(liTask);
 
-        txtTask.value = '';
+    function createTask() {
+        if (txtTask.value == '') {
+            alert('Digite uma tarefa');
+        }
+        else {
+            let listTasks = document.querySelector('.task-list');
+            let liTask = document.createElement('li');
+            
+            liTask.className = "task";
+            liTask.innerHTML = txtTask.value;
+            listTasks.appendChild(liTask);
+
+            txtTask.value = '';
+        }
     }
-}
 
-function clearListTasks() {    
-    let listTasks = document.getElementById('lista-tarefas');
-    listTasks.innerHTML = '';
+    function clearListTasks() {    
+        let listTasks = document.getElementById('lista-tarefas');
+        listTasks.innerHTML = '';
+    }
 }
