2ec55a90895f229c6aa3339d5e8cdcbb20bd545f
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-11 15:24:52 -0300

    Revisando requisito 5

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const listaPai = document.querySelector('#lista-tarefas')
-const botaoAdd = document.querySelector('#criar-tarefa')
-const input = document.querySelector('#texto-tarefa')
+const listaPai = document.querySelector('#lista-tarefas');
+const botaoAdd = document.querySelector('#criar-tarefa');
+const input = document.querySelector('#texto-tarefa');
+const lista = document.querySelectorAll('.lis');
 
 
 botaoAdd.addEventListener('click',function adicionarTarefas(){
     let tasks = document.createElement('li');
     tasks.innerText = input.value;
     listaPai.appendChild(tasks);
-    input.value = ' ';
+    input.value = '';
+    tasks.className="lis"
 });
+console.log(lista);
+
+for(let index = 0; index < lista.length; index+=1){
+    listaPai.addEventListener('click', function(event){
+        event.target.style.backgroundColor = 'rgb(128,128,128)';
+    })
+}
