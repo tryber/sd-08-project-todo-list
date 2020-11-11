f356903b8c8666b9f36b025ce7c00724121cf9e7
Ronald Jr <rpintojunior@gmail.com>
2020-11-11 17:56:39 -0300

    Correção Requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-document.getElementById('criar-tarefa').addEventListener('click', function () {
-    const inputItem = document.getElementById('texto-tarefa');
-    const arrayItens = [inputItem.value];
-    createItemList(arrayItens);
-    clearInputValue(inputItem);
-  });
+const orderlist = document.querySelector('#lista-tarefas');
+
+
+function addItemlist() {
+    let btnAdd = document.getElementById('criar-tarefa');
+   
+    function addItem(){
+        let inputItem = document.querySelector('#texto-tarefa').value;
+        let itemList = document.createElement('li');
+        itemList.innerHTML = inputItem;
+        itemList.className= 'tarefa'
+        orderList.appendChild(itemList);
+        clearInputValue();
+    }
+    btnAdd.addEventListener('click', addItem);
+
+    function clearInputValue(){
+        document.querySelector('#text-tarefa').value = '';
+    }
+}
+
