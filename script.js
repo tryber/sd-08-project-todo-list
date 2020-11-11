09112eb5d220d14c0d5d15ae53b0538563abf923
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-11 10:55:56 -0300

    Refatorando 05

--- script.js
+++ script.js
@@ -1,54 +1,20 @@
-
-
-
-// Requisito 04
-
-let ol = document.createElement('ol');
-function addList() {
-    ol.id = 'lista-tarefas'
-
-    for (let index = 0; index < body.length; index += 1) {
-        body[index].appendChild(ol);
-    }
-}
-
-addList();
-
 // Requisito 05
 
-// Função adiciona botao esta sendo chamada apos a criação do input
-function addAssignment(){
-    let button = document.createElement('button');
-    button.id = 'criar-tarefa';
-    button.innerText = 'Adicionar';
-    button.style.margin = '10px'
-    for (let index = 0; index < body.length; index += 1) {
-        body[index].appendChild(button);
-    }
+const listTasks = document.querySelector('#lista-tarefas');
+let button = document.querySelector('#criar-tarefa')
 
+function addTasks (){
     button.addEventListener('click', function(){
-        let textValue = document.getElementById('texto-tarefa').value;
-        let li = document.createElement('li');
-        li.innerText = textValue;
-        // Adiciona classe em todos elementos da lista
-        li.className = 'listItem';
-        if (textValue == ''){
-            alert('Digite algo!')
+        let inputTasks = document.getElementById('texto-tarefa').value;
+        if (inputTasks == '' || null){
+            alert('Digite uma tarefa');
         }else{
-            ol.appendChild(li)
+            let li = document.createElement('li');
+            li.innerText = inputTasks;
+            listTasks.appendChild(li);
             document.getElementById('texto-tarefa').value= '';
         }
-
-        // Requisito 08
-
-        li.addEventListener('click', function(event){
-          if (li.value !== null){
-              
-            event.target.style.backgroundColor = '';
-          }
-          event.target.style.backgroundColor = 'rgb(128,128,128)';
-        });
-
     });
 }
 
+addTasks();
