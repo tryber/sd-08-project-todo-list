4f99606464c6f3b9cee224f18bbeed571cf85f1f
Bruno <brunocmoraes97@gmail.com>
2020-11-11 14:48:38 -0300

    Requisito 9. Adiciona evento ao dar click duplo em um elemento da lista, o elemento é riscado.

--- script.js
+++ script.js
@@ -6,15 +6,43 @@ function createList(){
         let li = document.createElement('li')
         li.innerText += input.value;
         ol.appendChild(li);
-        input.value = "";
+        input.value = '';
     })
 }
 createList();
 
 function tintList(){
     let ol = document.getElementById('lista-tarefas');
-    ol.addEventListener('click', function(e){
-        e.target.style.backgroundColor = 'rgb(128,128,128)'
+    ol.addEventListener('click', function(e){   
+        e.target.style.backgroundColor = 'rgb(128,128,128)';  
     })
 }
 tintList();
+
+/*function selected(){
+    let ol = document.getElementById('lista-tarefas');
+    ol.addEventListener('click', function(e){
+        for(let index = 0; index < ol.length; index +=1){
+            if(e.target.style.backgroundColor == 'rgb(128,128,128'){
+                ol.style.backgroundColor = '';
+            }else{
+                e.target.style.backgroundColor = 'rgb(128,128,128)';
+            }
+        }
+    })
+}
+selected();*/
+
+function doneTask(){
+    let ol = document.getElementById('lista-tarefas');
+    ol.addEventListener('dblclick', function(e){
+        if(e.target.className == 'completed'){
+            e.target.classList.remove('completed');
+        }else {
+            e.target.classList.add('completed');
+        }
+    })
+}
+
+doneTask();
+
