c0e5d2b87a933ed6b34d3a4de626c92d12b89776
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-10 22:39:45 -0300

    add requisito 7

--- script.js
+++ script.js
@@ -9,11 +9,37 @@ window.onload = function(){
 function addTask(){  
   let novaTarefa = document.createElement('li')
   novaTarefa.innerText = document.getElementById('texto-tarefa').value
+  novaTarefa.id='tarefa'
   listaTarefa.appendChild(novaTarefa)
-  document.getElementById('texto-tarefa').value = ""
-
-  console.log(listaTarefa)
+  document.getElementById('texto-tarefa').value = ""  
 }
+
+    addEventListener('click',function(event){
+    let x = document.querySelectorAll('li');      
+      for (let i=0; i < x.length; i+=1){        
+        if (x[i].style.backgroundColor == "rgb(128,128,128)"){
+          x[i].style.backgroundColor == "rgb(255,255,255)"
+        }            
+    }
+  })
+
+  
+  addEventListener('click',function(event){
+    let y = event.target
+    if (y.id == 'tarefa'){
+    y.style.backgroundColor = "rgb(128,128,128)"
+    }    
+  })
+
+
+
+  
+
+  
+  
+
+  
+
 
 
 
