f61ece2769015da899368ea2d3fd86fa5175717b
victor Farias <victorfarias.tv@gmail.com>
2020-11-11 13:38:03 -0300

    CodeClimate

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-adicionaTarefa();
 function adicionaTarefa() {
-  let listaOrdenada = document.getElementById('lista-tarefas')
-  let buttonAdicionar = document.getElementById('criar-tarefa');
-  let textoTarefa = document.getElementById('texto-tarefa');
+  const listaOrdenada = document.getElementById('lista-tarefas');
+  const buttonAdicionar = document.getElementById('criar-tarefa');
+  const textoTarefa = document.getElementById('texto-tarefa');
   buttonAdicionar.addEventListener('click', function () {
-    let tarefa = document.createElement('li');
+    const tarefa = document.createElement('li');
     tarefa.className = 'tarefa';
     tarefa.innerText = textoTarefa.value;
     listaOrdenada.appendChild(tarefa);
     textoTarefa.value = '';
   });
 }
+adicionaTarefa();
 
 function removeColor() {
-  let listaTarefas = document.querySelector('#lista-tarefas');
-  let tarefas = listaTarefas.children;
+  const listaTarefas = document.querySelector('#lista-tarefas');
+  const tarefas = listaTarefas.children;
   for (let i = 0; i < tarefas.length; i += 1) {
     tarefas[i].classList.remove('selected');
   }
 }
 
-adicionaColor();
 function adicionaColor() {
-  let listaTarefas = document.querySelector('#lista-tarefas');
+  const listaTarefas = document.querySelector('#lista-tarefas');
   listaTarefas.addEventListener('click', function (event) {
     removeColor();
     event.target.classList.add('selected');
   });
 }
+adicionaColor();
 
-tarefaCompleta();
 function tarefaCompleta() {
-  let listaTarefas = document.querySelector('#lista-tarefas');
+  const listaTarefas = document.querySelector('#lista-tarefas');
   listaTarefas.addEventListener('dblclick', function (event) {
     if (event.target.classList.contains('completed')) {
       event.target.classList.remove('completed');
@@ -40,25 +39,26 @@ function tarefaCompleta() {
     }
   });
 }
+tarefaCompleta();
 
-apagaTudo();
 function apagaTudo() {
-  let apagaButton = document.getElementById('apaga-tudo');
+  const apagaButton = document.getElementById('apaga-tudo');
   apagaButton.addEventListener('click', function () {
-    let listaTarefas = document.getElementById('lista-tarefas');
+    const listaTarefas = document.getElementById('lista-tarefas');
     listaTarefas.innerText = '';
   });
 }
+apagaTudo();
 
-removeCompletos();
 function removeCompletos() {
-  let removeButton = document.getElementById('remover-finalizados');
-  let listaTarefas = document.getElementById('lista-tarefas');
+  const removeButton = document.getElementById('remover-finalizados');
+  const listaTarefas = document.getElementById('lista-tarefas');
   removeButton.addEventListener('click', function () {
-    let tarefas = document.querySelectorAll('.completed');
+    const tarefas = document.querySelectorAll('.completed');
     for (let i = 0; i < tarefas.length; i += 1) {
-      let tarefasCompletas = tarefas[i];
+      const tarefasCompletas = tarefas[i];
       listaTarefas.removeChild(tarefasCompletas);
     }
   });
 }
+removeCompletos();
