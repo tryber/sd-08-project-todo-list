97ec880a37e44e14a8f202dea09e3fa86dce037f
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-11 15:55:28 -0300

    puts

--- script.js
+++ script.js
@@ -8,4 +8,7 @@ buttongraber.addEventListener('click', function(){
     paragrafo.innerText=inputfield.value
     listadetarefas.appendChild(paragrafo)
     inputfield.value=" "
+    paragrafo.addEventListener('click', function(){
+        paragrafo.style.backgroundColor ='rgb(128, 128, 128)'
+    })
 })
