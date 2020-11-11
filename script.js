41da54270ee5015ea2a298ff45fddcf6dde476bb
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-11 13:58:52 -0300

    fixed ninth requirement

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 const getButton = document.getElementById('criar-tarefa');
 const getOrderedList = document.getElementById('lista-tarefas');
 const getTaskList = document.getElementsByClassName('task-list');
+const getInput = document.getElementById('texto-tarefa');
 
-function createTaskList() {
-  const getInput = document.getElementById('texto-tarefa');
-  getButton.addEventListener('click', () => {
-    const createList = document.createElement('li');
-    createList.innerText = getInput.value;
-    createList.className = 'task-list';
-    getOrderedList.appendChild(createList);
-    getInput.value = '';
-  });
-}
-createTaskList();
-
-function selectList() {
-  getOrderedList.addEventListener('click', (event) => {
-    for (let i = 0; i < getTaskList.length; i += 1) {
-      getTaskList[i].classList.remove('selected');
-      event.target.classList.add('selected');
-      getTaskList[i] = event.target;
-    }
-  });
-}
-selectList();
-
-function switchLineThrough() {
-  if (getTaskList.className !== 'completed') {
-    getOrderedList.addEventListener('dblclick', (event) => {
-      for (let i = 0; i < getTaskList.length; i += 1) {
-        event.target.classList.toggle('completed');
-        getTaskList[i] = event.target;
-      }
-    });
+function toggleSelected(event) {
+  for (let i = 0; i < getTaskList.length; i += 1) {
+    getTaskList[i].classList.remove('selected');
+    event.target.classList.add('selected');
+    getTaskList[i] = event.target;
   }
 }
-switchLineThrough();
+
+function toggleCompleted(event) {
+  event.target.classList.toggle('completed');
+}
+
+function createTaskList() {
+  const createList = document.createElement('li');
+  createList.innerText = getInput.value;
+  createList.className = 'task-list';
+  createList.addEventListener('dblclick', toggleCompleted);
+  createList.addEventListener('click', toggleSelected);
+  return createList;
+}
+
+getButton.addEventListener('click', () => {
+  const li = createTaskList();
+  getOrderedList.appendChild(li);
+  getInput.value = '';
+});
