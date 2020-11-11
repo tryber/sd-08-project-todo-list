8a4e983070b8dc640c00273858a741e587bf0b46
wagnerberna <cabelo29007@gmail.com>
2020-11-11 14:18:47 -0300

    inicio funções

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ function createButton(nome, id) {
 }
 
 createButton('Criar Tarefa', 'criar-tarefa');
+document.getElementById('criar-tarefa').addEventListener('click', fCriarTarefa);
 createButton('Apaga Tudo', 'apaga-tudo');
+document.getElementById('apaga-tudo').addEventListener('click', fApagaTudo);
 createButton('Remover Finalizados', 'remover-finalizados');
+document.getElementById('remover-finalizados').addEventListener('click', fRemoverFinalizados);
 createButton('Salvar Tarefas', 'salvar-tarefas');
 createButton('Mover para Cima', 'mover-cima');
 createButton('Mover para Baixo', 'mover-baixo');
 createButton('Remover Selecionado', 'remover-selecionado');
+
+function fCriarTarefa() {
+console.log('btn criar tarefa');
+}
+
+function fApagaTudo() {
+console.log('btn apaga tudo');
+}
+
+function fRemoverFinalizados() {
+console.log('btn remove finalizados');
+}
+
