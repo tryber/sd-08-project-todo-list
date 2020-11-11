9d3528103ed951579a611975dc72fa76190c7023
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-11 16:12:54 -0300

    teste

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ let buttongraber = document.getElementById("criar-tarefa")
 let inputfield = document.getElementById('texto-tarefa')
 let container = document.getElementsByClassName("container")
 let listadetarefas = document.getElementById('lista-tarefas')
+let completed = document.getElementsByClassName ('.completed')
 
 buttongraber.addEventListener('click', function(){
     let paragrafo = document.createElement('li')
     paragrafo.innerText=inputfield.value
     listadetarefas.appendChild(paragrafo)
-    inputfield.value=" "
+    inputfield.value=''
     paragrafo.addEventListener('click', function(){
         paragrafo.style.backgroundColor ='rgb(128, 128, 128)'
-    })
-})
+     paragrafo.addEventListener('dblclick', function(){
+         paragrafo.style.classname = "completed"
+            })
+
+        })
+           })
