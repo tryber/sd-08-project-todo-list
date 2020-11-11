7425e7bd759fd387ffe020d95bbe10cb279ac235
miguelbrn <miiguelalb@gmail.com>
2020-11-10 22:50:23 -0300

    Adcionado mais funções

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+window.onload = function () {
+    localStorage.getItem("tarefas") != {} && (listaTarefas.innerHTML = localStorage.getItem("tarefas"));
+  }
 
 let criarButton = document.getElementById("criar-tarefa")
 let input = document.getElementById("texto-tarefa")
 let listaTarefas = document.getElementById("lista-tarefas")
+let tarefas = listaTarefas.children
 
 function criarTarefa() {
     let tarefa = document.createElement('li');
     tarefa.innerText = input.value;
     listaTarefas.appendChild(tarefa);
+    tarefa.addEventListener('click', function() {
+        event.target.classList.toggle("selected");
+        for(item of tarefas) {
+        item != event.target && item.classList.remove("selected");
+            }
+        }
+    )
     input.value = "";
+}
+
+function apagaTudo() {
+    listaTarefas.innerHTML = ""
+}
+
+function apagaTarefa() {
+    for (item of tarefas) {
+        item.classList.contains("selected") && listaTarefas.removeChild(item)
+    }
 }
