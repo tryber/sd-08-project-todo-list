153e63f7dbf2943b45c901f743abff5ca399feca
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-11 16:08:06 -0300

    Tentando arrumar o requisito 9

--- script.js
+++ script.js
@@ -8,69 +8,42 @@ function createTask() {
         newLi.className = 'task';
         list.appendChild(newLi);
         input.value = '';
-        selecting();
     })
 }
 
 createTask();
 
 function selecting() {
-    let task = document.getElementsByClassName('task');
-    for (let i in task) {
-        task[i].addEventListener('click', function(event) {
-            let selected = document.querySelector('.selected');
-            if (selected !== null) {
-                selected.className = 'task';
-            }
+    let list = document.querySelector('#lista-tarefas');
+    list.addEventListener('click', function(event) {
+        let selected = document.querySelector('.selected');
+        if (selected !== null) {
+            selected.className = 'task';
+        } if (event.target.className === 'task') {
             event.target.className = 'selected';
-            completing();
-        })
-    }
-} 
-
-function completing() {
-    let selected = document.querySelector('.selected')
-    selected.addEventListener('click', function (event) {
-        event.target.className = 'completed';
-        notComplete();
+        }
     })
 }
 
+function completing() {
+    let list = document.querySelector('#lista-tarefas');
+    list.addEventListener('click', function (event) {
+        if (event.target.className === 'selected') {
+            event.target.className = 'completed';
+        }
+    })
+}
+
+completing();
+
 function notComplete() {
-    let completed = document.getElementsByClassName('completed');
-    for (let i in completed) {
-        completed[i].addEventListener('dblclick', function (event) {
-            event.target.className = 'task';       
-        }) 
-    } 
-}    
+    let list = document.querySelector('#lista-tarefas');
+    list.addEventListener('dblclick', function (event) {
+        if (event.target.className === 'completed') {
+            event.target.className = 'task';
+        }
+    })
+}
 
-
-// function selecting() {
-//     let list = document.querySelector('#lista-tarefas')
-//     list.addEventListener('click', function(event) {
-//         let selected = document.querySelector('.selected');
-//         if (selected !== null){
-//             selected.classList.remove('selected');
-//         }
-//         if (event.target.className === 'task') {
-//             event.target.classList.add('selected');
-//         } 
-//     }) 
-// } 
-
-// selecting();
-
-// function completing() {
-//     let list = document.querySelector('#lista-tarefas')
-//     list.addEventListener('dblclick', function (event) {
-//         if (event.target.className === 'completed') {
-//             event.target.classList.remove('completed');
-//         } else if (event.target.className === 'task') {
-//             event.target.classList.add('completed');
-//             event.target.classList.remove('selected');
-//         }
-//     })
-// }
-
-// completing();
+notComplete();
+selecting();
