85e618e7ab95e33230a0d26a5a2015015c6f9e7b
Natanael Neto <nesn.gamedev@gmail.com>
2020-11-11 09:57:51 -0300

    C5 - Getting Focused!

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const lista = document.getElementById('lista-tarefas');
 const itemInput = document.getElementById('texto-tarefa');
 const buttonAddItem = document.getElementById('criar-tarefa');
 
-buttonAddItem.addEventListener('click', function() {
+buttonAddItem.addEventListener('click', function () {
   const item = document.createElement('li');
   item.innerText = itemInput.value;
   itemInput.value = '';
   lista.appendChild(item);
-  console.log(item);
+});
+
+lista.addEventListener('click', function (e) {
+  if (document.getElementsByClassName('focus-item').length > 0) {
+    document.getElementsByClassName('focus-item')[0].classList.remove('focus-item');
+  }
+  e.target.classList.add('focus-item');
 });
