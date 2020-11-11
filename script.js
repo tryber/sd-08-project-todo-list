bc4e93754d78e1c0dfb15e274549673478cedaad
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-11 17:30:31 -0300

    teste

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ buttongraber.addEventListener('click', function(){
     paragrafo.innerText=inputfield.value
     listadetarefas.appendChild(paragrafo)
     inputfield.value=''
-    paragrafo.addEventListener('click', function(){
-        paragrafo.style.backgroundColor ='rgb(128, 128, 128)'
-     paragrafo.addEventListener('dblclick', function(){
-         paragrafo.style.classname = "completed"
-            })
+    paragrafo.addEventListener('click', function(event){
+        // if(event.target.style.backgroundColor !== 'rgb(128, 128, 128)'){
+        // event.target.style.backgroundColor ='rgb(128, 128, 128)'
+        // } 
+        event.target.classList.toggle('colorgray')
+                   
+     paragrafo.addEventListener('dblclick', function(event){
+      event.target.classList.toggle("completed")
 
+        //  paragrafo.classList.add("completed")
+    
+            })
+  
         })
            })
