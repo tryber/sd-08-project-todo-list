c198514ebf03192dc7ee2e1b91799c0a366a1e53
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-11 00:16:32 -0300

    teste backup

--- script.js
+++ script.js
@@ -15,15 +15,6 @@ function addTask() {
 }
 
 addEventListener("click", function (event) {
-  let x = document.querySelectorAll("li");
-  for (let i = 0; i < x.length; i += 1) {
-    if (x[i].style.backgroundColor == "rgb(128,128,128)") {
-      x[i].style.backgroundColor == "rgb(255,255,255)";
-    }
-  }
-});
-
-addEventListener("click", function (event) {
   let y = event.target;
   if (y.id == "tarefa") {
     y.style.backgroundColor = "rgb(128,128,128)";
@@ -32,13 +23,10 @@ addEventListener("click", function (event) {
 
 addEventListener("click", function (event) {
   let y = event.target;
-  if (y.id == "apaga-tudo") { 
-  let li = document.getElementById('lista-tarefas');     
-   li.parentNode.removeChild(li);
-    }     
-
+  if (y.id == "apaga-tudo") {
+    let li = document.getElementById("lista-tarefas");
+    li.parentNode.removeChild(li);
+  }
 });
-
-
 
 // dblclick no li faz text-decoration line-through solid rgb (0,0,0)
