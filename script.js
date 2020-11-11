00a42d21383e04232ee7551dc80f6930ae70ee1e
Leandro Lachini <leandrolachini@gmail.com>
2020-11-11 20:14:38 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-const listaOrdenadaTarefas = document.getElementById('lista-tarefas');
-const button = document.getElementById('criar-tarefa');
-const input = document.getElementById('texto-tarefa');
+let listaOrdenadaTarefas = document.getElementById('lista-tarefas');
+let botao = document.getElementById('criar-tarefa');
+let localTexto = document.getElementById('texto-tarefa');
 
     
-button.addEventListener('click', criaLi);
+botao.addEventListener('click', criaLi);
 
 function criaLi () {
     let tarefaInserida = document.createElement('li')
     tarefaInserida.className = 'tarefa';
-    tarefaInserida.innerHTML = input.value;
-    input.value = '';
+    tarefaInserida.innerHTML = localTexto.value;
+    localTexto.value = '';
     listaOrdenadaTarefas.appendChild(tarefaInserida);
     tarefaInserida.addEventListener('click', corFundoLista)
       
 }
 
+let tarefaSelecionada = ''; 
 function corFundoLista(Event) {
-    Event.target.style.backgroundColor = 'rgb(128,128,128)';
+    if (tarefaSelecionada === ''){
+        tarefaSelecionada = Event.target;
+    } else {
+        tarefaSelecionada.style.backgroundColor = 'white';
+        tarefaSelecionada = Event.target;
+    }
+    tarefaSelecionada.style.backgroundColor = 'rgb(128,128,128)';
     // event é o click
     // target é o elemento html que aconteceu o evento.
     // console.log(Event.target);
-    // event.target = <li></li> 
+   
 }
-
 
 
 // console.log(input);
 
-
 // const criaTarefa = document.getElementById('criar-tarefa');
-
 
 // localStorage.removeItem();
 // localStorage.setItem();
