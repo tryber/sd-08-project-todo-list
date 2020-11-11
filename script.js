88a525a7b8e107767e4204d0362c59d8187f7d65
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-11 20:48:54 -0300

    Overall project improved and some codeclimate issues fixed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const tasksList = document.getElementById('lista-tarefas');
 const taskInput = document.getElementById('texto-tarefa');
 const colors = {
   red: '#de3f44',
-  orange: '#f4a261',
+  orange: '#e57010',
   blue: '#0081ff',
   green: '#1ca345',
 };
@@ -23,6 +23,14 @@ function completedTask(event) {
   event.target.classList.toggle('completed');
 }
 
+function listenerForTasks() {
+  const allTasks = document.querySelectorAll('.task');
+  for (let task = 0; task < allTasks.length; task += 1) {
+    allTasks[task].addEventListener('click', selectTask);
+    allTasks[task].addEventListener('dblclick', completedTask);
+  }
+}
+
 // Adding a new task
 function addTask() {
   const newTask = document.createElement('li');
@@ -34,34 +42,26 @@ function addTask() {
   listenerForTasks();
 }
 
-function listenerForTasks() {
-  const allTasks = document.querySelectorAll('.task');
-  for (let task = 0; task < allTasks.length; task += 1) {
-    allTasks[task].addEventListener('click', selectTask);
-    allTasks[task].addEventListener('dblclick', completedTask);
-  }
-}
-
 // Setting the button to add a new task and clean the input
 btnCreateTask.addEventListener('click', addTask);
 
 // Removing all tasks
 const btnClearAll = document.getElementById('apaga-tudo');
-btnClearAll.addEventListener('click', function (event) {
-  let tasksList = document.getElementById('lista-tarefas');
-  tasksList.innerHTML = '';
+btnClearAll.addEventListener('click', function () {
+  const toDoList = document.getElementById('lista-tarefas');
+  toDoList.innerHTML = '';
 });
 
 // Removing completed tasks
-const btnClearCompleted = document.getElementById('remover-finalizados');
-btnClearCompleted.addEventListener('click', removeCompleted);
-
-function removeCompleted(event) {
+function removeCompleted() {
   const allCompletedTasks = document.querySelectorAll('.completed');
   for (let task = 0; task < allCompletedTasks.length; task += 1) {
     allCompletedTasks[task].remove();
   }
 }
+
+const btnClearCompleted = document.getElementById('remover-finalizados');
+btnClearCompleted.addEventListener('click', removeCompleted);
 
 // Recovering tasks from local storage
 if (localStorage.tasksList) {
@@ -78,8 +78,8 @@ btnSaveTasks.addEventListener('click', function () {
 
 // Setting the arrows to move the tasks up and down
 // -----------------------------------------------------------------
-btnArrowUp = document.getElementById('mover-cima');
-btnArrowDown = document.getElementById('mover-baixo');
+const btnArrowUp = document.getElementById('mover-cima');
+const btnArrowDown = document.getElementById('mover-baixo');
 
 // Moving up
 function moveTaskUp() {
@@ -96,7 +96,7 @@ function moveTaskUp() {
       selectedTask.previousElementSibling.classList.add('selected');
 
       // Removing "selected" class from previous task
-      let twoSelected = document.querySelectorAll('.selected');
+      const twoSelected = document.querySelectorAll('.selected');
       twoSelected[1].classList.remove('selected');
     }
   }
@@ -117,7 +117,7 @@ function moveTaskDown() {
       selectedTask.nextElementSibling.classList.add('selected');
 
       // Removing "selected" class from previous task
-      let twoSelected = document.querySelectorAll('.selected');
+      const twoSelected = document.querySelectorAll('.selected');
       twoSelected[0].classList.remove('selected');
     }
   }
@@ -128,10 +128,10 @@ btnArrowDown.addEventListener('click', moveTaskDown);
 // ------------------------------------------------------------------------
 
 // Setting the button that deletes a single task
-let btnClearTask = document.getElementById('remover-selecionado');
+const btnClearTask = document.getElementById('remover-selecionado');
 
 btnClearTask.addEventListener('click', function () {
-  let selectedTask = document.querySelector('.selected');
+  const selectedTask = document.querySelector('.selected');
   selectedTask.remove();
 });
 
