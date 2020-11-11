87fdf5e9eb3cb138c29ab8eebf4176613a91888c
Uilton Rocha <itoclick@gmail.com>
2020-11-11 02:06:48 -0300

    Requisitos

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-
+  // seletor do input
   let inputSEl = document.getElementById("texto-tarefa");
+  // seletor do ol
   let olsel = document.getElementById("lista-tarefas");
+  // seletor do btn adicionar
   let btnsel = document.getElementById("criar-tarefa");
 
   
-  
+  // escuta o evento adicionar
   btnsel.addEventListener("click", nvtarefa);
 
   function nvtarefa() {
@@ -15,12 +17,26 @@
     }
       else {
     objcria.textContent =container;
+    objcria.className= 'file';
     olsel.appendChild(objcria);
     inputSEl.value = '';}
-    }
+      };
     
+   //Aqui pegamos todos os li
+   let items = document.getElementsByClassName('.file');
   
+  //Depois no loop adicionamos o evento para cada elemento
+  for(let i = 0; i < items.length; i++)
+  items[i].addEventListener('click', mudabg);
+
+    function mudabg(){
+    items.style.background-color: white;
+
+  };
   
+      
+
+    
 
 
  
