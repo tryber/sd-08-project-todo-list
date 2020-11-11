6c80776e86bca5fafcab032bad395f00828ccb91
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-11 18:41:16 -0300

    C08 Requesito 8 primeira tentativa

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ function atualizaValor(input) {
 	campoInput.textContent = input.target.value;
 };
 
+// function toggleSelected(event) {
+//     event.target.classList.toggle('selected');
+//     };
 
-function toggleSelected(event) {
-    event.target.classList.toggle('selected');
+function toggleSelected(click) {
+    click.target.classList.toggle('selected');
+    const tarefasContainer = document.querySelectorAll('.tarefa');
+    for (let index = 0; index < tarefasContainer.length; index += 1) {
+        const tarefa = tarefasContainer[index];
+        if (tarefa.classList.contains('selected') === true) {
+            tarefa.classList.remove('selected');
+        } else {
+            click.target.classList.toggle('selected');
+        };
+    };
 };
 
 
@@ -23,7 +35,7 @@ function criarTarefa() {
     const tarefaElement = document.createElement('li');
     tarefaElement.className = 'tarefa';
     tarefaElement.textContent = campoInput.textContent;
-    tarefaElement.addEventListener('click', toggleSelected)
+    tarefaElement.addEventListener('click', toggleSelected);
 	const listaTarefasContainer = document.getElementById('lista-tarefas');
 	listaTarefasContainer.appendChild(tarefaElement);
 	document.getElementById('texto-tarefa').value='';
