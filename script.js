cdb8931970b0fa5d6c8b03f9e4e0d5379707e0e6
Antônio Schappo <antonioschappo@gmail.com>
2020-11-11 20:25:16 -0300

    first try req. 8

--- script.js
+++ script.js
@@ -7,25 +7,35 @@ taskButton.addEventListener('click', function(event){
 let task = document.getElementById('texto-tarefa');
 let taskListItem = document.createElement('li');
 let taskList = document.getElementById('lista-tarefas');
-let selectedTasks = document.body.getElementsByClassName('colorless taskSelected')
+let selectedTasks = document.body.getElementsByClassName('taskSelected')
 taskListItem.innerText = task.value;
 
 taskList.appendChild(taskListItem);
-taskListItem.classList.add('colorless');
 task.value = '';
 
-for (let i = 0; i< tasksListed.length; i+=1 ){
+for (let i = 0; i< tasksListed.length; i+=1 ){ // For loop que percorre as li's criadas
     let taskListed = tasksListed[i];
     taskListed.addEventListener('click', function(event){
     let task = event.target;
     
-for(let i = 0; i < selectedTasks.length; i+=1){
+for(let i = 0; i < selectedTasks.length; i+=1){ //For loop interno que percorre as Li's selecionadas
     let selectedTask = selectedTasks[i];
     if(selectedTask){
         selectedTask.classList.toggle('taskSelected')
     }
 }
 task.classList.toggle('taskSelected');
-})}
+})
+
+for (let i = 0; i< tasksListed.length; i+=1 ){ // For loop que percorre as li's criadas
+    let taskListed = tasksListed[i];
+    taskListed.addEventListener('dblclick', function(event){
+    let task = event.target;
+    task.classList.toggle('completed')
+    })
+}
+}
 });
 
+
+//Botão limpar (pesquisar do while para remover remove childs)
