a7ac60501e51a890a97fa74292b8edca1d68a0bc
victor718 <victorfelipeoliveira1@gmail.com>
2020-11-11 19:05:34 -0300

    adicionado opc SALVAR

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ function addTarefa() {
             getLista.appendChild(newLi);
             getText.value = '';
         }
-    })
+    });
 }
 
 //SELECIONA TAREFA
-function eventColor (ev, value) {
+function eventColor(ev, value) {
     let getLi = document.querySelectorAll('.list-iten');
     let estado = ev.target.hasAttribute('style');
-    
+
     getLi.forEach((el) => el.removeAttribute('style'));
 
-    if(!estado){
+    if (!estado) {
         ev.target.style.backgroundColor = value;
     }
 }
@@ -32,58 +32,94 @@ function eventColor (ev, value) {
 function selectTarefa() {
     let getOl = document.querySelector('#lista-tarefas');
     let color = `rgb(${128}, ${128}, ${128})`;
-    getOl.addEventListener('click', function(e) {
-        console.log(e.target)
-        if(e.target.classList.contains( 'list-iten' )){
+    getOl.addEventListener('click', function (e) {
+        if (e.target.classList.contains('list-iten')) {
             eventColor(e, color);
         }
-    })
+    });
 }
 
 //RISCANDO TAREFA
-function changeCompleted (el) {
-    if(el.target.classList.contains('completed')){
+function changeCompleted(el) {
+    if (el.target.classList.contains('completed')) {
         el.target.classList.remove('completed');
-    }else{
+    } else {
         el.target.classList.add('completed');
     }
 }
 
 function tarefaCompleta() {
     let getOl = document.querySelector('#lista-tarefas');
-    getOl.addEventListener('dblclick', function(e) {
-        if (e.target.classList.contains( 'list-iten' )) {
+    getOl.addEventListener('dblclick', function (e) {
+        if (e.target.classList.contains('list-iten')) {
             changeCompleted(e);
         }
-    })
+    });
 }
 
+//APAGA LISTA
 function clearAll() {
     let getLista = document.querySelector('#lista-tarefas');
     let getButton = document.querySelector('#apaga-tudo');
-    
+
     getButton.addEventListener('click', () => {
         let getItens = document.querySelectorAll('.list-iten');
         getItens.forEach((e) => getLista.removeChild(e))
-    })
+    });
 }
 
+//APAGA AS TAREFAS COMPLETADAS
 function removeFinish() {
     let getLista = document.querySelector('#lista-tarefas');
     let getButton = document.querySelector('#remover-finalizados');
 
     getButton.addEventListener('click', () => {
         let getLi = document.querySelectorAll('.completed');
-        console.log(getLi);
         getLi.forEach((e) => getLista.removeChild(e));
-    })
-
+    });
 }
 
+//SALVA AS TAREFAS
+function save() {
+    let getButton = document.querySelector('#salvar-tarefas');
+    getButton.addEventListener('click', () => {
+        localStorage.clear();
+        let getLista = document.querySelectorAll('.list-iten');
+        getLista.forEach( (e, key) => {
+            let date = [];
+            date.push(e.classList);
+            date.push(e.innerText);
+            localStorage.setItem(key, date);
+        });
+    });
+}
+
+//CARREGA TAREFAS SALVA
+function loadingDate() {
+    let getLista = document.querySelector('#lista-tarefas');
+    if (localStorage.length > 0){
+        for(let i = 0; i < localStorage.length; i += 1){
+            let li = document.createElement('li')
+            let dates = localStorage.getItem(i);
+            let arrayDate = dates.split(',');
+            li.classList = arrayDate[0];
+            li.innerText = arrayDate[1];
+            getLista.appendChild(li);
+        }
+    } else {
+        alert('NENHUMA TAREFA ENCONTRADA!');
+    }
+}
+
+
+
+//INICIA TODAS AS FUNÇÕES
 window.onload = function () {
     addTarefa();
     selectTarefa();
     tarefaCompleta();
     clearAll();
     removeFinish();
+    save();
+    loadingDate();
 }
