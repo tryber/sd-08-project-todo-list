3916775a8fac660d314116cfe97ca884a0a42ced
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-10 22:45:16 -0300

    Req8 e req9: double click

--- script.js
+++ script.js
@@ -44,7 +44,9 @@ function createInput() {
         //Cria a nova linha, adiciona o texto a ela e associa a lista
         let novaLinha = document.createElement('li');
         novaLinha.innerHTML = novaTarefa;
+        novaLinha.className = '';
         lista.appendChild(novaLinha);
+
         //Adiciona um listener a essa nova linha para que quando for clicado troque a cor de fundo
         novaLinha.addEventListener('click', function(event){
             let getAllListItems = document.querySelectorAll('li');
@@ -52,6 +54,19 @@ function createInput() {
                 getAllListItems[i].style.backgroundColor = 'white';                
             }
             event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        })
+
+        //Adiciona um listener a essa nova linha para que quando for clicado duas vezes o texto fique riscado
+        novaLinha.addEventListener('dblclick', function(event){
+
+            let checkClasses = event.target.className;
+            console.log(checkClasses);
+            
+            if (checkClasses == ''){
+                event.target.className = 'completed';
+            }else{
+                event.target.className = '';
+            }
         })
 
         //Limpa o campo input
