01cd6f4de5f7c9c9e05e075d0a864b3a912c4615
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-11-11 18:07:31 -0300

    Requisito 7 e 8 seleciona background ao clicar

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const addButton = document.querySelector('#criar-tarefa'); //botao
 const inputText = document.querySelector('#texto-tarefa');//onde digita o texto
 const taskList = document.querySelector('#lista-tarefas');//ol
+let liSelected = null;
 
 addButton.addEventListener('click', sendText);
 
 function sendText () {
     let textList =  document.createElement('li');
     textList.innerText = inputText.value;
+    textList.addEventListener('click', changeColor);
     taskList.appendChild(textList);
     inputText.value = "";
-}
+};
+
+
+function changeColor (event) {
+    if (liSelected !== null) {
+        liSelected.style.backgroundColor = "white";
+        
+    }
+        liSelected = event.target;
+        event.target.style.backgroundColor = "rgb(128, 128, 128)";   
+};
+
+
