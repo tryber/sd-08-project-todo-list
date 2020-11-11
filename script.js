2b3c05eb21a75f6f8adfa0218d5ab3340031b218
victor718 <victorfelipeoliveira1@gmail.com>
2020-11-11 00:49:42 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
+//ADICIONA TAREFA NA LISTA
 function addTarefa() {
     let getText = document.querySelector('#texto-tarefa');
     let getLista = document.querySelector('#lista-tarefas');
     let getButton = document.querySelector('#criar-tarefa');
 
-    getButton.addEventListener('click', function() {
-        if(getText.value === ''){
+    getButton.addEventListener('click', function () {
+        if (getText.value === '') {
             alert('ERRO!')
-        }else{
+        } else {
             let newLi = document.createElement('li');
             newLi.innerText = getText.value;
             getLista.appendChild(newLi);
-            getText.value = '';      
+            getText.value = '';
         }
     })
 }
 
-addTarefa();
+
+
+function selectTarefa() {
+    let getOl = document.getElementById('lista-tarefas');
+    const colorGray = `rgb(${128}, ${128}, ${128})`;
+    getOl.addEventListener('mouseenter', function (event) {
+        let getLi = document.querySelectorAll('li');
+        for (let i = 0; i < getLi.length; i += 1) {
+            getLi[i].addEventListener('click', function(event) {
+                event.target.style.background = colorGray;
+                getLi.forEach(function(e, index, arr) {
+                    e.removeAttribute('style')
+                });
+                event.target.style.backgroundColor = colorGray;
+            })
+        }
+    })
+}
+
+window.onload = function () {
+    addTarefa();
+    selectTarefa();
+}
