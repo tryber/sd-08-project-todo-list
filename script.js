a86b4d0a9d5b09122226ade34369a407c6d2648e
Joao <joaocarlitoneto@gmail.com>
2020-11-11 10:40:02 -0300

    Req 02 - Add paragraph

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* ---------------------------- REQUISITO 01 ---------------------------- */
 const header = document.getElementsByTagName('header')[0];
 const title = document.createElement('h1');
+const paragraph = document.createElement('p');
 title.innerHTML = 'Minha Lista de Tarefas';
+paragraph.id = 'funcionamento';
+paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 
 header.appendChild(title);
+header.appendChild(paragraph);
