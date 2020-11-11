f4caf57cba160d62525b5a9e504e774d9c18041a
Leandro Lachini <leandrolachini@gmail.com>
2020-11-11 16:28:45 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-// Parametros de carregamento da pagina
-window.onload = function () {
-           
-   }
+const listaOrdenadaTarefas = document.getElementById('lista-tarefas');
+const button = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+
+button.addEventListener('click',function evento() {
+    let tarefaInserida = document.createElement('li')
+    tarefaInserida.className = 'tarefa';
+    tarefaInserida.innerHTML = input.value;
+    input.value = '';
+
+    listaOrdenadaTarefas.appendChild(tarefaInserida);
+});
 
 
-// link.: https://www.w3schools.com/js/tryit.asp?filename=tryjs_events_onselect
-// function myFunction() {
-//     document.getElementById("demo").innerHTML = "You selected some text";
-//   }
+
+
+// let listaCriada = document.getElementsByClassName('li');
+// listaCriada.addEventListener('click',function evento() {
+//     // let target = evento.target;
+   
+//     console.log('hi');
+// });
+
+
+
+
+// console.log(input);
+
+
+// const criaTarefa = document.getElementById('criar-tarefa');
+
+
+// localStorage.removeItem();
+// localStorage.setItem();
+// localStorage.getItem();
+// localStorage.clear();
+
 
