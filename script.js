7d2bcaf68c61f0839cdf89d816d45478aa735a60
Joao <joaocarlitoneto@gmail.com>
2020-11-11 10:35:35 -0300

    Req 01 - Add title

---
+++ script.js
@@ -0,0 +1,4 @@
+/* ---------------------------- REQUISITO 01 ---------------------------- */
+const header = document.getElementsByTagName('header')[0];
+const title = document.createElement('h1');
+title.innerHTML = 'Minha Lista de Tarefas';
