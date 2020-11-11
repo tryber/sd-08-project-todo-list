e5dd4f08cf70f2fc66fd4791a28d05f456f824db
josebejohann <josebernardojohann@gmail.com>
2020-11-11 12:30:07 -0300

    Colorizing a single task after click

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 // DEFINING SELECTORS
 const inputTasks = document.querySelector('#texto-tarefa');
 const listTasks = document.querySelector('#lista-tarefas');
-const createTask = document.querySelector('#criar-tarefa');
-const cleanTasks = document.querySelector('#apaga-tudo');
-const removeFinishedTasks = document.querySelector('#remover-finalizados');
-const saveTasks = document.querySelector('#salvar-tarefas');
+const listItem = document.querySelectorAll('.list-item');
+const createTaskButton = document.querySelector('#criar-tarefa');
+const cleanTasksButton = document.querySelector('#apaga-tudo');
+const removeFinishedTasksButton = document.querySelector('#remover-finalizados');
+const saveTasksButton = document.querySelector('#salvar-tarefas');
 
-// EVENT LISTENERS
-const eventListeners = () => {
-  createTask.addEventListener('click', createNewTask);
-  // cleanTasks.addEventListener('click', removeAllTasks);
-  // saveTasks.addEvenetListener('click', saveAllTasks);
-}
 // CREATING A NEW TASK
-
 const createNewTask = (event) => {
   event.preventDefault();
 
@@ -28,7 +22,38 @@ const createNewTask = (event) => {
 
   inputTasks.value = '';
 
-  listTasks.appendChild(li);
+  return listTasks.appendChild(li);
 }
 
-eventListeners();
+createTaskButton.addEventListener('click', createNewTask);
+
+// REMOVING PREVIOUS SELECTED TASK
+const removeSelectedTask = () => {
+  for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].classList.remove('selected');
+  }
+};
+
+// SELECTING A SINGLE TASK
+const selectSingleTask = (event) => {
+  if (event.target.className !== 'selected') {
+    removeSelectedTask();
+
+    event.target.classList.add('selected');
+  }
+};
+
+listTasks.addEventListener('click', selectSingleTask);
+
+// COLORIZING A SINGLE TASK
+const colorizeSelectedTask = () => {
+  for (let i = 0; i < listItem.length; i += 1) {
+    if (listItem[i].classList.contains('selected')) {
+      listItem[i].style.backgroundColor = 'rgb(128,128,128)';
+    } else {
+      listItem[i].style.backgroundColor = '';
+    }
+  }
+};
+
+listTasks.addEventListener('click', colorizeSelectedTask);
