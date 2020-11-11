a65777f9f9d0d2e1c6afe28b2af0c15f734f77bd
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-11 18:06:47 -0300

    cansei

--- script.js
+++ script.js
@@ -3,23 +3,46 @@ let inputfield = document.getElementById('texto-tarefa')
 let container = document.getElementsByClassName("container")
 let listadetarefas = document.getElementById('lista-tarefas')
 let completed = document.getElementsByClassName ('.completed')
-
+function todo() {
 buttongraber.addEventListener('click', function(){
     let paragrafo = document.createElement('li')
     paragrafo.innerText=inputfield.value
+    paragrafo.id = ('color')
     listadetarefas.appendChild(paragrafo)
     inputfield.value=''
     paragrafo.addEventListener('click', function(event){
         // if(event.target.style.backgroundColor !== 'rgb(128, 128, 128)'){
         // event.target.style.backgroundColor ='rgb(128, 128, 128)'
         // } 
-        if (event.className ="colorgray"){
-            paragrafo.classList.remove('colorgray')
-            event.target.className ="colorgray"
-        }
+        // if (event.className ="colorgray"){
+        //     paragrafo.classList.remove('colorgray')
+        //     event.target.className ="colorgray"
+        // }
      paragrafo.addEventListener('dblclick', function(event){
       event.target.classList.toggle("completed")
             })
   
         })
            })
+        }
+        todo()
+
+           let paragrafo = document.createElement('li')
+
+           function selecionado (){
+            for ( let index =0; index < paragrafo.length; index+=1) {
+                const addcor = colorsItens[index]
+        
+                addcor.addEventListener('click', function(event){
+                    const selected = document.querySelector('.colorgray')
+                if ( addcor.id ==='color'){
+                        selected.classList.remove('color')
+                        event.target.className='colorgray'
+                    }
+        
+                })
+        
+            }
+        }
+        
+        selecionado()
