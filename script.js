06653ecbbbb41a3d61a9a50730e042c3ca5330b2
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-11 01:07:18 -0300

    C06 Requesito 5 pronto

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ campoInput.addEventListener('input', atualizaValor);
 function atualizaValor(input) {
 	campoInput.textContent = input.target.value;
 };
+
+
+// Cria um elemento 'li' para o input e adiciona o elemento ao pai 'ol' on click
+
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+
+buttonCriarTarefa.addEventListener('click', criarTarefa);
+
+function criarTarefa() {
+	const tarefaElement = document.createElement('li');
+	tarefaElement.textContent = campoInput.textContent;
+	const listaTarefasContainer = document.getElementById('lista-tarefas');
+	listaTarefasContainer.appendChild(tarefaElement);
+	document.getElementById('texto-tarefa').value='';
+};
+
