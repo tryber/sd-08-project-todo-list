6bd3aa09dc2c7055db0a72805aa4456e9410d5ef
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-10 23:17:01 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
-const listaTarefa = document.createElement('ol')
+const listaTarefa = document.createElement("ol");
 
 // criar li dinamicamente
-window.onload = function(){  
-  document.body.appendChild(listaTarefa)
-  listaTarefa.id = 'lista-tarefas'  
+window.onload = function () {
+  document.body.appendChild(listaTarefa);
+  listaTarefa.id = "lista-tarefas";
+};
+
+function addTask() {
+  let novaTarefa = document.createElement("li");
+  novaTarefa.innerText = document.getElementById("texto-tarefa").value;
+  novaTarefa.id = "tarefa";
+  listaTarefa.appendChild(novaTarefa);
+  document.getElementById("texto-tarefa").value = "";
 }
 
-function addTask(){  
-  let novaTarefa = document.createElement('li')
-  novaTarefa.innerText = document.getElementById('texto-tarefa').value
-  novaTarefa.id='tarefa'
-  listaTarefa.appendChild(novaTarefa)
-  document.getElementById('texto-tarefa').value = ""  
-}
-
-    addEventListener('click',function(event){
-    let x = document.querySelectorAll('li');      
-      for (let i=0; i < x.length; i+=1){        
-        if (x[i].style.backgroundColor == "rgb(128,128,128)"){
-          x[i].style.backgroundColor == "rgb(255,255,255)"
-        }            
+addEventListener("click", function (event) {
+  let x = document.querySelectorAll("li");
+  for (let i = 0; i < x.length; i += 1) {
+    if (x[i].style.backgroundColor == "rgb(128,128,128)") {
+      x[i].style.backgroundColor == "rgb(255,255,255)";
     }
-  })
+  }
+});
 
-  
-  addEventListener('click',function(event){
-    let y = event.target
-    if (y.id == 'tarefa'){
-    y.style.backgroundColor = "rgb(128,128,128)"
-    }    
-  })
+addEventListener("click", function (event) {
+  let y = event.target;
+  if (y.id == "tarefa") {
+    y.style.backgroundColor = "rgb(128,128,128)";
+  }
+});
 
+addEventListener("click", function (event) {
+  let y = event.target;
+  if (y.id == "apaga-tudo") { 
+  let li = document.getElementById('lista-tarefas');     
+   li.parentNode.removeChild(li);
+    }     
 
-
-  
-
-  
-  
-
-  
-
-
+});
 
 
 
