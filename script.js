2a027a5437aacec5503f70013a50f4d3fc6d4aa1
amandaos <amanda.775.souza@gmail.com>
2020-11-10 21:59:16 -0300

    Adiciona cor aos selecionados

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+// links de estudos:
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Node/appendChild
+// https://www.guj.com.br/t/capturar-valor-digitado-javascript-puro/300973/5
+// 
+
+var listItemCounter = 0;
+
+function selecionarItemLista(){
+  event.target.classList.add("selected");  
+ 
+}
+
 function adicionarItemLista(){
+
   const novoItemTexto = document.getElementById("texto-tarefa");
   let valorDigitado = novoItemTexto.value;
-  //const listas = `<li>${valorDigitado}</li>`;
   const lista = document.getElementById("lista-tarefas");
   const novoItemLista = document.createElement("li")
+
+  novoItemLista.id = 'list-Item' + listItemCounter++;
+  novoItemLista.onclick = selecionarItemLista;
+
   novoItemLista.appendChild(document.createTextNode(valorDigitado));
   lista.appendChild(novoItemLista);
   novoItemTexto.value = "";
+
 }
 
 window.onload = () => {
   let botaoAdiciona = document.getElementById("criar-tarefa");
   botaoAdiciona.onclick = adicionarItemLista;
+
+
 
 }
 
