700f6b3c6f3ec92f4447feffe9f6d2061fc315ce
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-11-11 00:51:42 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const criarTarefa = document.querySelector('#criar-tarefa');
+const criarTarefa = document.querySelector("#criar-tarefa");
+const apagaTudo = document.querySelector("#apaga-tudo");
 
-function tarefa (){
-  const listaDeTarefas = document.querySelector('#lista-tarefas');
-  const tarefa = document.getElementById('texto-tarefa');
-  const lista = document.createElement('li');
-  lista.className = 'lista';
+function tarefa() {
+  const listaDeTarefas = document.querySelector("#lista-tarefas");
+  const tarefa = document.getElementById("texto-tarefa");
+  const lista = document.createElement("li");
+  lista.className = "lista";
   lista.innerText = tarefa.value;
   listaDeTarefas.appendChild(lista);
   tarefa.value = "";
@@ -12,31 +13,40 @@ function tarefa (){
 
 criarTarefa.addEventListener("click", tarefa);
 
-function corLista (event) {
+function corLista(event) {
   const evento = event.target;
   const estilo = event.target.style;
-  const lista = document.querySelectorAll('.lista');
-  if(evento.className == 'lista' || evento.className == 'lista completed') {
+  const lista = document.querySelectorAll(".lista");
+  if (evento.className == "lista" || evento.className == "lista completed") {
     for (let index = 0; index < lista.length; index += 1) {
-      lista[index].style.backgroundColor = 'rgb(255, 255, 255)';
+      lista[index].style.backgroundColor = "rgb(255, 255, 255)";
     }
-    if(estilo.backgroundColor != 'rgb(128, 128, 128)'){
-      estilo.backgroundColor = 'rgb(128, 128, 128)';
-    } 
+    if (estilo.backgroundColor != "rgb(128, 128, 128)") {
+      estilo.backgroundColor = "rgb(128, 128, 128)";
+    }
   }
 }
 
-addEventListener('click', corLista);
+addEventListener("click", corLista);
 
 function riscado(event) {
   const evento = event.target;
-  const estilo = event.target.style;
-  const linha = document.querySelectorAll('.lista');
-  if(evento.className == 'lista') {
-    evento.className = "lista completed";
-  } else {
-    evento.className = "lista";
+  if (evento.className == "lista" || evento.className == "lista completed") {
+    if (evento.className == "lista") {
+      evento.className = "lista completed";
+    } else {
+      evento.className = "lista";
+    }
   }
 }
 
-addEventListener('dblclick', riscado);
+addEventListener("dblclick", riscado);
+
+function apagar() {
+  const lista = document.querySelector("#lista-tarefas");
+  while (lista.hasChildNodes()) {
+    lista.removeChild(lista.firstChild);
+  }
+}
+
+apagaTudo.addEventListener("click", apagar);
