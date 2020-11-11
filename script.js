2ef6f5447d13e88d930c0fb8a9d03968a6af4e80
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-11 12:19:45 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-const listaTarefa = document.createElement("ol");
+const listaTarefa = document.createElement('ol');
 
 // criar li dinamicamente
 window.onload = function () {
   document.body.appendChild(listaTarefa);
-  listaTarefa.id = "lista-tarefas";
+  listaTarefa.id = 'lista-tarefas';
 };
 
 function addTask() {
-  let novaTarefa = document.createElement("li");
-  novaTarefa.innerText = document.getElementById("texto-tarefa").value;
-  novaTarefa.id = "tarefa";
+  const novaTarefa = document.createElement('li');
+  novaTarefa.innerText = document.getElementById('texto-tarefa').value;
+  novaTarefa.id = 'tarefa';
   listaTarefa.appendChild(novaTarefa);
-  document.getElementById("texto-tarefa").value = "";
+  document.getElementById('texto-tarefa').value = '';
 }
 
-addEventListener("click", function (event) {
-  let y = event.target;
-  if (y.id == "tarefa") {
-    y.style.backgroundColor = "rgb(128,128,128)";
+addEventListener('click', function (event) {
+  const y = event.target;
+  if (y.id === 'tarefa') {
+    y.style.backgroundColor = 'rgb(128,128,128)';
+    y.classList.add('selected')
   }
 });
 
-addEventListener("click", function (event) {
-  let y = event.target;
-  if (y.id == "apaga-tudo") {
-    let li = document.getElementById("lista-tarefas");
+addEventListener('click', function (event) {
+  const y = event.target;
+  let selecteds = document.querySelectorAll('.selected')
+  for (let i = 0; i < selecteds.length; i++) {
+    if (selecteds[i].style.backgroundColor = 'rgb(128,128,128)'){
+      selecteds[i].style.backgroundColor = 'rgb(255,255,255)';
+      y.style.backgroundColor = 'rgb(128,128,128)';  
+    }
+  }
+});
+
+
+
+
+addEventListener('click', function (event) {
+  const y = event.target;
+  if (y.id === 'apaga-tudo') {
+    const li = document.getElementById('lista-tarefas');
     li.parentNode.removeChild(li);
   }
 });
