5fdf8299cc490d7e036b0038b4fac5ea6a0bd4e1
miguelbrn <miiguelalb@gmail.com>
2020-11-10 22:30:26 -0300

    Adcionado funcão de adcionar tarefa

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function criarTarefa() {
 
+let criarButton = document.getElementById("criar-tarefa")
+let input = document.getElementById("texto-tarefa")
+let listaTarefas = document.getElementById("lista-tarefas")
+
+function criarTarefa() {
+    let tarefa = document.createElement('li');
+    tarefa.innerText = input.value;
+    listaTarefas.appendChild(tarefa);
+    input.value = "";
 }
