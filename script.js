7f1cb7402d672f6e0fab6fea6e08989771e717a7
Thiago Ferreira <thiagocristhian@gmail.com>
2020-11-10 23:31:24 -0300

    Requisito 9 - adicionando risco de completed

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ let selButtom = document.querySelector('#criar-tarefa');
 let selInput = document.querySelector('#texto-tarefa');
 let selLista = document.querySelector('#lista-tarefas');
 
-selButtom.addEventListener('click', function() {  
+//Adiciona tarefas
+selButtom.addEventListener('click', function () {
   let textoInput = selInput.value;
   let adiciona = document.createElement('li');
   adiciona.innerText = textoInput;
+  adiciona.className = 'lista';
   selLista.appendChild(adiciona);
   selInput.value = ''
 });
-
-selLista.addEventListener('click', function(event) {
+//Seleciona tarefas
+selLista.addEventListener('click', function (event) {
   let selected = document.querySelector('.selected');
-  if (selected != null) {
-    selected.className = '';
+  if (selected !== null) {
+    selected.classList.remove('selected');
   }
-  event.target.className = 'selected';
-  
-  
+  event.target.classList.add('selected');
+});
+//Coloca risco de completed
+selLista.addEventListener('dblclick', function (event) {
+  let selected = document.querySelector('.completed');
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  };
 });
