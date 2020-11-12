f8903cbe630720b18aa01bf158eb648283df025d
Felipe Segovia <felipe.vsegovia@gmail.com>
2020-11-11 22:37:26 -0300

    Requisito 9 line-through

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function sendText () {
     let textList =  document.createElement('li');
     textList.innerText = inputText.value;
     textList.addEventListener('click', changeColor);
+    textList.addEventListener('dblclick', taskCompleted);
     taskList.appendChild(textList);
     inputText.value = "";
 };
@@ -17,10 +18,20 @@ function sendText () {
 function changeColor (event) {
     if (liSelected !== null) {
         liSelected.style.backgroundColor = "white";
-        
     }
         liSelected = event.target;
         event.target.style.backgroundColor = "rgb(128, 128, 128)";   
 };
+
+function taskCompleted (event) {
+    textList = event.target;
+    if(event.target.classList.contains('completed')){
+        event.target.classList.remove('completed')
+    } else {
+        event.target.classList.add('completed');
+    }
+};
+
+
 
 
