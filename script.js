e81775328c779e46f5d02bef797e0764134ea664
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2020-11-12 10:50:43 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// 984651
+function criaHeader() {
+  let header = document.createElement('header');
+  document.body.appendChild(header);
+  header.innerHTML = 'Minha Lista de Tarefas';
+}
+criaHeader();
+
+function criaParagrafoFuncionamento() {
+  let paragrafo = document.createElement('p');
+  document.body.appendChild(paragrafo);
+  paragrafo.id = 'funcionamento';
+  paragrafo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+}
+criaParagrafoFuncionamento();
+
+function criaInput() {
+  let input = document.createElement('input');
+  document.body.appendChild(input);
+  input.type = 'text';
+  input.id = 'texto-tarefa';
+  input.placeholder = 'item que deseja adicionar';
+}
+criaInput();
