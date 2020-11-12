e24412c3e91c232a673560e04f76085969310174
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-11 22:02:25 -0300

    corrigindo requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let addBotaoDeTarefa = documento.getElementById('criar-tarefa'); 
-let listaDeTarefa = document.getElementById('texto-tarefa'); 
-let textoDaTarefa = document.getElementById('lista-tarefas'); 
+let addBotaoDeTarefa = document.getElementById('criar-tarefa'); 
+let inputDaTarefa = document.getElementById('texto-tarefa'); 
+let listaDaTarefa = document.getElementById('lista-tarefas'); 
 
 addBotaoDeTarefa.addEventListener('click', functionCriaTarefa); 
 function functionCriaTarefa() {
     let listaDeItens = document.createElement('li');
-    listaDeItens.innerText = listaDeTarefa.value;
-    textoDaTarefa.appendChild(listaDeItens);
-    listaDeTarefa.value = '';
+    listaDeItens.innerText = inputDaTarefa.value;
+    listaDaTarefa.appendChild(listaDeItens);
+    inputDaTarefa.value = '';
 }
