481bafcf69d139c735b60388d31dd6b626a39e86
Daniel Leon <daniel.radialista@gmail.com>
2020-11-12 13:58:59 -0300

    desafio 8

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
+// fiz o código com a ajuda do colega Leandro Piasseta
+
 function createList() {
     let button = document.getElementById('criar-tarefa');
     button.addEventListener('click', function () {
         let text = document.getElementById('texto-tarefa');
         let listItems = document.getElementById('lista-tarefas');
-        let listChildsElement = document.createElement('li');
-        listItems.appendChild(listChildsElement);
+        let listChildrenElement = document.createElement('li');
+        listItems.appendChild(listChildrenElement);
         let newText = text.value;
-        listChildsElement.innerText = newText;
+        listChildrenElement.innerText = newText;
         text.value = "";
-        listChildsElement.addEventListener('click', function () {
-            listChildsElement.style.backgroundColor = 'rgb(128,128,128)';
-
-        });
+        listChildrenElement.addEventListener('click', elementColor);
+            listChildrenElement.style.backgroundColor = 'rgb(128,128,128)';
     });
 }
 
 createList();
 
-
+// fiz o código com a ajuda do colega Leandro Piasseta
+function elementColor(event){
+    let itemsList = document.querySelectorAll('li');
+    for(let i = 0;i < itemsList.length; i +=1) {
+        itemsList[i].style.backgroundColor = '';
+    }
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
