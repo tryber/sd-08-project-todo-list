45942110408f4c462a148c011ccb36098677e38d
Natanael Neto <nesn.gamedev@gmail.com>
2020-11-12 15:33:47 -0300

    C8 - Oh, that's right! Only completed must be removed! And 11th req delivered. All required tasks finished!

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const lista = document.getElementById('lista-tarefas');
 const itemInput = document.getElementById('texto-tarefa');
 const buttonAddItem = document.getElementById('criar-tarefa');
 const buttonRemoveAll = document.getElementById('apaga-tudo');
+const buttonRemoveFin = document.getElementById('remover-finalizados');
 
 buttonAddItem.addEventListener('click', function () {
   const item = document.createElement('li');
@@ -23,4 +24,11 @@ lista.addEventListener('dblclick', function (e) {
 
 buttonRemoveAll.addEventListener('click', function () {
   lista.innerHTML = '';
+});
+
+buttonRemoveFin.addEventListener('click', function () {
+  const completed = document.getElementsByClassName('completed');
+  while (completed.length > 0) {
+    completed[0].remove();
+  }
 });
