e622fd9c8a8b77c78544e0a42bb179dd92f3a18d
Maurice Alves <maurice.estevam@gmail.com>
2020-11-12 12:26:50 -0300

    Ao clicar em um item da lista, altere a cor de fundo do item para cinza e Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo. Requisitos 7 e 8.

--- script.js
+++ script.js
@@ -4,17 +4,20 @@
 
     const listaOrdenada = document.getElementById('lista-tarefas');
     
-
     botao.addEventListener('click', criarElemento);
 
     function criarElemento() {
         const itemLista = document.createElement('li');
         itemLista.innerText = entrada.value;
-        itemLista.addEventListener('click', selecionarCor);
+        itemLista.addEventListener('click', corDeFundo);
         listaOrdenada.appendChild(itemLista);
         entrada.value = "";
     }
 
-    function selecionarCor() {
-
-    };
+     function corDeFundo(event) {
+        let  linha =document.querySelectorAll('li');
+        for (let index = 0; index < linha.length;index += 1){
+                linha[index].style.backgroundColor = 'white';
+        }
+        event.target.style.backgroundColor ='rgb(128,128,128)'
+    }
