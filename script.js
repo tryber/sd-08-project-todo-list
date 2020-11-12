b23e84994a11bf88b1b09d97d955631dd2487414
Daniel <marcio.daniel@msn.com>
2020-11-11 21:12:18 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 // Referenciando endereços da lista de atividades e das suas filhas
 const listaTarefas = document.getElementById('lista-tarefas');
 const textoTarefa = document.getElementById('texto-tarefa');
-
 // Referenciando endereços dos botões
 const criarTarefa = document.getElementById('criar-tarefa');
 const apagaTudo = document.getElementById('apaga-tudo');
 const removerFinalziados = document.getElementById('remover-finalizados');
-
 // Criando eventos para os botões
 criarTarefa.addEventListener("click", novasTarefas);
 apagaTudo.addEventListener("click", removeTudo);
 removerFinalziados.addEventListener('click', removeApenasFinalizados);
-//listaTarefas.addEventListener('click', mudarCor);
-
 // Funação que Gerar Novas Lista de Tarefas
 function novasTarefas() {
   const descricaoTarefa = textoTarefa.value;
   const novaTarefa = document.createElement('li');
-  novaTarefa.className = 'minhas-listas';
+  novaTarefa.className = 'toras-tarefas';
   novaTarefa.innerText = descricaoTarefa;
-  novaTarefa.addEventListener('click',mudarCor);
+  novaTarefa.addEventListener('dblclick', tarefaCompleada);
+  novaTarefa.addEventListener('click', mudarCor);
   listaTarefas.appendChild(novaTarefa);
   textoTarefa.value = '';
   console.log("clicando botão cria tarefa");
@@ -29,21 +26,35 @@ function removeTudo() {
   listaTarefas.innerHTML = '';
   console.log(" clicando apaga tudo");
 }
-
 //Função que Remove as Tarefas já realizadas
 function removeApenasFinalizados() {
-    console.log('remove finalziados');
+  console.log('remove finalziados');
+}
+//Referenciando endereço das li
+const todasTarefas = document.getElementsByClassName('toras-tarefas');
+
+function mudarCor(event) {
+  for (let i = 0; i < todasTarefas.length; i++) {
+    todasTarefas[i].style.backgroundColor = '';
+  }
+  event.target.style.backgroundColor = "rgb(128, 128, 128)";
+  console.log('Clicando Alterar Cor...');
+}
+function tarefaCompleada(event) {
+  let elemento = event.target;
+  if (elemento.classList.contains('completed')) {
+    elemento.classList.remove('completed');
+  } else {
+    elemento.classList.add('completed');
+  }
 }
 
-//Referenciando endereço das il
-const todasTarefas = document.getElementsByClassName('minhas-listas');
-
-function mudarCor(event){
-  for(let i = 0; i <todasTarefas.length; i++) {
-    todasTarefas[i].style.backgroundColor='';
-  }
-   event.target.style.backgroundColor = "rgb(128, 128, 128)";
-
-  console.log('Clicando Alterar Cor...');
+function removeApenasFinalizados(event) {
+  let apagaConcluidos = document.querySelectorAll('.completed');
+  apagaConcluidos.forEach((item)=>{
+    if(item.classList.contains('completed')) {
+      listaTarefas.removeChild(item);
+    }
+  });
 
 }
