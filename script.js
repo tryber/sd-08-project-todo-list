0746c252f02b49ac64392d99660d4fb5adcb71a4
Natanael Neto <nesn.gamedev@gmail.com>
2020-11-12 15:25:38 -0300

    C7 - Oh my God, its all cleared?? Adding the 10th req

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lista = document.getElementById('lista-tarefas');
 const itemInput = document.getElementById('texto-tarefa');
 const buttonAddItem = document.getElementById('criar-tarefa');
+const buttonRemoveAll = document.getElementById('apaga-tudo');
 
 buttonAddItem.addEventListener('click', function () {
   const item = document.createElement('li');
@@ -18,4 +19,8 @@ lista.addEventListener('click', function (e) {
 
 lista.addEventListener('dblclick', function (e) {
   e.target.classList.toggle('completed');
+});
+
+buttonRemoveAll.addEventListener('click', function () {
+  lista.innerHTML = '';
 });
