33c8c0fe39e9296c8ba4bc27a3c4095601ca5ec1
JRGCast <joao.ralph1@gmail.com>
2020-11-11 23:14:28 -0300

    feitas funçoes aleatorias

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
+// Função adiciona task e remove o conteúdo do input (ok!):
 function adicionaTarefa() {
-  let botaoAdd = document.getElementById('criar-tarefa');
-  let input = document.getElementById('texto-tarefa');
-  let taskList = document.getElementById('lista-tarefas');
+  const botaoAdd = document.getElementById('criar-tarefa');
+  const input = document.getElementById('texto-tarefa');
+  const taskList = document.getElementById('lista-tarefas');
   botaoAdd.addEventListener('click', function () {
-    sessionStorage.setItem(input.innerText, input.innerText);
     let task = document.createElement('li');
-    task.innerHTML = sessionStorage.getItem(input.innerText).value;
+    task.innerHTML = input.value;
+    task.className = 'task';
     taskList.appendChild(task);
+    input.value = '';
+    input.style.backgroundColor = 'white';
   });
 }
 adicionaTarefa();
 
-function limpaInput() {
-  let botaoApaga = document.getElementById('apaga-tudo');
-  botaoApaga.addEventListener('click', function () {
-    let taskList = document.getElementById('lista-tarefas');
-    taskList.children = '';
+function highlightTask() {
+  const taskList = document.getElementById('lista-tarefas');
+  taskList.addEventListener('mouseover', function (event) {
+    if ((event.target.className === 'task')) {
+      if (event.target.style.backgroundColor !== 'yellow') {
+        event.target.style.backgroundColor = 'yellow';
+      }
+    }
+  });
+  taskList.addEventListener('mouseout', function (event) {
+    if ((event.target.className === 'task')) {
+      if (event.target.style.backgroundColor === 'yellow') {
+        event.target.style.backgroundColor = 'white';
+      }
+    }
   });
 }
-limpaInput();
+highlightTask();
+
+function moverCima() {
+  const btnUp = document.getElementById('mover-cima');
+  btnUp.addEventListener('click', function () {
+    const taskList = document.querySelector('#lista-tarefas');
+  });
+}
+
+function moverBaixo() {
+  const btnDown = document.getElementById('mover-baixo');
+}
+
+// Funções atoas:
+function removeFirst() {
+  const btnEraseLast = document.getElementById('remover-selecionado');
+  btnEraseLast.addEventListener('click', function () {
+    const taskList = document.querySelector('#lista-tarefas');
+    taskList.removeChild(taskList.firstElementChild);
+  });
+}
+removeFirst();
+
+function highlightInput() {
+  const taskList = document.getElementById('lista-tarefas');
+  const inputPlace = document.getElementById('texto-tarefa');
+  inputPlace.addEventListener('focus', function () {
+    inputPlace.style.backgroundColor = 'yellow';
+  });
+  inputPlace.addEventListener('blur', function () {
+    if (inputPlace.value !== '') {
+      inputPlace.style.backgroundColor = 'yellow';
+    } else {
+      inputPlace.style.backgroundColor = 'white';
+    }
+  });
+}
+highlightInput();
