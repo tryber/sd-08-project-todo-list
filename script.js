a038a8f0e80202743cdf018a7ee458f2b3ecf354
Patrícia <silva.patricia.v@gmail.com>
2020-11-11 23:02:33 -0300

    requisito 8 completo

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function listTask() {
-  const taskButton = document.getElementById("criar-tarefa");
+  let taskButton = document.getElementById("criar-tarefa");
   taskButton.addEventListener("click", createTask);
 
   function createTask() {
-    const inputText = document.querySelector("#texto-tarefa");
-    const olTaks = document.querySelector("#lista-tarefas");
-    const liTask = document.createElement("li");
+    let inputText = document.querySelector("#texto-tarefa");
+    let olTaks = document.querySelector("#lista-tarefas");
+    let liTask = document.createElement("li");
     olTaks.appendChild(liTask);
     liTask.innerText = inputText.value;
     inputText.value = "";
-    liTask.addEventListener("click", function () {
-      liTask.style.backgroundColor = "rgb(128,128,128)";
-    });
+    liTask.addEventListener("click", changeColor);
   }
 }
 listTask();
+
+function changeColor(event) {
+  listItem = document.querySelectorAll("li");
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItem[index].style.backgroundColor = "";
+  }
+  event.target.style.backgroundColor = "rgb(128,128,128)";
+}
