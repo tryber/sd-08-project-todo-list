48a8db890b6355225b6d17c90161c65b8332f619
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2020-11-12 22:12:40 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ function criaInput() {
   input.placeholder = 'item que deseja adicionar';
 }
 criaInput();
+
+function criaListaTarefas() {
+  let lista = document.createElement('ol');
+  document.body.appendChild(lista);
+  lista.id = 'lista-tarefas';
+  let item = document.createElement('li');
+  lista.appendChild(item);
+  item.innerHTML = 'teste';
+}
+criaListaTarefas()
+
+function criaBotao() {
+  // let input = document.getElementById('texto-tarefa');
+  // let lista = document.getElementById('lista-tarefas');
+  let botao = document.createElement('button');
+  document.body.appendChild(botao);
+  botao.id = 'criar-tarefa';
+  botao.innerText = 'Adicionar';
+  // botao.addEventListener('click', function() {
+  //   if (input.valeu.lenght > 0) {
+  //     let novoItem = document.createElement('li');
+  //     novoItem.innerText = input.value;
+  //     lista.appendChild(novoItem);
+  //     input.value = '';
+  //   }
+  // });
+}
+criaBotao();
