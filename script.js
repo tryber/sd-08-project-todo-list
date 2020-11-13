d28f023313143f819516d74498fa557cb857aed0
Nícolas Tozato <ntozato9@gmail.com>
2020-11-13 12:57:01 -0300

    corrige requisito 7

--- script.js
+++ script.js
@@ -7,17 +7,14 @@ function createTasks () {
     let newText = textBox.value;
     listChild.innerText = newText;
     textBox.value = "";
-    listChild.addEventListener('click', function(){
-        listChild.style.backgroundColor = "rgb(128, 128, 128)";
+    listChild.addEventListener('click', function(event){
+        event.target.style.backgroundColor = "rgb(128, 128, 128)";
     })    
 
 }
 
 let button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createTasks);
-
-
-
 
 
 
