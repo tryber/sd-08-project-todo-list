af145f7ac7f62393dba71667523bf92e969d0be0
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-12 21:15:20 -0300

    C09 Requesitos 7 e 8 resolvidos

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Reconhecendo o texto digitado no campo input e armazenando (atualizando)
-const campoInput = document.getElementById('texto-tarefa');
+let campoInput = document.getElementById('texto-tarefa');
+let buttonCriarTarefa = document.getElementById('criar-tarefa');
+let listaTarefasContainer = document.getElementById('lista-tarefas');
+let todasTarefasCriadas = document.getElementsByClassName('tarefa');
 
 campoInput.addEventListener('input', atualizaValor);
 
@@ -7,37 +10,33 @@ function atualizaValor(input) {
 	campoInput.textContent = input.target.value;
 };
 
-// function toggleSelected(event) {
-//     event.target.classList.toggle('selected');
-//     };
-
-function toggleSelected(click) {
-    click.target.classList.toggle('selected');
-    const tarefasContainer = document.querySelectorAll('.tarefa');
-    for (let index = 0; index < tarefasContainer.length; index += 1) {
-        const tarefa = tarefasContainer[index];
-        if (tarefa.classList.contains('selected') === true) {
-            tarefa.classList.remove('selected');
-        } else {
-            click.target.classList.toggle('selected');
-        };
-    };
-};
-
-
 // Cria um elemento 'li' para o input e adiciona o elemento ao pai 'ol' on click
-
-const buttonCriarTarefa = document.getElementById('criar-tarefa');
 
 buttonCriarTarefa.addEventListener('click', criarTarefa);
 
 function criarTarefa() {
-    const tarefaElement = document.createElement('li');
+    let tarefaElement = document.createElement('li');
     tarefaElement.className = 'tarefa';
+    tarefaElement.addEventListener('click', addSelected);
     tarefaElement.textContent = campoInput.textContent;
-    tarefaElement.addEventListener('click', toggleSelected);
-	const listaTarefasContainer = document.getElementById('lista-tarefas');
 	listaTarefasContainer.appendChild(tarefaElement);
 	document.getElementById('texto-tarefa').value='';
 };
+
+// Adiciona classe selected para tarefa e altera cor de fundo
+
+function addSelected(event) {
+    for (let index=0; index < todasTarefasCriadas.length; index += 1) {
+        todasTarefasCriadas[index].classList.remove('selected');
+        todasTarefasCriadas[index].style.backgroundColor = 'white';
+	}
+    event.target.classList.add('selected');
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+    
+
+
+
+
+
 
