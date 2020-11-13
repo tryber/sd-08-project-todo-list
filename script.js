f6544784eab1dde8bfb5985d7500d46d895e46f5
Nícolas Tozato <ntozato9@gmail.com>
2020-11-13 15:34:53 -0300

    requisito 10

--- script.js
+++ script.js
@@ -8,13 +8,24 @@ function createTasks () {
     listChild.innerText = newText;
     textBox.value = "";
     listChild.addEventListener('click', function(event){
+
         event.target.style.backgroundColor = "rgb(128, 128, 128)";
-    })    
+    });
+    
 
 }
+
 
 let button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createTasks);
 
 
+let clearButton = document.getElementById('apaga-tudo');
 
+function clearAll() {
+    let olList = document.getElementById('lista-tarefas');
+    olList.innerText = "";    
+
+}
+
+clearButton.addEventListener('click', clearAll);
