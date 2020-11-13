3573f40639e16c61d88f2e0e082d91ecbca26ef3
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-12 23:05:27 -0300

    C12 Requesito 9 completo

--- script.js
+++ script.js
@@ -18,16 +18,25 @@ function criarTarefa() {
     let tarefaElement = document.createElement('li');
     tarefaElement.className = 'tarefa';
     tarefaElement.addEventListener('click', addSelected);
+    tarefaElement.addEventListener('dblclick', addCompleted);
     tarefaElement.textContent = campoInput.textContent;
 	listaTarefasContainer.appendChild(tarefaElement);
 	document.getElementById('texto-tarefa').value='';
 };
 
 // Adiciona classe selected para tarefa e altera cor de fundo
-function addSelected(event) {
+function addSelected(click) {
     for (let index = 0; index < todasTarefasCriadas.length; index += 1) {
         let tarefa = todasTarefasCriadas[index];
         tarefa.classList.remove('selected');
     };
-    event.target.classList.toggle('selected');
+    click.target.classList.toggle('selected');
+};
+
+function addCompleted(dblclick) {
+    if (dblclick.target.classList.contains('completed')) {
+        dblclick.target.classList.remove('completed');
+    } else {
+        dblclick.target.classList.add('completed');
+    };
 };
