db2c6902af8008c47034ffcaa6fa9e0e86c62f7c
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-11-13 20:50:03 -0300

    Aceitando sugestoes do CR Ediberto Oliveira Obrigado

--- script.js
+++ script.js
@@ -3,28 +3,22 @@ const listaTarefa = document.getElementById('lista-tarefas');
 function salvaStorage() {
   localStorage.setItem('lista-tarefas', listaTarefa.innerHTML);
 }
-
 function carregaStorage() {
   if (localStorage.getItem('lista-tarefas')) {
     const tarefasSalvas = localStorage.getItem('lista-tarefas');
     listaTarefa.innerHTML = tarefasSalvas;
   }
 }
-
 function capturaEvento(elementoHTML, tipoEvento, acaoDoEvento) {
   const elementoPai = document.getElementById(elementoHTML);
   elementoPai.addEventListener(tipoEvento, (element) => acaoDoEvento(element.target));
 }
-
 function itemSelecionado(item) {
   const preSelecionado = document.querySelector('.selected');
-  if (preSelecionado != null) {
-    preSelecionado.classList.remove('selected');
-  }
+  if (preSelecionado != null) preSelecionado.classList.remove('selected');
   item.classList.toggle('selected');
   salvaStorage();
 }
-
 function moverCima() {
   const preSelecionado = document.querySelector('.selected');
   if (preSelecionado !== null && preSelecionado.previousElementSibling) {
@@ -32,7 +26,6 @@ function moverCima() {
     listaTarefa.insertBefore(preSelecionado, preSelecionadoAnterior);
   }
 }
-
 function moverBaixo() {
   const preSelecionado = document.querySelector('.selected');
   if (preSelecionado !== null && preSelecionado.nextElementSibling) {
@@ -40,36 +33,30 @@ function moverBaixo() {
     listaTarefa.insertBefore(preSelecionado, preSelecionadoProximo.nextElementSibling);
   }
 }
-
 function tarefaConcluida(item) {
   item.classList.toggle('completed');
   salvaStorage();
 }
-
 function preencheItem(item, li) {
   li.innerText = item;
   li.className = 'item';
   return li;
 }
-
 function criarItemLista(tarefa) {
   const li = document.createElement('li');
   preencheItem(tarefa, li);
   listaTarefa.appendChild(li);
 }
-
 function criarTarefa() {
   const campoTarefa = document.getElementById('texto-tarefa');
   criarItemLista(campoTarefa.value);
   campoTarefa.value = '';
   salvaStorage();
 }
-
 function apagaTudo() {
   while (listaTarefa.firstChild) listaTarefa.removeChild(listaTarefa.firstChild);
   salvaStorage();
 }
-
 function removerFinalizados() {
   const listaCompletados = document.querySelectorAll('.completed');
   listaCompletados.forEach((elemento, index) => {
@@ -77,7 +64,6 @@ function removerFinalizados() {
   });
   salvaStorage();
 }
-
 function removeSelecionado() {
   const tarefaSelecionda = document.querySelector('.selected');
   (tarefaSelecionda.parentNode).removeChild(tarefaSelecionda);
