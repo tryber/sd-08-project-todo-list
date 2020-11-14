fcfa1aa473347fceba4d6bee21401f6723910771
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-14 15:39:34 -0300

    adding requisit nine

--- script.js
+++ script.js
@@ -9,17 +9,32 @@ function createTasks() {
     let newText = boxText.value;
     listChilds.innerText = newText;
     boxText.value = "";
-    listChilds.addEventListener("click",changeColor);
-    });
-  }
+    listChilds.addEventListener("click", changeColor);
+    listChilds.addEventListener("dblclick",captureInput);
+  });
+}
 createTasks();
-
-function changeColor(event){
-    listItem = document.querySelectorAll("li");
-    for(let i=0;i < listItem.length; i+=1){
-        listItem[i].style.backgroundColor = "";
-    }
-    event.target.style.backgroundColor = "rgb(128,128,128)";
+//change colors in the list item of tasks
+function changeColor(event) {
+  listItem = document.querySelectorAll("li");
+  for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].style.backgroundColor = "";
+  }
+  event.target.style.backgroundColor = "rgb(128,128,128)";
 }
 
+//by double clicking on an item where it will be scratched
 
+// function elementScratched() {
+//   let itemScratched = document.querySelectorAll("li");
+//   itemScratched.addEventListener("dblclick",function(){
+//     for(let i=0;i < itemScratched.length; i += 1){
+//       itemScratched[i].className ="completed"; 
+//       itemScratched[i].style.textDecoration = "line-through solid rgb(0, 0, 0)";
+//     }
+//   });
+// }
+// elementScratched();
+function captureInput(event){
+  event.target.classList.toggle("completed");
+}
