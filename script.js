c8befbb05a25babe8c0d82a292e79ff7748573bc
vivianeflowt <vivianeflowt@gmail.com>
2020-11-14 14:25:39 -0300

    update

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
+const settings = {
+  selected: null
+};
+
 // Criar Tarefa
 document.getElementById('criar-tarefa').addEventListener('click', () => {
   const taskText = document.getElementById('texto-tarefa').value || '';
 
   if (taskText.trim() !== '') {
-    setTimeout(() => {
-      document.getElementById('texto-tarefa').value = '';
-    }, 100);
-    setTimeout(() => {
-      document.getElementById(
-        'lista-tarefas'
-      ).innerHTML += `<li class='task'>${taskText.trim()} </li>`;
-    }, 200);
+    document.getElementById('texto-tarefa').value = '';
+
+    document.getElementById(
+      'lista-tarefas'
+    ).innerHTML += `<li class='task'>${taskText.trim()} </li>`;
   }
 });
 
 // Remover Todas
 document.getElementById('apaga-tudo').addEventListener('click', () => {
-  setTimeout(() => {
-    document.getElementById('lista-tarefas').innerHTML = '';
-  }, 200);
+  document.getElementById('lista-tarefas').innerHTML = '';
 });
 
 // Seleciona
 document.getElementById('lista-tarefas').addEventListener('click', () => {
-  if (event.target.classList.contains('task')) {
+  if (event.target.classList.contains('selected')) {
+    event.target.classList.remove('selected');
+    settings.selected = null;
+  } else if (event.target.classList.contains('task')) {
     document.querySelectorAll('.task').forEach((element) => {
       element.classList.remove('selected');
     });
     event.target.classList.add('selected');
+    settings.selected = event.target;
+  }
+});
+
+// Remove Selecionado
+document.getElementById('remover-selecionado').addEventListener('click', () => {
+  if (settings.selected !== null) {
+    settings.selected.remove();
+    settings.selected = null;
   }
 });
