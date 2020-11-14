1c9226cec446eb98e1ec6d15f97194a97095dbe0
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-14 15:07:11 -0300

    requisito 7

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const newTaskContainer = document.querySelector('#lista-tarefas')
 
 function newTask () {
     const createList = document.createElement('li');
-    createList.innerText = caixaTexto.value
-    newTaskContainer.appendChild(createList)
-
-
+    createList.className = 'lista';
+    createList.addEventListener('click', paintTask);
+    createList.addEventListener('dblclick', taskComplete)
+    createList.innerText = caixaTexto.value;
+    newTaskContainer.appendChild(createList);
 }
 
 eventButton.addEventListener('click', newTask)
+
+const colorItem = document.querySelectorAll('.lista');
+// colorItem.addEventListener('click', paintTask);
+
+function paintTask(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)'
+    
+}
+
+function taskComplete(event) {
+    let alemento = event.target
+
+}
