2f77bae1923f30bfca61916f65f16721447035f0
Daniel Leon <daniel.radialista@gmail.com>
2020-11-14 15:39:02 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // fiz o código com a ajuda do colega Leandro Piasseta
 
-function createList() {
+function createList() {                                      // criada função para gerar lista (li)
     let button = document.getElementById('criar-tarefa');
     button.addEventListener('click', function () {
-        let text = document.getElementById('texto-tarefa');
-        let listItems = document.getElementById('lista-tarefas');
-        let listChildrenElement = document.createElement('li');
-        listItems.appendChild(listChildrenElement);
-        let newText = text.value;
+        let text = document.getElementById('texto-tarefa');     // criada variável para pegar o id "texto-tarefa"
+        let listItems = document.getElementById('lista-tarefas'); // variável criada pra pegar o id "lista-tarefas" (ol)
+        let listChildrenElement = document.createElement('li');    // criado os elementos "LI"
+        listItems.appendChild(listChildrenElement);                // os elementos "li" são filhas de "ol"
+        let newText = text.value;                                  // o texto criado irá receber o valor
         listChildrenElement.innerText = newText;
         text.value = "";
         listChildrenElement.addEventListener('click', elementColor);
+        listChildrenElement.addEventListener('dblclick', capturarClick);
 
     });
 }
@@ -24,4 +25,11 @@ function elementColor(event) {
         itemsList[i].style.backgroundColor = '';
     }
     event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+
+
+
+function capturarClick(event) {
+    event.target.classList.toggle('completed');
 }
