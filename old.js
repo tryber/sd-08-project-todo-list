d98568e4aff72c3a5ea9b7f3a271333fc4e42fb2
vivianeflowt <vivianeflowt@gmail.com>
2020-11-14 14:41:55 -0300

    aaaa

---
+++ old.js
@@ -0,0 +1,73 @@
+const initColorPalette = () => {
+  const cores = [
+    '#000000',
+    `#${Math.random().toString(16).substr(2, 6)}`,
+    `#${Math.random().toString(16).substr(2, 6)}`,
+    `#${Math.random().toString(16).substr(2, 6)}`
+  ];
+  document.querySelectorAll('.color').forEach((element) => {
+    element.style.backgroundColor = cores[0];
+    cores.shift();
+  });
+};
+
+initColorPalette();
+
+const config = {
+  color: '#000000'
+};
+
+document.getElementById('color-palette').addEventListener('click', () => {
+  console.log(event.target);
+  if (event.target.classList.contains('color')) {
+    document.querySelectorAll('.color').forEach((element) => {
+      element.classList.remove('selected');
+    });
+    event.target.classList.add('selected');
+    config.color = event.target.style.backgroundColor;
+    console.log(event.target);
+  }
+});
+
+document.getElementById('pixel-board').addEventListener('click', () => {
+  if (event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = config.color;
+  }
+});
+
+document.getElementById('clear-board').addEventListener('click', () => {
+  document.querySelectorAll('.pixel').forEach((element) => {
+    element.style.backgroundColor = 'white';
+  });
+});
+
+document.getElementById('generate-board').addEventListener('click', () => {
+  const size = document.getElementById('board-size').value;
+  if (size.toString().trim() === '') {
+    alert('Board inválido!');
+  } else {
+    boardBuilder(size);
+  }
+});
+
+const boardBuilder = (size = 5) => {
+  if (size < 5) {
+    size = 5;
+  }
+  if (size > 50) {
+    size = 50;
+  }
+  const board = document.getElementById('pixel-board');
+
+  board.innerHTML = '';
+
+  for (let i = 0; i < size; i++) {
+    board.innerHTML += '<div class=\'pixel-row\'>';
+    for (let j = 0; j < size; j++) {
+      board.innerHTML += '<div class="pixel"></div>';
+    }
+    board.innerHTML += '<div>';
+  }
+};
+
+boardBuilder(5);
--- script.js
+++ script.js
@@ -34,6 +34,17 @@ document.getElementById('lista-tarefas').addEventListener('click', () => {
   }
 });
 
+// executado
+document.getElementById('lista-tarefas').addEventListener('dblclick', () => {
+  if (event.target.classList.contains('task')) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }
+});
+
 // Remove Selecionado
 document.getElementById('remover-selecionado').addEventListener('click', () => {
   if (settings.selected !== null) {
@@ -41,3 +52,80 @@ document.getElementById('remover-selecionado').addEventListener('click', () => {
     settings.selected = null;
   }
 });
+
+// Remove Finalizado
+document.getElementById('remover-finalizados').addEventListener('click', () => {
+  document.querySelectorAll('.completed').forEach((element) => {
+    element.remove();
+  });
+});
+
+/*
+
+function saveList() {
+  btnSave = document.querySelector('#salvar-tarefas');
+  btnSave.addEventListener('click', () => {
+    clearListLocalStorage();
+    const lista = document.querySelector('#lista-tarefas');
+    for (let i = 1; i < lista.children.length + 1; i += 1) {
+      const key = `Item${i}`;
+      const value = lista.children[i - 1].outerHTML;
+      localStorage.setItem(key, value);
+    }
+  });
+}
+
+function loadList() {
+  const lista = document.querySelector('#lista-tarefas');
+  let itemAdd = 'Item1';
+  let indexAdd = 1;
+  while (localStorage.getItem(itemAdd)) {
+    const storage = localStorage.getItem(itemAdd);
+    lista.innerHTML += storage;
+    indexAdd += 1;
+    itemAdd = `Item${indexAdd}`;
+  }
+}
+function clearListLocalStorage() {
+  let itemRemove = 'Item1';
+  let indexRemove = 1;
+  while (localStorage.getItem(itemRemove)) {
+    localStorage.removeItem(itemRemove);
+    indexRemove += 1;
+    itemRemove = `Item${indexRemove}`;
+  }
+}
+
+function moveUp() {
+  const lista = document.querySelector('#lista-tarefas');
+  const btnMoveUp = document.querySelector('#mover-cima');
+  btnMoveUp.addEventListener('click', () => {
+    if (document.querySelector('.selected')) {
+      const itemSelected = document.querySelectorAll('.selected');
+      if (itemSelected[0].previousElementSibling !== null) {
+        const previousItem = itemSelected[0].previousElementSibling.outerHTML;
+        itemSelected[0].previousElementSibling.outerHTML = itemSelected[0].outerHTML;
+        itemSelected[0].outerHTML = previousItem;
+      }
+    }
+  });
+}
+moveUp();
+
+function moveDown() {
+  const lista = document.querySelector('#lista-tarefas');
+  const btnMoveDown = document.querySelector('#mover-baixo');
+  btnMoveDown.addEventListener('click', () => {
+    if (document.querySelector('.selected')) {
+      const itemSelected = document.querySelectorAll('.selected');
+      if (itemSelected[0].nextElementSibling !== null) {
+        const nextItem = itemSelected[0].nextElementSibling.outerHTML;
+        itemSelected[0].nextElementSibling.outerHTML = itemSelected[0].outerHTML;
+        itemSelected[0].outerHTML = nextItem;
+      }
+    }
+  });
+}
+moveDown();
+
+*/
