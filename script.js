92f296d949f414a8a5a3b315122ba6a7482f8a2f
vivianeflowt <vivianeflowt@gmail.com>
2020-11-14 14:11:04 -0300

    interface

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Criar Tarefa
 document.getElementById('criar-tarefa').addEventListener('click', () => {
-  document.getElementById('lista-tarefas').innerHTML += `<li class='task'>${
-    document.getElementById('texto-tarefa').value
-  } </li>`;
+  const taskText = document.getElementById('texto-tarefa').value;
   document.getElementById('texto-tarefa').value = '';
+  document.getElementById('lista-tarefas').innerHTML += `<li class='task'>${taskText.trim()} </li>`;
 });
 
 // Remover Todas
 document.getElementById('apaga-tudo').addEventListener('click', () => {
   document.getElementById('lista-tarefas').innerHTML = '';
+});
+
+// Seleciona
+document.getElementById('lista-tarefas').addEventListener('click', () => {
+  if (event.target.classList.contains('task')) {
+    document.querySelectorAll('.task').forEach((element) => {
+      element.classList.remove('selected');
+    });
+    event.target.classList.add('selected');
+  }
 });
