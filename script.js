aaca49dbb2b4711d61d3fd90e0e6346384f1b3fb
Marcela <marcela9908@gmail.com>
2020-11-14 18:00:15 -0300

    Adiociona funçao nova tarefa

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let botao = document.querySelector("#criar-tarefa");
+//Referenciar
+let botaoCriarTarefas = document.querySelector("#criar-tarefa");
+let botaoApagarTudo = document.querySelector("#apaga-tudo");
+let botaoRevFinalizados = document.querySelector("#remover-finalizados");
+let listaOrdenada = document.querySelector("#lista-tarefas");
+let entradaTarefa = document.querySelector("#texto-tarefa");
+
+botaoCriarTarefas.addEventListener("click", criaTarefas);
+botaoApagarTudo.addEventListener("click", apagaTudo);
+botaoRevFinalizados.addEventListener("click", revFinalizados);
+
+function criaTarefas() {
+  const novaTarefa = document.createElement("li"); //criando uma tag
+  listaOrdenada.appendChild(novaTarefa); //Local onde ela vai ser armazenada
+  novaTarefa.innerText = entradaTarefa.value; //value é o valor que o input recebe
+}
+
+function apagaTudo() {
+  console.log("Estou apagando Tudo");
+}
+
+function revFinalizados() {
+  console.log("Estou removendo todas tarefas");
+}
