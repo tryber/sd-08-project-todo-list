a95df1a463dce535dd80cf9dbec367cd68fced0c
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-15 12:26:41 -0300

    adiciona requisito 9 e modifica anteriores

--- script.js
+++ script.js
@@ -5,33 +5,34 @@ const listaDeItens = document.getElementsByTagName('li');
 const botaoApagaTudo = document.getElementById('apaga-tudo');
 
 botaoAdiciona.addEventListener('click', functionCriaTarefa);
-
 function functionCriaTarefa() {
     const listaDeItens = document.createElement('li');
     listaDeItens.innerText = caixaDeTexto.value;
     listaOrdenada.appendChild(listaDeItens);
     caixaDeTexto.value = '';
+    listaDeItens.addEventListener('click', corFundoClicado);
     listaDeItens.addEventListener('click', removeSelection);
-    //listaDeItens.addEventListener('click', functionApagaTarefa);
+    listaDeItens.addEventListener("dblclick", tarefaCompleta);
 }
-
-function corFundoClicado() {
+function corFundoClicado(event) {
     const color = 'rgb(128, 128, 128)';
-    listaOrdenada.addEventListener('click', (event) => {
         event.target.style.backgroundColor = color;
-        console.log(event.target);
-    });
 }
-corFundoClicado();
-
 function removeSelection(event) {
-    listaDeItem = document.getElementsByTagName('li');
+    let listaDeItem = document.getElementsByTagName('li');
     for (let index = 0; index < listaDeItem.length; index += 1) {
         listaDeItem[index].style.backgroundColor = '';
     }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
-
+function tarefaCompleta(event) {
+    let elemento = event.target;
+    if (elemento.classList.contains("completed")) {
+      event.target.classList.remove("completed");
+    } else {
+      elemento.classList.add("completed");
+    }
+}
 botaoApagaTudo.addEventListener('click', functionApagaTarefa);
 function functionApagaTarefa() {
     listaOrdenada.innerHTML = '';
