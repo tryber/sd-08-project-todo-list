92d209fdfc973dd8bff1f4d0f54c052c0ff92f15
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-14 21:44:26 -0300

    ajustes requisito 9

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const caixaTexto = document.querySelector('#texto-tarefa');
-const eventButton = document.querySelector('#criar-tarefa')
-const newTaskContainer = document.querySelector('#lista-tarefas')
+const eventButton = document.querySelector('#criar-tarefa');
+const newTaskContainer = document.querySelector('#lista-tarefas');
 
-function newTask () {
+function paintTask(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)'
+}
+
+function taskComplete(event) {
+    const elemento = event.target;
+    if (elemento.classList.contains('completed')) {
+        elemento.classList.remove('completed');
+    } else {
+        elemento.classList.add('completed');
+    }
+
+}
+
+function newTask() {
     const createList = document.createElement('li');
     createList.className = 'lista';
     createList.addEventListener('click', paintTask);
@@ -10,23 +24,6 @@ function newTask () {
     createList.innerText = caixaTexto.value;
     newTaskContainer.appendChild(createList);
     document.getElementById('texto-tarefa').value = '';
-    console.log(createList)
 }
 
-eventButton.addEventListener('click', newTask)
-
-const colorItem = document.querySelectorAll('.lista');
-
-function paintTask(event) {
-    event.target.style.backgroundColor = 'rgb(128,128,128)'   
-}
-
- function taskComplete(event) {
-    const elemento = event.target;
-    if (elemento.classList.contains('completed')) {
-        elemento.classList.remove('completed')
-    } else {
-        elemento.classList.add('completed')
-    }
-
-}
+eventButton.addEventListener('click', newTask);
