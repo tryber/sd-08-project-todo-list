7b05a37bc297c227b55d26c25114fe0ad6e422fb
vivianeflowt <vivianeflowt@gmail.com>
2020-11-15 11:31:50 -0300

    injection

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-require('./script');
+require('./original');
+
+// #
+console.log('Injection Works');
+// #
 
 
-console.log('debug');
+// (function(){
+//   console.log('Hello World!');
+//   setTimeout(() => {
+//       console.log('2');
+//         document.querySelectorAll('.task').forEach((element) => {
+//         element.classList.add('bug')
+//     });
+//   },2000)
+// })();
 
-},{"./script":2}],2:[function(require,module,exports){
+
+(function(){
+  setTimeout(() => {
+    console.log('Hello World!');
+  },2000)
+})();
+
+},{"./original":2}],2:[function(require,module,exports){
 /*
   Bug Bounty - Domingo
   Author: Viviane Florido
