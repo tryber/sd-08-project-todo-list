8db6060d7fd7ce84b0a3a0a6077dcbd3ce0f072d
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-11-15 20:25:12 -0300

    realizado requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //variáveis
 const adicionarItem = document.getElementById('criar-tarefa'); //campo com o valor do item
 const button = document.getElementById('criar-tarefa'); // botao enviar
+const lista = document.getElementById('lista-tarefas'); // lista de tarefas
 
 // nao permite que atualize a page ao click do button
 button.addEventListener('click', function(event) {
@@ -16,13 +17,11 @@ adicionarItem.addEventListener('click', function (event){
 
     ol.appendChild(li);
     li.innerHTML = item.value
-    
     item.value = '';
 })
 
 //altera a cor de fundo do item da lista clicado
-const lista = document.getElementById('lista-tarefas');
-lista.addEventListener('click', function(event) {
+lista.addEventListener('click', function selectItem(event) {
     let listaItens = lista.children; 
 
     for (let indice = 0; indice < listaItens.length; indice += 1) {
@@ -35,5 +34,30 @@ lista.addEventListener('click', function(event) {
         if (listaItens[indice] == event.target) {
             listaItens[indice].className = 'selected'
         }   
+    }
+})
+
+//risca o item finalizado da lista
+lista.addEventListener("dblclick", function completItem(event) {
+    let listaItens = lista.children; 
+    for (let indice = 0; indice < listaItens.length; indice += 1) {
+        //verifica se já existe um selecionado e remove a class 'completed'
+        if (listaItens[indice] == event.target ) {
+            if (listaItens[indice] == event.target && listaItens[indice].className == 'completed') {
+                listaItens[indice].className.remove = 'completed';
+            } else if (listaItens[indice] == event.target ){
+                listaItens[indice].className.includes = 'completed'
+            }
+        }
+
+    }
+})
+
+let reset = document.getElementById("apaga-tudo");
+
+reset.addEventListener('click', function deletAll () {
+    let liElement = document.getElementsByTagName('li');
+    for (key in liElement) {
+        lista.innerHTML = '';
     }
 })
