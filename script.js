bdf5a90e358704b08fdeb7d3c9bd5d9e5df33f37
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-15 17:48:56 -0300

    veifica req7 e req9

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 function taskList(){
     let addButton = document.getElementById("criar-tarefa");
-    addButton.addEventListener.EventListener("click", taskCreator)
+    addButton.addEventListener("click", taskCreator);
 
     function taskCreator(){
         let task = document.querySelector("#texto-tarefa");
         let orderedTask = document.querySelector("#lista-tarefas");
         let listTask = document.createElement("li");
         orderedTask.appendChild(listTask);
-        listTask.innerHTML = task.value;
+        listTask.innerText = task.value;
         task.value = "";
+        listTask.addEventListener("click" , colorChanging);
+        listTask.addEventListener("dbclick", completeTask);
+       
     }
 }
 taskList();
 
+function colorChanging(event){
+    let listItem = document.querySelectorAll("li");
+    for (let index = 0; index < listItem.length; index += 1){
+        listItem[index].style.backgroundColor = "";
+    }
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+}
+
+function completeTask(event){
+    let taskIndex = event.target;
+    if (taskIndex.classList.contains("completed")){
+        event.target.classList.remove("completed");
+    } else {
+        taskIndex.classList.add("completed");
+    }
+}
       
     
 
@@ -21,9 +40,3 @@ taskList();
 
 
 
-/*function createTask(){
-    let tasks = document.getElementById("lista-tarefas");
-    let list = document.createElement("li");
-    task.appendChild(list);
-}
-createTask();*/
