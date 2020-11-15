58aa05fc2a0a74209e411f7273fc847b2240c7ee
Daniel <marcio.daniel@msn.com>
2020-11-15 00:25:13 -0300

    Limpando Codigo

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// Referenciando endereços da lista de atividades e das suas filhas
 const listaTarefas = document.getElementById('lista-tarefas');
 const textoTarefa = document.getElementById('texto-tarefa');
-// Referenciando endereços dos botões
 const criarTarefa = document.getElementById('criar-tarefa');
 const apagaTudo = document.getElementById('apaga-tudo');
 const removerFinalziados = document.getElementById('remover-finalizados');
-// Criando eventos para os botões
 criarTarefa.addEventListener("click", novasTarefas);
 apagaTudo.addEventListener("click", removeTudo);
 removerFinalziados.addEventListener('click', removeApenasFinalizados);
-// Funação que Gerar Novas Lista de Tarefas
 function novasTarefas() {
   const descricaoTarefa = textoTarefa.value;
   const novaTarefa = document.createElement('li');
@@ -21,18 +17,14 @@ function novasTarefas() {
   textoTarefa.value = '';
   console.log("clicando botão cria tarefa");
 }
-//Função que Apaga todas as Tarefas
 function removeTudo() {
   listaTarefas.innerHTML = '';
   console.log(" clicando apaga tudo");
 }
-//Função que Remove as Tarefas já realizadas
 function removeApenasFinalizados() {
   console.log('remove finalziados');
 }
-//Referenciando endereço das li
 const todasTarefas = document.getElementsByClassName('toras-tarefas');
-
 function mudarCor(event) {
   for (let i = 0; i < todasTarefas.length; i++) {
     todasTarefas[i].style.backgroundColor = '';
@@ -55,5 +47,4 @@ function removeApenasFinalizados(event) {
       listaTarefas.removeChild(item);
     }
   });
-
 }
--- teste.js
+++
@@ -1,3 +0,0 @@
-console.log(array.forEach(element => {
-
-});)
