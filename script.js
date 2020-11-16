27d0319d5802aae20028544c15ba2b5ff9edb77f
Blucilha <blucilha.nascimento@gmail.com>
2020-11-15 23:21:22 -0300

    Adiciona lista

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ let adicionado = document.getElementById("lista-tarefas");
 let btnAdiciona = document.querySelector('#criar-tarefa');
 let listagem = document.querySelector('#lista-tarefas');
 
-btnAdiciona.addEventListener('click', function(){
-    let crialista = document.createElement('li');
-    crialista.innerText = caixaTexto.value + ';';
-    listagem.appendChild(crialista);
-    caixaTexto.value= " ";
-})
+function adicionar() {
+    let item = document.createElement('li');
+    let inserirValor= document.principal.texto.value;
+    let inserirTexto=document.createTextNode(inserirValor);
+  
+    item.appendChild(inserirTexto);
+    listagem.appendChild(item);
+    document.principal.texto.value= "";
+  
+    //createCloseButton(li);
+  }
