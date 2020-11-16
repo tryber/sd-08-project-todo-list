514d52b43451fee0fa0dd714c6ce7f86d44a3715
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-16 11:57:31 -0400

    Remove selecionado e salva e recupera dados

--- script.js
+++ script.js
@@ -35,8 +35,48 @@ window.onload = function () {
     for (let index = 0; index < item.length; index += 1) {
       lista.removeChild(item[index]);
     }
-    
   })
+  // Remove tarefas finalizadas
+  document.getElementById('remover-finalizados').addEventListener('click', function () {
+    let item = document.querySelectorAll('.item-list');
+    for (let index = 0; index < item.length; index += 1) {
+      if (item[index].classList.contains('completed')) {
+        lista.removeChild(item[index]);
+      }
+    }
+  })
+  // Remove selecionado
+  document.getElementById('remover-selecionado').addEventListener('click', function () {
+    let item = document.querySelectorAll('.item-list');
+    for (let index = 0; index < item.length; index += 1) {
+      if (item[index].classList.contains('selected')) {
+        lista.removeChild(item[index]);
+      }
+    }
+  })
+  // Salva tarefas
+  
+  document.getElementById('salvar-tarefas').addEventListener('click', function () {
+    localStorage.clear();
+    tarefasArray = []
+    let getItemList = document.querySelectorAll('.item-list');
+    for (let index = 0; index < getItemList.length; index += 1) {
+      let itemTarefaLista = {
+        item: getItemList[index].innerText,
+        class: getItemList[index].className
+      }
+      tarefasArray.push(itemTarefaLista)
+    }
+    localStorage.setItem('tarefa', JSON.stringify(tarefasArray));
+  })
+  // Recupera tarefas após atualizar a aba ou abrir outra
+  let tarefaSalva = JSON.parse(localStorage.getItem('tarefa'));
+  for (let index = 0; index < tarefaSalva.length; index += 1){
+    let tarefa = document.createElement('li');
+    tarefa.className = tarefaSalva[index].class;
+    tarefa.innerText = tarefaSalva[index].item;
+    lista.appendChild(tarefa);
+  }
 };
 
   
