8da855f306509853e7410acb0d0a9b935f475eed
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-15 23:10:09 -0300

    updated script

--- script.js
+++ script.js
@@ -6,39 +6,39 @@ const getTaskList = document.getElementsByClassName('task-list');
 const getInput = document.getElementById('texto-tarefa');
 
 function createTaskList() {
-  const createList = document.createElement('li');
-  createList.innerText = getInput.value;
-  createList.className = 'task-list';
-  createList.addEventListener('dblclick', toggleCompleted);
-  createList.addEventListener('click', toggleSelected);
-  return createList;
+	const createList = document.createElement('li');
+	createList.innerText = getInput.value;
+	createList.className = 'task-list';
+	createList.addEventListener('dblclick', toggleCompleted);
+	createList.addEventListener('click', toggleSelected);
+	return createList;
 }
 
 getButtonCreateTask.addEventListener('click', () => {
-  const li = createTaskList();
-  getOrderedList.appendChild(li);
-  getInput.value = '';
+	const li = createTaskList();
+	getOrderedList.appendChild(li);
+	getInput.value = '';
 });
 
 function toggleSelected(event) {
-  for (let index = 0; index < getTaskList.length; index += 1) {
-    getTaskList[index].classList.remove('selected');
-    event.target.classList.add('selected');
-    getTaskList[index] = event.target;
-  }
+	for (let index = 0; index < getTaskList.length; index += 1) {
+		getTaskList[index].classList.remove('selected');
+		event.target.classList.add('selected');
+		getTaskList[index] = event.target;
+	}
 }
 
 function toggleCompleted(event) {
-  event.target.classList.toggle('completed');
+	event.target.classList.toggle('completed');
 }
 
 getButtonClearAll.addEventListener('click', () => {
-  getOrderedList.innerHTML = '';
+	getOrderedList.innerHTML = '';
 });
 
 getButtonRemoveFinished.addEventListener('click', () => {
-  let completedTask = document.getElementsByClassName('completed');
-  for (let index = completedTask.length - 1; index >= 0; index -= 1) {
-    completedTask[index].remove();
-  }
+	let completedTask = document.getElementsByClassName('completed');
+	for (let index = completedTask.length - 1; index >= 0; index -= 1) {
+		completedTask[index].remove();
+	}
 });
