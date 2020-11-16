16cdb97afec347d200b604d3dd12da0e6a17f3b9
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-16 19:26:15 -0300

    verifica req5 novamente corrigindo o src do script

--- script.js
+++ script.js
@@ -6,32 +6,103 @@ function taskList(){
         let task = document.querySelector("#texto-tarefa");
         let orderedTask = document.querySelector("#lista-tarefas");
         let listTask = document.createElement("li");
-        orderedTask.appendChild(listTask);
         listTask.innerText = task.value;
-        task.value = "";
-        listTask.addEventListener("click" , colorChanging);
-        listTask.addEventListener("dbclick", completeTask);
+        orderedTask.appendChild(listTask);
+        task.value = "";  
        
-    }
+    }  
 }
 taskList();
 
-function colorChanging(event){
-    let listItem = document.querySelectorAll("li");
-    for (let index = 0; index < listItem.length; index += 1){
-        listItem[index].style.backgroundColor = "";
-    }
-    event.target.style.backgroundColor = "rgb(128,128,128)";
-}
 
-function completeTask(event){
-    let taskIndex = event.target;
-    if (taskIndex.classList.contains("completed")){
-        event.target.classList.remove("completed");
-    } else {
-        taskIndex.classList.add("completed");
-    }
-}
+
+
+
+
+
+
+
+/*listTask.addEventListener("click" , colorChanging); listTask.addEventListener("dbclick", completeTask); 
+
+function colorChanging(event){ 
+    let listItem = document.querySelectorAll("li"); 
+    for (let index = 0; index < listItem.length; index += 1){ listItem[index].style.backgroundColor = ""; 
+} 
+    event.target.style.backgroundColor = "rgb(128,128,128)"; 
+} 
+    
+function completeTask(event){ 
+    let taskIndex = event.target; 
+    if (taskIndex.classList.contains("completed")){ 
+        event.target.classList.remove("completed"); 
+    } else { taskIndex.classList.add("completed"); 
+} 
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       
     
 
