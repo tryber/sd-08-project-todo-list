9f112680871350f7acdd7386930338925da16c36
Bruna <92brunacampos@gmail.com>
2020-11-17 16:15:10 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function taskCreator() {
+function criaTarefa() {
   let listTask = document.createElement("li");
   let orderedTask = document.querySelector("#lista-tarefas");
   let task = document.querySelector("#texto-tarefa");
@@ -8,4 +8,28 @@ function taskCreator() {
   task.value = "";
 }
 let addButton = document.getElementById("criar-tarefa");
-addButton.addEventListener("click", taskCreator);
+addButton.addEventListener("click", criaTarefa);
+
+
+
+
+function alterarBackground(e) {
+    let lista = document.querySelectorAll('li');
+    if (e.target.className !== 'selecionado') {
+        for (let index = 0; index < lista.length; index++) {
+            lista[index].classList.remove('selecionado');
+        }
+        e.target.className = 'selecionado';
+    } else if (e.target.className === 'selecionado') {
+        e.target.classList.remove('selecionado');
+    }
+   
+}
+
+let listaTarefa = document.querySelector('#lista-tarefas');
+listaTarefa.addEventListener('click',alterarBackground);
+
+
+
+//let adcEstilo = document.querySelectorAll("li");
+//adcEstilo.addEventListener("click", )
