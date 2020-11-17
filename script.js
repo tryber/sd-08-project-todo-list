efcb84a832f74d32f597cff9cf2d7a78b1594c54
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-17 19:05:38 -0300

    verifica req8: selecionando tarefas individualmente

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ function taskList(){
         let task = document.querySelector("#texto-tarefa");
         let orderedTask = document.querySelector("#lista-tarefas");
         let listTask = document.createElement("li");
-        listTask.innerText = task.value;
         orderedTask.appendChild(listTask);
-        task.value = "";  
-       
+        listTask.innerText = task.value;
+        task.value = "";
+        listTask.addEventListener("click", backgroundColorChanging);
+        
     }  
 }
 taskList();
+  
+function backgroundColorChanging(event) {
+    let taskItem = document.querySelectorAll("li");
+    for ( let index = 0; index < taskItem.length; index += 1) {
+        taskItem[index].style.backgroundColor = "";
+    }
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+}
+backgroundColorChanging();
 
 
 
@@ -20,24 +30,6 @@ taskList();
 
 
 
-
-
-/*listTask.addEventListener("click" , colorChanging); listTask.addEventListener("dbclick", completeTask); 
-
-function colorChanging(event){ 
-    let listItem = document.querySelectorAll("li"); 
-    for (let index = 0; index < listItem.length; index += 1){ listItem[index].style.backgroundColor = ""; 
-} 
-    event.target.style.backgroundColor = "rgb(128,128,128)"; 
-} 
-    
-function completeTask(event){ 
-    let taskIndex = event.target; 
-    if (taskIndex.classList.contains("completed")){ 
-        event.target.classList.remove("completed"); 
-    } else { taskIndex.classList.add("completed"); 
-} 
-}*/
 
 
 
