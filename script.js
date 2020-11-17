773cbd40b0a7a641761eafe698b160c53ea7dc86
Pedro Alves <ped.alm2709@gmail.com>
2020-11-17 12:59:38 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let createTask = document.getElementById('criar-tarefa'); // elemento button
-let inputText = document.getElementById('texto-tarefa'); // elemento input
+let createTask = document.getElementById('criar-tarefa'); //elemento button criador de tarefas
+let clearAll = document.getElementById('apaga-tudo'); //elemento button para apagar todas as tarefas
+let removeTask = document.getElementById('remover-finalizados'); //elemento button que remove tarefas completas
+let inputText = document.getElementById('texto-tarefa'); //elemento input
 let taskList = document.getElementById('lista-tarefas'); //elemento ol
 
 createTask.addEventListener('click', ()=> {
@@ -7,4 +9,24 @@ createTask.addEventListener('click', ()=> {
   newTask.innerHTML = inputText.value;
   taskList.appendChild(newTask);
   inputText.value = '';
+  newTask.addEventListener('click', ()=> {
+    let allTasks = document.getElementsByTagName('li');
+    for (let index=0; index<allTasks.length; index++) {
+      allTasks[index].classList.remove('oneClickselect');
+    }
+    newTask.classList.add('oneClickselect');
+  })
+  newTask.addEventListener('dblclick', ()=> {
+    if (newTask.classList.contains('completed')) {
+      newTask.classList.remove('completed');
+    } else {
+    newTask.classList.add('completed');
+    }
+  })
 })
+
+
+
+
+
+
