401cf8e1cd9d69bbafcb788a6c58c6b6b1bc7d93
Rafael <rafaeljavaro750@gmail.com>
2020-11-17 15:22:54 -0300

    Cria funcao criaNovaTarefa - problema: Tarefa esta sendo criada porem, nao estaficando gravada na lista

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function criaLista() {
-    const novaTarefa = document.createElement('li');
-    const lista = document.getElementById('lista-tarefa');
-    lista.appendChild(novaTarefa);
+function criaNovaTarefa() {
+  const botaoCriaTarefa = document.querySelector('#criar-tarefa');
+  const lista = document.querySelector('ol');
+  botaoCriaTarefa.addEventListener('click', () => {
+    const novaTarefa = document.querySelector('input').value;
+    const novaTarefaCriada = document.createElement('li');
+    novaTarefaCriada.innerHTML = novaTarefa;
+    lista.appendChild(novaTarefaCriada);
+  });
 }
+criaNovaTarefa();
+
+ 
