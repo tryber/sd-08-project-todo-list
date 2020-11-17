52bb645a641980dac9f45b29349db04263708388
Bruna <92brunacampos@gmail.com>
2020-11-17 15:24:07 -0300

    cria tarefas ordenadas

---
+++ script.js
@@ -0,0 +1,18 @@
+function criarTarefa() {
+    let addButton = document.getElementById("criar-tarefa");
+    addButton.addEventListener("click", taskCreator);
+
+    function taskCreator(){
+        let task = document.querySelector("#texto-tarefa");
+        let orderedTask = document.querySelector("#lista-tarefas");
+        let listTask = document.createElement("li");
+        listTask.innerText = task.value;
+        orderedTask.appendChild(listTask);
+        task.value = "";
+
+    }
+
+
+
+}
+criarTarefa();
