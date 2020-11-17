7d870e7d940c555b9e357ac5fba821a11c797837
Pedro Piquini <piquini.pedro@gmail.com>
2020-11-17 12:59:44 -0300

    adiconado botões que movimentam item seleiconar além de melhorias de usabilidade

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ document.addEventListener('click', function (event) {
     if (event.target.id === 'criar-tarefa') {
         const list = document.getElementById('lista-tarefas');
         let taskText = document.getElementById('texto-tarefa');
-        let createList = document.createElement('li');
-        createList.className = 'task';
-        createList.innerHTML = taskText.value;
-        list.appendChild(createList);
-        taskText.value = '';
+        if (taskText.value != '') {
+            let createList = document.createElement('li');
+            createList.className = 'task';
+            createList.innerHTML = taskText.value;
+            list.appendChild(createList);
+            taskText.value = '';
+        };
     } else if (event.target.classList.contains('task')) {
         let removeSelect = document.getElementsByClassName('selected');
         for (let i = 0; i < removeSelect.length; i += 1) {
@@ -24,14 +26,42 @@ document.addEventListener('click', function (event) {
         for (let j = 0; j < removeCompleted.length;) {
             removeCompleted[0].remove();
         };
+    } else if (event.target.id === 'mover-cima') {
+        let selected = document.getElementsByClassName('selected')[0];
+        let previousSibling = document.getElementsByClassName('selected')[0].previousSibling;
+        document.getElementById('lista-tarefas').insertBefore(selected, previousSibling);
+    } else if (event.target.id === 'mover-baixo') {
+        let selected = document.getElementsByClassName('selected')[0];
+        let nextSibling = document.getElementsByClassName('selected')[0].nextSibling;
+        if (nextSibling != null) {
+            document.getElementById('lista-tarefas').insertBefore(nextSibling, selected);
+        };
     }
 }, false);
 
 document.addEventListener('dblclick', function (event) {
     if (event.target.classList.contains('completed')) {
         event.target.classList.remove('completed');
-    } else {
+    } else if (event.target.classList.contains('task')) {
         event.target.classList.add('completed');
     };
 }, false);
 
+// Fonte do código abaixo: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key#Example
+// Pesquisei esse código abaixo para conseguir adicionar itens na lista apertando a tecla "Enter".
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+  
+    switch (event.code) {
+      case "Enter":
+        document.getElementById('criar-tarefa').click();
+        break;
+    default:
+        return; // Quit when this doesn't handle the key event.      
+    }
+  
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  }, true);
