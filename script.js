196b69d083d4212a12e04a92ea930f00c10706d1
mateusvictor7 <mateussantiago.victor@gmail.com>
2020-11-17 16:52:48 -0300

    adicionando os botoes

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-//Requisito 2
-document.getElementById("funcionamento").innerText = "Clique duas vezes em um item para marcá-lo como completo";
+/* //Requisito 1
+function createTitle(){
+    let header = document.getElementsByTagName("header");
+    let title = document.createElement("h1");
+    title.innerText = "Minha Lista de Tarefas";
+    header.appendChild(h1);
+}
+createTitle();
 
-//Requisito 3
+//Requisito 2
+function createParagraph (){
+let funcionamento = document.getElementById("funcionamento");
+funcionamento.innerText = "Clique duas vezes em um item para marcá-lo como completo";
+}
+createParagraph(); */
+
+/* //Requisito 3
 function createOl (){
     let lista = document.getElementById("texto-tarefa");
     let listaOrdenada = document.createElement("ol");
     listaOrdenada.getElementById("lista-tarefas");
     lista.appendChild(listaOrdenada);
 }
-createOl();
+createOl(); */
 
 //Requisito 4
 function createLi (){
@@ -25,3 +38,6 @@ function createLi (){
     }
 }
 createLi ();
+
+//Requisito 5
+
