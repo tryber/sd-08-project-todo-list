6471c2ede11ec89bcb49c9003239e75b24727449
Lucas Duque <lucas3rios2014@gmail.com>
2020-11-17 20:33:17 -0300

    Requisitos 6 e 7.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+function clearSelection() {
+  const selected = document.getElementsByClassName('selected');
+  selected.className = '';
+  selected.style.backgroundColor = 'white';
+}
+
+function itemSelect(event) {
+  if (event.target.style.backgroundColor === 'rgb(128, 128, 128)') {
+    event.target.className = null;
+    event.target.style.backgroundColor = 'white';
+  }
+  else {
+    event.target.className = 'selected';
+  }
+}
+
 function criarTarefa() {
   const input = document.getElementById('texto-tarefa');
   const listaTarefas = document.querySelector('#lista-tarefas');
   const novaTarefa = document.createElement('li');
   // novaTarefa.innerText = 'placeholder';
   novaTarefa.innerText = input.value;
+  novaTarefa.addEventListener('click', itemSelect);
   listaTarefas.appendChild(novaTarefa);
   input.value = '';
 }
