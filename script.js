6079a17e31d76c5b4390217515e9e2fe9ee08820
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-17 20:14:33 -0300

    verifica req7

--- script.js
+++ script.js
@@ -9,9 +9,18 @@ function taskList(){
         orderedTask.appendChild(listTask);
         listTask.innerText = task.value;
         task.value = "";
+        listTask.addEventListener("click", backgroundColorChanging);
     }  
 }
 taskList();
+
+function backgroundColorChanging(event){
+    let taskItem = document.querySelectorAll("li");
+    for (let index = 0; index < taskItem.length; index += 1){
+        taskItem[index].style.backgroundColor = "";
+        event.target.style.backgroundColor = "rgb(128,128,128)"
+    }
+}
   
 
 
