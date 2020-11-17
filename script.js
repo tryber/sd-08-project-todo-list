da90bf73def545273267c3cbdbf1a63d7fadb9d0
Rafael Greco <rafaelmgreco@hotmail.com>
2020-11-17 13:48:53 -0300

    cinzar

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ function adicionarTarefa() {
     elemento.innerText = tarefa;
     lista.appendChild(elemento);
   }
-  
+  function mudarCor(){
+    let lista = document.getElementById('lista-tarefas');
+    lista.addEventListener('click', (event) =>{
+        let elemento = document.querySelectorAll('li');
+        for ( let contador = 0; contador < elemento.length; contador+=1){
+            elemento[contador].style.backgroundColor = "white";
+        }
+        event.target.style.backgroundColor = "rgb(128,128,128)";
+    })
+}
+mudarCor()
+
+
   window.onload = function () {
   let botaoAdicionar = document.querySelector('#criar-tarefa');
   botaoAdicionar.addEventListener('click', adicionarTarefa);
