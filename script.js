caf51b4c2349087c93fd466fb09e389881d723f6
Rafael <rafaeljavaro750@gmail.com>
2020-11-17 20:59:27 -0300

    Cria Classe selected, cria removeSelected - Req 7 e 8

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+const lista = document.querySelector('ol');
+const botaoCriaTarefa = document.querySelector('#criar-tarefa');
+
+function removeSelected() {
+  const listaDeTarefas = document.getElementsByTagName('li');
+  for (let index = 0; index < listaDeTarefas.length; index += 1) {
+    listaDeTarefas[index].classList.remove('selected');
+  }
+}
+
 function criaNovaTarefa() {
-  const botaoCriaTarefa = document.querySelector('#criar-tarefa');
-  const novaTarefa = document.getElementById('texto-tarefa');
-  const lista = document.querySelector('ol');
+  const novaTarefa = document.getElementById('texto-tarefa')
   botaoCriaTarefa.addEventListener('click', () => {
     const novaTarefaCriada = document.createElement('li');
+    novaTarefaCriada.addEventListener('click', () => {
+      removeSelected();
+      novaTarefaCriada.classList.add('selected');
+      console.log(lista);
+    });
     novaTarefaCriada.innerHTML = novaTarefa.value;
     lista.appendChild(novaTarefaCriada);
     novaTarefa.value = '';
   });
 }
 criaNovaTarefa();
-
-function mudaCorDaTarefa() {
-    const tarefas = document.querySelectorAll('li');
-}
-
