23f4b207791ac3d4b05c2988fe684da356c54264
brutibbc <brutibbc@gmail.com>
2020-11-16 22:29:47 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-function novaTarefa() {
-    let tarefa = document.querySelector('#texto-tarefa');
-    let botaoAdicionar = document.querySelector('#criar-tarefa');
-    let listaTarefa = document.querySelector('#lista-tarefas')
+const tarefa = document.querySelector('#texto-tarefa');
+const botaoAdicionar = document.querySelector('#criar-tarefa');
+const listaTarefa = document.querySelector('#lista-tarefas');
 
+function novaTarefa() {
     botaoAdicionar.addEventListener('click', function () {
         if (tarefa.value.length > 0) {
             let item = document.createElement('li');
+            item.className = 'item';
             item.innerText = tarefa.value;
             listaTarefa.appendChild(item);
-            tarefa.value = '';
+            tarefa.value = '';                                  
         } else {
             alert('Não esqueça de digitar o nome da tarefa!')
-        }
+        }                 
     })
 }
 novaTarefa();
+
+function selecionaItem() {
+   
+    listaTarefa.addEventListener('click', function (event) {
+        let itemSelecionado = document.querySelector('.selected');
+        if (itemSelecionado == null) {
+            event.target.className = 'item selected';
+        } else {
+            itemSelecionado.className = 'item';
+            event.target.className = 'item selected';
+        }
+    })    
+}
+selecionaItem();
