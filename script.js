3cced1ef881acb7b90752490ba5cfc8917b1b8bb
maym4y <jennim.almeida@gmail.com>
2020-11-17 13:46:06 -0300

    Elemento da lista clicado fica cinza

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function adicionarTarefa() {
-    let item = document.createElement('li');
-    let lista = document.querySelector('#lista-tarefas');
-    let texto = document.querySelector('#texto-tarefa');
+    const item = document.createElement('li');
+    const lista = document.querySelector('#lista-tarefas');
+    const texto = document.querySelector('#texto-tarefa');
     item.innerHTML = texto.value;
     lista.appendChild(item);
     texto.value = '';
 }
 
-let botao = document.querySelector('#criar-tarefa');
+const botao = document.querySelector('#criar-tarefa');
 botao.addEventListener('click', adicionarTarefa);
+
+function tarefaSelecionada(e) {
+    let item = document.querySelectorAll('li');
+    if (e.target.id !== 'selecionado') {
+        for (let check = 0; check < item.length; check += 1) {
+            item[check].removeAttribute('id');
+        }
+        e.target.id = 'selecionado';
+    } else if (e.target.id === 'selecionado') {
+        e.target.removeAttribute('id');
+    }
+}
+
+const lista = document.querySelector('#lista-tarefas');
+lista.addEventListener('click', tarefaSelecionada);
