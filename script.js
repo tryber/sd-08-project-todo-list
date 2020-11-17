aa21f2dc82d1e501671ab40691025f4ef86693e9
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-17 00:09:23 -0300

    :up: mover para baixo e para cima pronto

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const inputTxt = document.querySelector('#texto-tarefa');
 const tarefaList = document.querySelector('#lista-tarefas');
+tarefaList.innerHTML = localStorage.getItem("tarefaList");
 addEventListener('click', (event) => {
     if(event.target.id == 'criar-tarefa'){
+        if(inputTxt.value == ''){
+            alert('insira uma tarefa por favor');
+        }else{
         let li = document.createElement('li');
         li.className = 'item';
         li.innerText = inputTxt.value;
         tarefaList.appendChild(li);
         inputTxt.value = '';
+        };
     }else if(event.target.className == 'item'){
         for(key = 0; key < tarefaList.children.length; key++){
             if(tarefaList.children[key].classList.contains('itemSelected')){
@@ -24,10 +29,38 @@ addEventListener('click', (event) => {
             tarefaList.removeChild(completed[key]);
         };
     };
+    if(event.target.id == 'salvar-tarefas'){
+        localStorage.setItem('tarefaList', tarefaList.innerHTML);
+    };
+    if(event.target.id == 'mover-cima'){
+        const list = document.querySelectorAll('.item');
+        let selected;
+        for(key = 0; key < list.length; key++){
+          if(list[key].classList.contains('itemSelected')){
+            selected = key;
+          };
+        };
+        if(selected > 0){
+          list[selected].parentElement.insertBefore( list[selected], list[selected - 1]);
+        };
+      }
+      if(event.target.id == 'mover-baixo') {
+        const list = document.querySelectorAll('.item');
+        let selected;
+        for(key = 0; key < list.length; key++){
+          if (list[key].classList.contains('itemSelected')){
+            selected = key + 1;
+          }
+        }
+        if(selected < list.length){
+          list[selected].parentElement.insertBefore(list[selected], list[selected - 1]);
+        };
+      };
 });
 addEventListener('dblclick', (event) => {
-    if(event.target.classList.contains('item')){
+    if(event.target.classList.contains('itemSelected')){
         event.target.classList.toggle('completed');
     };
 });
+
 
