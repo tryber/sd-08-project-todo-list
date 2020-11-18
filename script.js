738fb1a3b91f1de744ee91cf8e4cba6dc440166c
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-18 11:38:01 -0300

    add rq5 no done yet

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function contentP() {
     document.getElementById('funcionamento').innerText = "Clique duas vezes em um item para marcá-lo como completo"
 }
 contentP();
+
+let addButton = document.getElementById("criar-tarefa");
+addButton.addEventListener('click', addToDoItem);
+function addToDoItem(){
+  
+}
