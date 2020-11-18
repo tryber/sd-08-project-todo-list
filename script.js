6a23346a988c24158f8c1a1e4487c901bcfddbb8
William Vieira <william.marangon@gmail.com>
2020-11-18 12:15:18 -0300

    ex 01-05 tt01

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let addTaskButton = document.querySelector('#criar-tarefa')
+function addTarefa() {
+    let texto = document.querySelector("#texto-tarefa")
+    let lista = document.querySelector("#lista-tarefas")
+    let criar = document.querySelector("#criar-tarefa")
+
+    criar.addEventListener('click', function() {
+    let li = document.createElement('li')
+    li.innerHTML = texto.value
+    lista.appendChild(li)
+    lista.next;
+    texto.value = null
+    })
+}
+addTarefa();
+
+
+/*let addTaskButton = document.querySelector('#criar-tarefa')
 let taskInput = document.querySelector('#texto-tarefa')
 let taskList = document.querySelector('#lista-tarefas')
 let lastSelectedElement = null
@@ -21,4 +37,4 @@ function setSelectedColor(event) {
     latsSelectedElement = event.target
     event.target.style.backgroundColor = "rgb(128, 128, 128)"
 
-}
+}*/
