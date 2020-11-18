cd9e558891d5397000344ef8d554967b24130048
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-11-18 13:59:49 -0300

    Vamos ver

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ let getText = document.getElementById('texto-tarefa');
 let buttonTarefa = document.getElementById('criar-tarefa');
 let eraseAll = document.getElementById('apaga-tudo');
 let TasksList = document.getElementById('lista-tarefas');
-getText.className = 'list-of-tasks';
+let getTasks = document.getElementById('tasks');
+
+
 
 function ListofTasks(event) {
 
     let paragraph = document.createElement('li');
     paragraph.innerText = getText.value;
-    paragraph.className = 'task';
+    paragraph.className('tarefas')
     TasksList.appendChild(paragraph);
+    paragraph.addEventListener('click', Selec);
+    getText.value = '';
+
+
+
 }
 
 buttonTarefa.addEventListener('click', ListofTasks);
 
 function Selec(event) {
-    //let tarefas = document.getElementById('task');
-
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    let selected = document.getElementsByClassName('selected');
+    if (selected.length > 0) {
+        selected[0].className = '';
+    }
+    event.target.className = 'selected';
 }
 
 TasksList.addEventListener('click', Selec);
