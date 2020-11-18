7fc36dd7636de6c8d049663213b0a8173e1f1867
WagnerMor <wagnermor@outlook.com>
2020-11-17 23:44:24 -0300

    requisito 09

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function adicionaLi() {
             const olItem = document.createElement('li');
             olList.style.listStyleType = 'decimal';
             olItem.classList = 'item';
-            // olItem.style.backgroundColor = '#fff';
             olItem.innerHTML = inputTextoTarefa.value;
             olList.appendChild(olItem);
             inputTextoTarefa.value ='';
@@ -25,14 +24,19 @@ function adicionaLi() {
                     for (let i = 0; i < ols.length; i += 1){
                         ols[i].style.backgroundColor = '#fff';
                     }
-                    // event.target.classList.toggle('selected');
                     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
                 });
             }
         }addSelected();
 
         //duplo clique
-        
+        olList.addEventListener('dblclick', function (evento) {
+            if (evento.target.classList.contains('completed')) {
+              evento.target.classList.remove('completed');
+            } else {
+              evento.target.classList.add('completed');
+            };
+          });
     
     }
 }adicionaLi();
