7af7dff6b1fa5231d3fcaa834621913812dea94b
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-18 15:03:18 -0300

    Botão adiciona tarefa à lista ordenada

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-let valorDigitado = document.getElementById('texto-tarefa').innerHTML;
+const qSelector = (elemento) => document.querySelector(elemento);
+const qSelectorAll = (elemento) => document.querySelectorAll(elemento);
+const cria = qSelector('#cria-tarefa');
+const apaga = qSelector('#apaga-tudo');
+const lista = qSelector('#lista-tarefas');
+const removeFinalizados = qSelector('#remover-finalizados');
 
-function adicionarTarefa(valor){
-    let olTag = document.getElementById('lista-tarefas');
-    let liTag = document.createElement('li');
-    liTag.innerHTML = valor;
-    olTag.appendChild(liTag);
-}
-adicionarTarefa(valorDigitado);
 
-addEventListener('click', adicionarTarefa)
+cria.addEventListener('click', () => {
+    var texto = qSelector('#texto-tarefa');
+    const lista = qSelector('#lista-tarefas');
+    if(texto.value.length > 2){
+        let liTag = document.createElement('li');
+        liTag.className = 'tarefa';
+        liTag.innerText = texto.value;
+        lista.appendChild(liTag);
+        texto.value = '';
+    } else {
+        alert("Digite uma Tarefa antes de adicionar!!!")
+    }
+});
+
+
+
+
