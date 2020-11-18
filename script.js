309f237c4135719881d8f4061946cc447c6a175e
Rafael <rafaeljavaro750@gmail.com>
2020-11-17 21:21:36 -0300

    Req 11 - cria Funçao apagaLista()

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const lista = document.querySelector('ol');
 const botaoCriaTarefa = document.querySelector('#criar-tarefa');
+const botaoApagaTudo = document.querySelector('#apaga-tudo');
 
 function removeSelected() {
   const listaDeTarefas = document.getElementsByTagName('li');
@@ -9,20 +10,26 @@ function removeSelected() {
 }
 
 function criaNovaTarefa() {
-  const novaTarefa = document.getElementById('texto-tarefa')
+  const novaTarefa = document.getElementById('texto-tarefa');
   botaoCriaTarefa.addEventListener('click', () => {
     const novaTarefaCriada = document.createElement('li');
     novaTarefaCriada.addEventListener('click', () => {
       removeSelected();
       novaTarefaCriada.classList.add('selected');
-      console.log(lista);
     });
     novaTarefaCriada.addEventListener('dblclick', () => {
-      novaTarefaCriada.classList.toggle('completed')
-    })
+      novaTarefaCriada.classList.toggle('completed');
+    });
     novaTarefaCriada.innerHTML = novaTarefa.value;
     lista.appendChild(novaTarefaCriada);
     novaTarefa.value = '';
   });
 }
 criaNovaTarefa();
+
+function apagaLista() {
+  botaoApagaTudo.addEventListener('click', () => {
+    lista.innerHTML = '';
+  });
+}
+apagaLista();
