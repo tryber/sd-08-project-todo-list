75ee4d482b043c951fd3647352b8aae9cd7a48eb
WagnerMor <wagnermor@outlook.com>
2020-11-17 23:31:19 -0300

    requisito 07 e 08

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const olList = document.getElementById('lista-tarefas');
 let inputTextoTarefa = document.getElementById('texto-tarefa');
 const btnCriaTarefa = document.getElementById('criar-tarefa');
-console.log(inputTextoTarefa.value);
-console.log(olList);
-console.log(btnCriaTarefa);
 // adiciona li
 function adicionaLi() {
     btnCriaTarefa.addEventListener('click', addTarefa);
@@ -11,19 +8,31 @@ function adicionaLi() {
         if(inputTextoTarefa.value !== '' && inputTextoTarefa.value !== ' ') {
             const olItem = document.createElement('li');
             olList.style.listStyleType = 'decimal';
+            olItem.classList = 'item';
+            // olItem.style.backgroundColor = '#fff';
             olItem.innerHTML = inputTextoTarefa.value;
             olList.appendChild(olItem);
-            console.log(olItem.innerText);
             inputTextoTarefa.value ='';
         }
         inputTextoTarefa.focus();
+
+            // adiciona seleção
+        let ols = document.querySelectorAll('#lista-tarefas li');
+        console.log(ols.length);
+        function addSelected() {
+            for (let index = 0; index < ols.length; index += 1) {
+                ols[index].addEventListener('click',function(event){
+                    for (let i = 0; i < ols.length; i += 1){
+                        ols[i].style.backgroundColor = '#fff';
+                    }
+                    // event.target.classList.toggle('selected');
+                    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+                });
+            }
+        }addSelected();
+
+        //duplo clique
+        
+    
     }
 }adicionaLi();
-// function adicionaLi() {
-//     btnCriaTarefa.addEventListener('click', addTarefa);
-//     function addTarefa() {
-//         // const olItem = document.createElement('li');
-//         // olList.appendChild(olItem);
-//         // console.log(inputTextoTarefa.value);
-//     }
-// }adicionaLi();
