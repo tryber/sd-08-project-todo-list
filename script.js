cf5afd1c99bd7378732a158009b7fc37fe3663f7
Thais Quintela <thais.s.quintela@gmail.com>
2020-11-18 03:32:09 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function addNewTask() {
 
 const addTask = document.querySelector('#criar-tarefa');
 addTask.addEventListener('click', addNewTask);
+
+function selectTask(event) {
+  let chosen = event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+const chosenTask = document.getElementById('lista-tarefas');
+chosenTask.addEventListener('click', selectTask)
