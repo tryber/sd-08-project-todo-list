5b9e906df37f8c85ad5176c09e74f0542a3ce644
Andre Sampaio <andresampaiolm@gmail.com>
2020-11-18 00:48:05 -0300

    implementaçao dos requisitos bonus

--- script.js
+++ script.js
@@ -2,14 +2,46 @@ const listaTarefas = document.querySelector('#lista-tarefas');
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 const btnApagarTarefas = document.querySelector('#apaga-tudo');
 const btnRemoverFinalizados = document.querySelector('#remover-finalizados');
+const btnSalvarLista = document.querySelector('#salvar-tarefas');
+const btnMoverCima = document.querySelector('#mover-cima');
+const btnMoverBaixo = document.querySelector('#mover-baixo');
+const btnRemoverSelecionado = document.querySelector('#remover-selecionado');
 
 btnCriarTarefa.addEventListener('click', adicionarTarefa);
 btnApagarTarefas.addEventListener('click', apagarTarefas);
 btnRemoverFinalizados.addEventListener('click', removerFinalizados);
+btnSalvarLista.addEventListener('click', salvarLista);
+btnMoverCima.addEventListener('click', moverCima);
+btnMoverBaixo.addEventListener('click', moverBaixo);
+btnRemoverSelecionado.addEventListener('click', removerSelecionado);
+
+function listaSalva() {
+  if (localStorage.getItem('content') !== '') {
+    let tarefas = localStorage.getItem('content');
+    tarefas = tarefas.split(',');
+    
+    for (let tarefa of tarefas) {
+      const li = document.createElement('li');
+      const textNode = document.createTextNode(tarefa);
+      li.appendChild(textNode);
+      li.addEventListener('click', selecionarTarefa);
+      li.addEventListener('dblclick', completarTarefa);
+
+      listaTarefas.appendChild(li);
+    }
+  }
+}
+
+listaSalva()
 
 function adicionarTarefa() {
   const input = document.querySelector('#texto-tarefa');
   const text = input.value;
+
+  if (text === '') {
+    return;
+  }
+  
   input.value = '';
   const textNode = document.createTextNode(text);
   const li = document.createElement('li');
@@ -32,6 +64,14 @@ function removerFinalizados() {
   }
 }
 
+function salvarLista() {
+  const conteudo = [];
+  for (let index = 0; index < listaTarefas.children.length; index += 1) {
+    conteudo.push(listaTarefas.children[index].innerText);
+  }
+  localStorage.setItem('content', conteudo);
+}
+
 function selecionarTarefa(event) {
   if (document.querySelector('.selected') !== null) {
     document.querySelector('.selected').classList.remove('selected');
@@ -48,5 +88,38 @@ function completarTarefa(event) {
     tarefa.classList.remove('completed');
   } else {
     tarefa.classList.add('completed');
+  }
+}
+
+function moverCima() {
+  let tarefaMover = document.querySelector('.selected');
+  let tarefas = listaTarefas.children
+
+  for (let index = 0; index < tarefas.length; index += 1) {
+    if (tarefaMover === tarefas.item(index) && index !== 0) {
+      listaTarefas.removeChild(tarefaMover);
+      listaTarefas.insertBefore(tarefaMover, tarefas.item(index - 1));
+      break;
+    } 
+  }
+}
+
+function moverBaixo() {
+  let tarefaMover = document.querySelector('.selected');
+  let tarefas = listaTarefas.children
+
+  for (let index = 0; index < tarefas.length; index += 1) {
+    if (tarefaMover === tarefas.item(index) && index !== tarefas.length - 1) {
+      listaTarefas.removeChild(tarefaMover);
+      listaTarefas.insertBefore(tarefaMover, tarefas.item(index).nextSibling);
+      break;
+    } 
+  }
+}
+
+function removerSelecionado() {
+  const tarefaSelecionada = document.querySelector('.selected');
+  if (tarefaSelecionada !== null){
+    listaTarefas.removeChild(tarefaSelecionada);
   }
 }
