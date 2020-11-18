d1e33db3e43f2e8fadfe9d205d0bda77bd11719d
Víctor Cançado <victor.cancado@gmail.com>
2020-11-18 13:35:12 -0300

    Atualização geral

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let orderedList = [];
-let createList = document.getElementById("lista-tarefas");
-for(let index = 0; index < 3; index += 1) {
-    let newList = orderedList[index];
-    let listItens = document.createElement("li");
-    listItens.innerHTML = newList;
-    listItens.className = "itens-lista";
-    createList.appendChild(listItens);
-}
 //requisitos 5 e 6
 let createTaskButton = document.getElementById("criar-tarefa");
 function taskButton() {
@@ -18,21 +9,15 @@ function newEvent() {
     let newItens = document.createElement("li");
     newItens.innerHTML = inputText;
     newItens.className = "itens-lista";
+    newItens.addEventListener("click", paddingColor);
     createList.appendChild(newItens);
 }
 //requisito 7
-// window.onload = selectWhite;
-// function selectWhite() {
-//    let selectListIten = document.getElementById("itens-lista");
-//    selectListIten.className = "color-list-init"
-// }
-let taskList = document.getElementsByClassName("itens-lista");
-function paintList() {
-    for(let index = 0; index < taskList.length; index += 1) {
-        taskList[index].addEventListener("click", paddingColor);
-    }
-}
 paintList();
 function paddingColor(event) {
-    event.target.style.backgroundColor = "rgb(128, 128, 128)";
+    for(let index = 0; index < taskList.length; index += 1) {
+        taskList[index].className = "itens-lista";
+    }
+    event.target.className = "itens-lista click-class";
 }
+//requisito 8
