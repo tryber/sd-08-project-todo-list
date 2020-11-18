4bb782badc572058548385a798c6eb064a2f4313
Daniel <marcio.daniel@msn.com>
2020-11-17 23:33:36 -0300

    Colocando Bonus

--- script.js
+++ script.js
@@ -9,30 +9,24 @@ removerFinalziados.addEventListener('click', removeApenasFinalizados);
 function novasTarefas() {
   const descricaoTarefa = textoTarefa.value;
   const novaTarefa = document.createElement('li');
-  novaTarefa.className = 'toras-tarefas';
+  novaTarefa.className = 'todas-tarefas';
   novaTarefa.innerText = descricaoTarefa;
-  novaTarefa.addEventListener('dblclick', tarefaCompleada);
+  novaTarefa.addEventListener('dblclick', tarefaCompletada);
   novaTarefa.addEventListener('click', mudarCor);
   listaTarefas.appendChild(novaTarefa);
   textoTarefa.value = '';
-  console.log("clicando botão cria tarefa");
 }
 function removeTudo() {
   listaTarefas.innerHTML = '';
-  console.log(" clicando apaga tudo");
 }
-function removeApenasFinalizados() {
-  console.log('remove finalziados');
-}
-const todasTarefas = document.getElementsByClassName('toras-tarefas');
+const todasTarefas = document.getElementsByClassName('todas-tarefas');
 function mudarCor(event) {
   for (let i = 0; i < todasTarefas.length; i++) {
     todasTarefas[i].style.backgroundColor = '';
   }
   event.target.style.backgroundColor = "rgb(128, 128, 128)";
-  console.log('Clicando Alterar Cor...');
 }
-function tarefaCompleada(event) {
+function tarefaCompletada(event) {
   let elemento = event.target;
   if (elemento.classList.contains('completed')) {
     elemento.classList.remove('completed');
@@ -42,9 +36,82 @@ function tarefaCompleada(event) {
 }
 function removeApenasFinalizados(event) {
   let apagaConcluidos = document.querySelectorAll('.completed');
-  apagaConcluidos.forEach((item)=>{
-    if(item.classList.contains('completed')) {
+  apagaConcluidos.forEach((item) => {
+    if (item.classList.contains('completed')) {
       listaTarefas.removeChild(item);
     }
   });
 }
+botaoParaCima = document.getElementById('mover-cima');
+botaoParaCima.addEventListener('click', moveParaCima);
+function moveParaCima() {
+  let indexSelecionado = 0;
+  for (let i = 0; i < todasTarefas.length; i += 1) {
+    if (todasTarefas[i].style.backgroundColor === "rgb(128, 128, 128)") {
+      indexSelecionado = i;
+    }
+  }
+  if (indexSelecionado - 1 < 0) {
+    todasTarefas[indexSelecionado].style.backgroundColor = "";
+    todasTarefas[todasTarefas.length - 1].style.backgroundColor = "rgb(128, 128, 128)";
+  } else {
+    todasTarefas[indexSelecionado].style.backgroundColor = "";
+    todasTarefas[indexSelecionado - 1].style.backgroundColor = "rgb(128, 128, 128)";
+  }
+  let atual = todasTarefas[indexSelecionado].innerText;
+  if (indexSelecionado - 1 < 0) {
+    let ultimo = todasTarefas[todasTarefas.length - 1].innerText;
+    todasTarefas[todasTarefas.length - 1].innerText = atual;
+    todasTarefas[indexSelecionado].innerText = ultimo;
+  } else {
+    let anterior = todasTarefas[indexSelecionado - 1].innerText;
+    todasTarefas[indexSelecionado - 1].innerText = atual;
+    todasTarefas[indexSelecionado].innerText = anterior;
+  }
+}
+botaoParaBaixo = document.getElementById('mover-baixo');
+botaoParaBaixo.addEventListener('click', moveParaBaixo);
+function moveParaBaixo() {
+  let indexSelecionado = 0;
+  for (let i = 0; i < todasTarefas.length; i += 1) {
+    if (todasTarefas[i].style.backgroundColor === "rgb(128, 128, 128)") {
+      indexSelecionado = i;
+    }
+  }
+  if (indexSelecionado == todasTarefas.length - 1) {
+    todasTarefas[indexSelecionado].style.backgroundColor = "";
+    todasTarefas[0].style.backgroundColor = "rgb(128, 128, 128)";
+  } else {
+    todasTarefas[indexSelecionado].style.backgroundColor = "";
+    todasTarefas[indexSelecionado + 1].style.backgroundColor = "rgb(128, 128, 128)";
+  }
+  let atual = todasTarefas[indexSelecionado].innerText;
+  if (indexSelecionado == todasTarefas.length - 1) {
+    let primeiro = todasTarefas[0].innerText;
+    todasTarefas[0].innerText = atual;
+    todasTarefas[indexSelecionado].innerText = primeiro;
+  } else {
+    let proximo = todasTarefas[indexSelecionado + 1].innerText;
+    todasTarefas[indexSelecionado + 1].innerText = atual;
+    todasTarefas[indexSelecionado].innerText = proximo;
+  }
+}
+botaoRemoveItem = document.getElementById('remover-selecionado');
+botaoRemoveItem.addEventListener('click', removeItem);
+function removeItem(){
+  for (let i = 0; i < todasTarefas.length; i += 1) {
+    if (todasTarefas[i].style.backgroundColor === "rgb(128, 128, 128)") {
+      todasTarefas[i].parentNode.removeChild(todasTarefas[i]);
+    }
+  console.log('remove item');
+}
+}
+botaoSalvarTarefas = document.getElementById('salvar-tarefas');
+botaoSalvarTarefas.addEventListener('click', salvarTarefas);
+function salvarTarefas() {
+  localStorage.setItem("lista", listaTarefas.innerHTML);
+}
+window.onload = function () {
+  let listaSalva = localStorage.getItem("lista");
+  listaTarefas.innerHTML = listaSalva;
+};
