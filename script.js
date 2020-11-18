a14e10c71170afdac5c638a7c876fbacd90b8faf
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2020-11-18 00:58:31 -0300

    requisito 7, tentativa 1

--- script.js
+++ script.js
@@ -4,11 +4,30 @@ btnCriaTarefa.addEventListener("click", function () {
     if (textoTarefa.value != "") {
     let novaTarefa = textoTarefa.value;
     textoTarefa.value = "";
-    console.log(novaTarefa);
     let lista = document.getElementById("lista-tarefas");
     let itemLista = document.createElement("li");
     itemLista.innerHTML = novaTarefa;
-    lista.appendChild(itemLista)
-
+    itemLista.className = "tarefa-na-lista";
+    lista.appendChild(itemLista);
+    itemLista.addEventListener("click", function () {
+    itemLista.style.backgroundColor = "rgb(128, 128, 128)";
+    })
     }
-})
+});
+// para fazer essa funcionalidade abaixo pesquisei na internet e utilizei o link: https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+textoTarefa.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+        let novaTarefa = textoTarefa.value;
+    textoTarefa.value = "";
+    let lista = document.getElementById("lista-tarefas");
+    let itemLista = document.createElement("li");
+    itemLista.innerHTML = novaTarefa;
+    itemLista.className = "tarefa-na-lista";
+    lista.appendChild(itemLista);
+    itemLista.addEventListener("click", function () {
+    itemLista.style.backgroundColor = "rgb(128, 128, 128)";
+    })
+    }
+});
+
+
