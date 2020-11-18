29020e71f60d32e39db05af03c477a061f75c7fd
Uilton Rocha <itoclick@gmail.com>
2020-11-17 22:20:19 -0300

    adicionando mais req

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-  // seletor do input
+  
+ 
+
+  //botão salva Tudo
+  let btnsalva = document.getElementById("salvar-tarefas");
+    // seletor do input
   const inputSEl = document.getElementById("texto-tarefa");
   // seletor do ol
   const olsel = document.getElementById("lista-tarefas");
   // seletor do btn adicionar
   const btnsel = document.getElementById("criar-tarefa");
+  //deve receber o liquando for celecionado
+  let liselecionada; 
+
+  
 
   
   // escuta o evento adicionar
@@ -29,6 +38,7 @@
     let bgcor = '';
     if (event.target.style.backgroundColor === ""){
       bgcor = 'rgb(128, 128, 128)'
+      liselecionada =event.target;
       }
       for (i=0; i<todoli.length;i+=1){
         todoli[i].style.backgroundColor ="";
@@ -45,9 +55,7 @@
     };
    }
 
-   
-
-
+    //botão apagatudo
    document.getElementById('apaga-tudo').addEventListener('click',funcApagao);
   
   function funcApagao (){
@@ -55,9 +63,40 @@
      olconteiner.innerHTML = '';
      
   };
-
-    /**/
+   
+    //botão apaga selecionado
+  /*   document.getElementById('remover-selecionado').addEventListener('click',funcApacel);
   
- //for (i=0; i<list.length;i+=1){
-    // document.body.removeChild(event.target[i]);
-    //}
+  function funcApacel (){
+            let olconteiner = document.querySelector('#lista-tarefas');
+           }
+    
+     olconteiner.innerHTML.liselecionada = ''; */
+
+    
+    // escuta o evento salvar lista
+  btnsalva.addEventListener("click", salvatd);
+    function salvatd () {
+      let selectedlis = document.querySelector('li');
+     
+      for (i=0; i < selectedlis.length; i+=1){
+       localStorage.setItem(selectedlis[i].index, selectedlis[i].inertext)      
+        }
+      
+    };
+/* //    btnsel.addEventListener("click", nvtarefa);
+
+  // function nvtarefa() {
+  //   let objcria =document.createElement('li')
+  //   let container = inputSEl.value;
+  //   if (container ===""|| container ===null) {
+  //     alert('Insira um valor');
+  //   }
+  //     else {
+  //   objcria.textContent =container;
+  //   objcria.className ='file';
+  //   objcria.addEventListener("click", bgli);
+  //   objcria.addEventListener("dbleclick", sobrelist);
+  //   olsel.appendChild(objcria);
+  //   inputSEl.value = '';}
+  //     } */
