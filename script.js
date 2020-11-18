f5267e0842fb1914284c7f3c9b072030ca75d5f2
Víctor Cançado <victor.cancado@gmail.com>
2020-11-18 11:01:56 -0300

    Cria função de adicionar itens a lista

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ for(let index = 0; index < 3; index += 1) {
     let newList = orderedList[index];
     let listItens = document.createElement("li");
     listItens.innerHTML = newList;
+    listItens.className = "itens-lista";
     createList.appendChild(listItens);
+}
+
+let createTaskButton = document.getElementById("criar-tarefa");
+function taskButton() {
+    createTaskButton.addEventListener("click", newEvent);
+}
+taskButton();
+function newEvent() {
+    let inputText = document.getElementById("texto-tarefa");
+    let newItens = document.createElement("li");
+    newItens.innerHTML = inputText;
+    newItens.className = "itens-lista";
+    createList.appendChild(newItens);
 }
