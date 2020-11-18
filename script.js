0fd9f47060bfb59778159e1b06e0ddef97b72984
Victor Moraes <moraesvictorgk@gmail.com>
2020-11-18 13:28:14 -0300

    adiciona header

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let btn = document.getElementById('criar-tarefa');
-let texto = document.getElementById(te)
+let texto = document.getElementById('texto-tarefa');
+let list = document.getElementById('lista-tarefas');
 
-btn.addEventListener('click', function(){
-    
+btn.addEventListener('click', function () {
+    var lista = list.innerHTML;
+    lista = lista + "<li>" + texto + "</li>";
+    list.innerText = lista;
+
+
+
 })
