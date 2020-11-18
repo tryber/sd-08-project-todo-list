b1b94930ee5a58d3666630fcbd4cff706137347b
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2020-11-18 03:31:55 -0300

    requisito 8, tentaiva 1

--- script.js
+++ script.js
@@ -2,31 +2,45 @@ let textoTarefa = document.getElementById("texto-tarefa");
 let btnCriaTarefa = document.getElementById("criar-tarefa");
 btnCriaTarefa.addEventListener("click", function () {
     if (textoTarefa.value != "") {
-    let novaTarefa = textoTarefa.value;
-    textoTarefa.value = "";
-    let lista = document.getElementById("lista-tarefas");
-    let itemLista = document.createElement("li");
-    itemLista.innerHTML = novaTarefa;
-    itemLista.className = "tarefa-na-lista";
-    lista.appendChild(itemLista);
-    itemLista.addEventListener("click", function () {
-    itemLista.style.backgroundColor = "rgb(128, 128, 128)";
-    })
+        let novaTarefa = textoTarefa.value;
+        textoTarefa.value = "";
+        let lista = document.getElementById("lista-tarefas");
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = novaTarefa;
+        itemLista.className = "tarefa-na-lista";
+        lista.appendChild(itemLista);
+        itemLista.addEventListener("click", function () {
+            let todasLi = document.getElementsByTagName("li");
+            for (let contador = 0; contador < todasLi.length; contador += 1) {
+                let liAtual = todasLi[contador];
+                liAtual.style.backgroundColor = "";
+                liAtual.id = "tarefa-na-lista";
+            }
+            itemLista.style.backgroundColor = "rgb(128, 128, 128)";
+            itemLista.id = "selecionado";
+        })
     }
 });
 // para fazer essa funcionalidade abaixo pesquisei na internet e utilizei o link: https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 textoTarefa.addEventListener("keydown", function (event) {
     if (event.keyCode === 13) {
         let novaTarefa = textoTarefa.value;
-    textoTarefa.value = "";
-    let lista = document.getElementById("lista-tarefas");
-    let itemLista = document.createElement("li");
-    itemLista.innerHTML = novaTarefa;
-    itemLista.className = "tarefa-na-lista";
-    lista.appendChild(itemLista);
-    itemLista.addEventListener("click", function () {
-    itemLista.style.backgroundColor = "rgb(128, 128, 128)";
-    })
+        textoTarefa.value = "";
+        let lista = document.getElementById("lista-tarefas");
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = novaTarefa;
+        itemLista.className = "tarefa-na-lista";
+        lista.appendChild(itemLista);
+        itemLista.addEventListener("click", function () {
+            let todasLi = document.getElementsByTagName("li");
+            for (let contador = 0; contador < todasLi.length; contador += 1) {
+                let liAtual = todasLi[contador];
+                liAtual.style.backgroundColor = "";
+                liAtual.id = "tarefa-na-lista";
+            }
+            itemLista.style.backgroundColor = "rgb(128, 128, 128)";
+            itemLista.id = "selecionado";
+        })
     }
 });
 
