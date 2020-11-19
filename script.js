387d0a1c27e6c59ed280c16a1a937b83e408eaa7
Victor Moraes <moraesvictorgk@gmail.com>
2020-11-19 20:38:39 -0300

    cria background color e risca

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 let btn = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const texto = document.getElementById('texto-tarefa');
+const tarefas = document.getElementsByClassName('classeItens')
 
 //requisito 
 btn.addEventListener('click', function () {
-	if (texto.value.length != 0){
-	let createLi = document.createElement("li");
-	list.appendChild(createLi);
-	createLi.innerHTML = texto.value;
-	texto.value = null;
+	if (texto.value.length != 0) {
+		let createLi = document.createElement("li");
+		list.appendChild(createLi);
+		createLi.innerHTML = texto.value;
+		texto.value = null;
+		createLi.className = "classeItens"
 	}
 })
+//requisito 8
+function completo(event) {
+	event.target.classList.toggle('feito');
+}
+
+list.addEventListener('dblclick', completo)
+
+function alterar(event) {
+	for (let i = 0; i < tarefas.length; i += 1) {
+		tarefas[i].classList.remove('alteraCor')
+		event.target.classList.add('alteraCor')
+	}
+}
+
+list.addEventListener('click', alterar)
