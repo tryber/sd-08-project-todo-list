26bcd1675acb0447829fe3f438431e1f40570b9d
Leandro Lachini <leandrolachini@gmail.com>
2020-11-20 16:23:02 -0300

    correcao codeclimate

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
-let listaOrdenadaTarefas = document.getElementById('lista-tarefas');
-let botao = document.getElementById('criar-tarefa');
-let localTexto = document.getElementById('texto-tarefa');
+const listaOrdenadaTarefas = document.getElementById('lista-tarefas');
+const botao = document.getElementById('criar-tarefa');
+const localTexto = document.getElementById('texto-tarefa');
 
-    
 botao.addEventListener('click', function () {
-    let tarefaInserida = document.createElement('li')
-    tarefaInserida.className = 'tarefa';
-    tarefaInserida.innerHTML = localTexto.value;
-    localTexto.value = '';
-    listaOrdenadaTarefas.appendChild(tarefaInserida);
-    tarefaInserida.addEventListener('click', corFundoLista)
-    tarefaInserida.addEventListener('dblclick', tarefaConcluida)
+  const tarefaInserida = document.createElement('li');
+  tarefaInserida.className = 'tarefa';
+  tarefaInserida.innerHTML = localTexto.value;
+  localTexto.value = '';
+  listaOrdenadaTarefas.appendChild(tarefaInserida);
+  tarefaInserida.addEventListener('click', corFundoLista)
+  tarefaInserida.addEventListener('dblclick', tarefaConcluida)
 });
 
-
-let tarefaSelecionada = ''; 
+let tarefaSelecionada = '';
 function corFundoLista(Event) {
-    if (tarefaSelecionada === ''){
-        tarefaSelecionada = Event.target; // event é o click. target é o elemento html que aconteceu o evento. 
-    } else {
-        tarefaSelecionada.style.backgroundColor = 'white';
-        tarefaSelecionada = Event.target;
-    }
-    tarefaSelecionada.style.backgroundColor = 'rgb(128,128,128)';
+  if (tarefaSelecionada === ''){
+    tarefaSelecionada = Event.target; // event é o click. target é o elemento html que aconteceu o evento.
+  } else {
+    tarefaSelecionada.style.backgroundColor = 'white';
+    tarefaSelecionada = Event.target;
+   }
+  tarefaSelecionada.style.backgroundColor = 'rgb(128,128,128)';
 }
 // console.log(Event.target);
-
-
 let botaoApaga = document.getElementById('apaga-tudo');
 botaoApaga.addEventListener('click', function () {
-    listaOrdenadaTarefas.innerHTML = ''; 
+  listaOrdenadaTarefas.innerHTML = '';
 });
 
-
 function tarefaConcluida(Event) {
-    Event.target.className = 'completed';
-    let retiraTarefaConcluida = Event.target;
-    retiraTarefaConcluida.addEventListener('dblclick', function(Event) {
-        Event.target.className = 'tarefa';
-    });
+  Event.target.className = 'completed';
+  let retiraTarefaConcluida = Event.target;
+  retiraTarefaConcluida.addEventListener('dblclick', function(Event) {
+    Event.target.className = 'tarefa';
+  });
 }
 
 let botaoRemoveFinalizados = document.getElementById('remover-finalizados');
 botaoRemoveFinalizados.addEventListener('click', function () {
-    let tarefaFinalizada = document.getElementsByClassName('completed')
-    let tarefaFinalizadaArray = tarefaFinalizada.length;
+  let tarefaFinalizada = document.getElementsByClassName('completed')
+  let tarefaFinalizadaArray = tarefaFinalizada.length;
     for (let index = 0; index < tarefaFinalizadaArray; index+= 1) {
-        if( tarefaFinalizada[0].classList.contains('completed')){
-            tarefaFinalizada[0].remove();
-        }
+      if( tarefaFinalizada[0].classList.contains('completed')){
+        tarefaFinalizada[0].remove();
+      }
     }
 });
 
