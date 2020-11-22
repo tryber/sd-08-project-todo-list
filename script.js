3323437e69972b2aa5871c82a5ddd983b5968af2
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-21 23:08:08 -0300

    Requisites 07/08

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ function addTask() {
     let taskItem = document.getElementById('texto-tarefa');
     let taskList = document.getElementById('lista-tarefas');
     let newTaskItem = document.createElement('li');
+    newTaskItem.classList.add('task-style');
+    newTaskItem.addEventListener('click', changeLiClassColor)
     taskList.appendChild(newTaskItem);
     newTaskItem.innerText = taskItem.value;
     taskItem.value = '';
   });
 }
 addTask();
+
+function changeLiClassColor(event) {
+  let liArray = document.getElementsByClassName('task-style');
+  for (let index = 0; index < liArray.length; index += 1) {
+    liArray[index].classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+  
