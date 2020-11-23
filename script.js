37d7793505d113bc5500e46f03fbb5a4ca33973c
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-23 16:27:45 -0300

    Requisite 13 bonus

--- script.js
+++ script.js
@@ -42,6 +42,36 @@ function removeCompleted() {
 }
 removeCompleted();
 
+function moveTaskUp() {
+  const buttonMoveUp = document.getElementById('mover-cima');
+  buttonMoveUp.addEventListener('click', function () {
+    if (document.querySelector('.selected')) {
+      let taskSelectedUp = document.querySelector('.selected');
+      if (taskSelectedUp.previousElementSibling != null) {
+        let previousTask = taskSelectedUp.previousElementSibling.outerHTML;
+        taskSelectedUp.previousElementSibling.outerHTML = taskSelectedUp.outerHTML;
+        taskSelectedUp.outerHTML = previousTask;
+      }
+    }
+  });
+}
+moveTaskUp();
+
+function moveTaskDown() {
+  const buttonMoveDown = document.getElementById('mover-baixo');
+  buttonMoveDown.addEventListener('click', function () {
+    if (document.querySelector('.selected')) {
+      let taskSelectedDown = document.querySelector('.selected');
+      if (taskSelectedDown.nextElementSibling != null) {
+        let nextTask = taskSelectedDown.nextElementSibling.outerHTML;
+        taskSelectedDown.nextElementSibling.outerHTML = taskSelectedDown.outerHTML;
+        taskSelectedDown.outerHTML = nextTask;
+      }
+    }
+  });
+}
+moveTaskDown();
+
 function removeSelected() {
   let selectedTask = document.getElementsByClassName('selected');
   const buttonRemoveTask = document.getElementById('remover-selecionado');
