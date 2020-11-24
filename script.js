222b5547e2e9312f4b8a4a842c42df8d25919ebc
Victor Moraes <moraesvictorgk@gmail.com>
2020-11-24 17:23:44 -0300

    adiciona botao Salvar

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-let btn = document.getElementById('criar-tarefa');
+const btn = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const texto = document.getElementById('texto-tarefa');
 const tarefas = document.getElementsByClassName('classeItens');
 const botao = document.getElementById('apaga-tudo');
 const botaofinal = document.getElementById('remover-finalizados');
 const rmvselectedbtn = document.getElementById('remover-selecionado');
+const btnSave = document.getElementById('salvar-tarefas');
+
+
+window.onload = function () {
+	for (i = 0; i < localStorage.length; i += 1) {
+		let criarLista = document.createElement('li');
+		list.appendChild(criarLista);
+		let check = localStorage.getItem(i);
+		let ultimaPosicao = check.length - 1;
+		if (check[ultimaPosicao] == '!') {
+			criarLista.innerHTML = check.slice(0, -1);
+			criarLista.classList.add('completed')
+		} else {
+			criarLista.innerHTML = check;
+		}
+
+	}
+}
+
 //requisito 
 btn.addEventListener('click', function () {
 	if (texto.value.length != 0) {
@@ -50,3 +69,15 @@ rmvselectedbtn.addEventListener('click', function () {
 		selected[i].remove()
 	}
 })
+
+btnSave.addEventListener('click', function () {
+	localStorage.clear();
+	let listaOrdenada = document.querySelectorAll('ol li');
+	for (let i = 0; i < listaOrdenada.length; i += 1) {
+		if (listaOrdenada[i].classList.contains('completed')) {
+			localStorage.setItem(localStorage.length, listaOrdenada[i].innerHTML + '!');
+		} else {
+			localStorage.setItem(localStorage.length, listaOrdenada[i].innerHTML)
+		}
+	}
+}) 
