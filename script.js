88a33f143aba7270358411ceeca8a9b538f72c2e
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-25 00:12:06 -0300

    Fix Git

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const qSelector = (elemento) => document.querySelector(elemento);
 const qSelectorAll = (elemento) => document.querySelectorAll(elemento);
-const cria = qSelector('#cria-tarefa');
-const apaga = qSelector('#apaga-tudo');
-const lista = qSelector('#lista-tarefas');
-const removeFinalizados = qSelector('#remover-finalizados');
+const cria = qSelector("#cria-tarefa");
+const apaga = qSelector("#apaga-tudo");
+const lista = qSelector("#lista-tarefas");
+const removeFinalizados = qSelector("#remover-finalizados");
 
-
-cria.addEventListener('click', () => {
-    var texto = qSelector('#texto-tarefa');
-    const lista = qSelector('#lista-tarefas');
-    if(texto.value.length > 2){
-        let liTag = document.createElement('li');
-        liTag.className = 'tarefa';
-        liTag.innerText = texto.value;
-        lista.appendChild(liTag);
-        texto.value = '';
-    } else {
-        alert("Digite uma Tarefa antes de adicionar!!!")
-    }
+cria.addEventListener("click", () => {
+  var texto = qSelector("#texto-tarefa");
+  const lista = qSelector("#lista-tarefas");
+  if (texto.value.length > 2) {
+    let liTag = document.createElement("li");
+    liTag.className = "tarefa";
+    liTag.innerText = texto.value;
+    lista.appendChild(liTag);
+    texto.value = "";
+  } else {
+    alert("Digite uma Tarefa antes de adicionar!!!");
+  }
 });
 
-
-
-
+apaga.addEventListener("click", () => {
+  let ol = qSelector("#apaga-tudo");
+  document.removeChild(ol);
+});
