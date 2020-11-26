149cfa1198ae719e19e2f78a4d7a2c53e574e834
Marconi <marconieng@gmail.com>
2020-11-26 11:37:17 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Puxar as informações salvas ao carregar: tarefas listadas.
+const tarefasListadas = document.getElementById('lista-tarefas');
+tarefasListadas.innerHTML = localStorage.getItem('lista-de-tarefas');
+
+//Adiciona tarefas.
 document.getElementById('criar-tarefa').addEventListener('click', (event) => {
   const entrada = document.getElementById('texto-tarefa').value;
   const listaTarefas = document.getElementById('lista-tarefas');
@@ -8,3 +13,12 @@ document.getElementById('criar-tarefa').addEventListener('click', (event) => {
   document.getElementById('texto-tarefa').value = '';
 });
 
+//Pinta as tarefas.
+const pintaTarefas = document.getElementById('lista-tarefas');
+pintaTarefas[0].addEventListener('click', (event) => {
+  const seleciona = document.getElementsByClassName('selected');
+  if (seleciona[0] !== undefined) {
+    seleciona[0].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
