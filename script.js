41bbe951bb621f17e2ebda79e9fce4f9d410ac59
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2020-11-28 15:06:15 -0300

    Select items

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function createTask() {
     let newItem = document.createElement('li');
     let input = document.getElementById('texto-tarefa');
     newItem.innerText = input.value;
+    newItem.className = 'task-entry';
+    newItem.onclick = () => {
+        Array.from(document.getElementsByClassName('task-entry')).forEach((value) => {
+            value.style.backgroundColor = 'white';
+        });
+        newItem.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
     input.value = '';
     taskList.appendChild(newItem);
 }
