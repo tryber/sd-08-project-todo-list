b509f7a9e44ba1143d60e338ec38c6e8cba6fd1c
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2020-11-28 18:28:39 -0300

    Save the Tasks

--- script.js
+++ script.js
@@ -4,23 +4,27 @@ function createTask() {
     let input = document.getElementById('texto-tarefa');
     newItem.innerText = input.value;
     newItem.className = 'task-entry';
-    newItem.onclick = () => {
-        Array.from(document.getElementsByClassName('task-entry')).forEach((value) => {
-            value.style.backgroundColor = 'white';
-        });
-        newItem.style.backgroundColor = 'rgb(128, 128, 128)';
-    }
-    newItem.ondblclick = () => {
-        if (newItem.classList.contains('completed')) {
-            newItem.classList.remove('completed');
-        } else {
-            newItem.classList.add('completed');
-        }
-    }
+    events(newItem);
     input.value = '';
     taskList.appendChild(newItem);
 }
 
+
+function events(item) {
+    item.onclick = () => {
+        Array.from(document.getElementsByClassName('task-entry')).forEach((value) => {
+            value.style.backgroundColor = 'white';
+        });
+        item.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+    item.ondblclick = () => {
+        if (item.classList.contains('completed')) {
+            item.classList.remove('completed');
+        } else {
+            item.classList.add('completed');
+        }
+    }
+}
 function eraseEverything() {
     Array.from(document.getElementsByClassName('task-entry')).forEach((value) => {
         value.remove();
@@ -31,4 +35,31 @@ function removeCompleted() {
     Array.from(document.getElementsByClassName('task-entry')).forEach((value) => {
         if (value.classList.contains('completed')) value.remove();
     });
+}
+
+function saveTasks() {
+    localStorage.clear();
+    Array.from(document.getElementsByClassName('task-entry')).forEach((element, index) => {
+        localStorage.setItem(index.toString(),
+        ((element.classList.contains('completed'))? '£' : '') +
+        element.innerText);
+    });
+}
+
+window.onload = () => {
+    for (let index = 0; true; index++) {
+        let text = localStorage.getItem(index.toString());
+        if (text != null) {
+            let newElement = document.createElement('li');
+            if (text.startsWith('£')) {
+                newElement.className = 'task-entry completed';
+                newElement.innerText = text.substr(1);
+            } else {
+                newElement.className = 'task-entry';
+                newElement.innerText = text;
+            }
+            events(newElement);
+            document.getElementById('lista-tarefas').appendChild(newElement);
+        } else break;
+    }
 }
