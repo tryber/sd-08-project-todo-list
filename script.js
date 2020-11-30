146898ef301cb5b5bb419567a0037cb5e770766f
Wendrick <wendrick.b.c1@gmail.com>
2020-11-30 18:15:18 -0300

    Acho que a troca de task Selecionada foi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const inputTask = document.querySelector('#texto-tarefa');
 const createTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
-const taskItem = document.querySelectorAll('.task');
 
+// Submit Task to Task List
 const submitTask = () => {
   if (inputTask.value === '') {
     return alert('Insira um valor válido para sua task!!');
@@ -15,3 +15,22 @@ const submitTask = () => {
 };
 
 createTask.addEventListener('click', submitTask);
+
+// Change task color
+const unselectsTasks = () => {
+  const taskItem = document.querySelectorAll('.task');
+  for (let i = 0; i < taskItem.length; i += 1) {
+    taskItem[i].classList.remove('selected');
+    taskItem[i].style.backgroundColor = 'rgb(98 , 65 , 160)';
+  }
+};
+
+const selectTask = (event) => {
+  if (event.target.className !== 'task selected') {
+    unselectsTasks();
+    event.target.classList.add('selected');
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+};
+
+taskList.addEventListener('click', selectTask);
