75c497b6b30213da79e3d237eea70be364d0e46b
Wendrick <wendrick.b.c1@gmail.com>
2020-11-30 16:36:21 -0300

    Fazendo o botao funfar

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const inputTask = document.querySelector('#texto-tarefa');
 const createTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const taskItem = document.querySelectorAll('.task');
 
 const submitTask = () => {
   if (inputTask.value === '') {
-    return alert('Insira um valor válido para sua task!!')
+    return alert('Insira um valor válido para sua task!!');
   }
   const taskListElement = document.createElement('li');
   taskListElement.className = 'task';
   taskListElement.innerText = inputTask.value;
-  inputTask.value = ' ';
+  inputTask.value = '';
   return taskList.appendChild(taskListElement);
 };
 
