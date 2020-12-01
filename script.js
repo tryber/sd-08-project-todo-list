25209c4ea36698ea2018c8a73cd6191020d7bfce
GabrielLennox <meunomeelennox@gmail.com>
2020-12-01 12:57:23 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const criarTarefa = document.getElementById('criar-tarefa').innerText;
+const criarTarefa = document.getElementById('criar-tarefa');
+criarTarefa.addEventListener('click', function() {
+    const itemTarefa = document.createElement('li');
+    const txtTarefa = document.getElementById('texto-tarefa');
+    let tarefa = document.getElementById('texto-tarefa').value;
+    const lista = document.getElementById('lista-tarefas');
+    lista.appendChild(itemTarefa).innerText = tarefa;
+    txtTarefa.value = '';
+})
