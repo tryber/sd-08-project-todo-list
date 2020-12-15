6f86506d1e445ccb24984c8be0b1021579cc3669
Tiago <tiagaoalb@gmail.com>
2020-12-15 17:57:05 -0300

    updated some files

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-const getButtonCreateTask = document.getElementById('criar-tarefa');
-const getButtonClearAll = document.getElementById('apaga-tudo');
-const getButtonRemoveFinished = document.getElementById('remover-finalizados');
-const getOrderedList = document.getElementById('lista-tarefas');
-const getTaskList = document.getElementsByClassName('task-list');
-const getInput = document.getElementById('texto-tarefa');
-
-function createTaskList() {
-	const createList = document.createElement('li');
-	createList.innerText = getInput.value;
-	createList.className = 'task-list';
-	createList.addEventListener('dblclick', toggleCompleted);
-	createList.addEventListener('click', toggleSelected);
-	return createList;
-}
-
-getButtonCreateTask.addEventListener('click', () => {
-	const li = createTaskList();
-	getOrderedList.appendChild(li);
-	getInput.value = '';
-});
-
 function toggleSelected(event) {
-	for (let index = 0; index < getTaskList.length; index += 1) {
-		getTaskList[index].classList.remove('selected');
-		event.target.classList.add('selected');
-		getTaskList[index] = event.target;
-	}
+  const getTaskList = document.getElementsByClassName('task-list');
+  for (let index = 0; index < getTaskList.length; index += 1) {
+    getTaskList[index].classList.remove('selected');
+    event.target.classList.add('selected');
+    getTaskList[index] = event.target;
+  }
 }
 
 function toggleCompleted(event) {
-	event.target.classList.toggle('completed');
+  event.target.classList.toggle('completed');
 }
 
-getButtonClearAll.addEventListener('click', () => {
-	getOrderedList.innerHTML = '';
+function createTaskList() {
+  const getInput = document.getElementById('texto-tarefa');
+  const createList = document.createElement('li');
+  createList.innerText = getInput.value;
+  createList.className = 'task-list';
+  createList.addEventListener('dblclick', toggleCompleted);
+  createList.addEventListener('click', toggleSelected);
+  return createList;
+}
+
+const getButtonCreateTask = document.getElementById('criar-tarefa');
+const getOrderedList = document.getElementById('lista-tarefas');
+getButtonCreateTask.addEventListener('click', () => {
+  const getInput = document.getElementById('texto-tarefa');
+  const li = createTaskList();
+  getOrderedList.appendChild(li);
+  getInput.value = '';
 });
 
-getButtonRemoveFinished.addEventListener('click', () => {
-	let completedTask = document.getElementsByClassName('completed');
-	for (let index = completedTask.length - 1; index >= 0; index -= 1) {
-		completedTask[index].remove();
-	}
+const getButtonClearAll = document.getElementById('apaga-tudo');
+getButtonClearAll.addEventListener('click', () => {
+  getOrderedList.innerHTML = '';
 });
+
+const getButtonRemoveFinished = document.getElementById('remover-finalizados');
+getButtonRemoveFinished.addEventListener('click', () => {
+  const completedTask = document.getElementsByClassName('completed');
+  for (let index = completedTask.length - 1; index >= 0; index -= 1) {
+    completedTask[index].remove();
+  }
+});
+
