ccc304457581f1c3df1c40f3878b09bead88d424
emassaki <massakihirayama@gmail.com>
2020-12-19 09:35:53 -0300

    Refatorando o codigo

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const taskList = document.getElementById('lista-tarefas');
 taskList.innerHTML = localStorage.getItem('List');
 
-// Eventos click
 document.addEventListener('click', (event) => {
-  // Adicionar tarefa à lista
   if (event.target.id === 'criar-tarefa') {
     const input = document.getElementById('texto-tarefa');
     const task = document.createElement('li');
@@ -12,7 +10,7 @@ document.addEventListener('click', (event) => {
     input.value = '';
     taskList.appendChild(task);
   }
-  // Pinta a tarefa clicada
+
   if (event.target.classList.contains('task')) {
     const tasks = document.querySelector('.selected');
     if (tasks === null) {
@@ -23,31 +21,28 @@ document.addEventListener('click', (event) => {
       event.target.classList.add('selected');
     }
   }
-  // Apaga todas as tarefas
+
   if (event.target.id === 'apaga-tudo') {
     taskList.innerHTML = '';
   }
-  // Apaga tarefas completas
+
   if (event.target.id === 'remover-finalizados') {
-    const completedTasks = document.getElementsByClassName('completed');
-    const length = completedTasks.length;
-    for (let index = 0; index < length; index += 1) {
-      completedTasks[0].parentElement.removeChild(completedTasks[0]);
-    }
+    const completedTasks = document.querySelectorAll('.completed');
+    completedTasks.forEach(item => item.remove());
   }
-  // Salva a lista de tarefas
+
   if (event.target.id === 'salvar-tarefas') {
     localStorage.setItem('List', taskList.innerHTML);
   }
-  // Mover para cima o item selecionado
+
   if (event.target.id === 'mover-cima') {
     const childNode = document.querySelectorAll('.task');
     let selected;
-    for (let index = 0; index < childNode.length; index += 1) {
-      if (childNode[index].classList.contains('selected')) {
+    childNode.forEach((item, index) => {
+      if (item.classList.contains('selected')) {
         selected = index;
       }
-    }
+    });
     if (selected > 0) {
       childNode[selected].parentElement.insertBefore(
         childNode[selected],
@@ -56,15 +51,15 @@ document.addEventListener('click', (event) => {
       // Solução encontrada no site 'https://stackoverflow.com/questions/9732624/how-to-swap-dom-child-nodes-in-javascript'
     }
   }
-  // Mover para baixo o item selecionado
+
   if (event.target.id === 'mover-baixo') {
     const childNode = document.querySelectorAll('.task');
     let selected;
-    for (let index = 0; index < childNode.length; index += 1) {
-      if (childNode[index].classList.contains('selected')) {
+    childNode.forEach((item, index) => {
+      if (item.classList.contains('selected')) {
         selected = index + 1;
       }
-    }
+    })
     if (selected < childNode.length) {
       childNode[selected].parentElement.insertBefore(
         childNode[selected],
@@ -72,13 +67,12 @@ document.addEventListener('click', (event) => {
       );
     }
   }
+
   if (event.target.id === 'remover-selecionado') {
     document.querySelector('.selected').remove();
   }
 });
 
-// Eventos dblclick
-// Torna a tarefa completa
 taskList.addEventListener('dblclick', (event) => {
   event.target.classList.toggle('completed');
 });
