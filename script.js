3641a3662914377fcc3ef26c0acc83cd9b9eed6e
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-17 02:20:43 -0300

    r07.01

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function criaNovaTarefa() {
     const listaDeTarefas = document.getElementById("lista-tarefas");
     if (entradaDeTarefas.value.length > 0) {
       const novaTarefa = document.createElement("li");
+      novaTarefa.className = "tarefa";
       novaTarefa.innerText = entradaDeTarefas.value;
       listaDeTarefas.appendChild(novaTarefa);
       entradaDeTarefas.value = "";
@@ -12,3 +13,15 @@ function criaNovaTarefa() {
   });
 }
 criaNovaTarefa();
+
+function selecionaItemDaLista() {
+  const listaDeTarefas = document.getElementById("lista-tarefas");
+  listaDeTarefas.addEventListener("click", function (evento) {
+    const itensDaLista = document.getElementsByClassName("tarefa");
+    for (let i = 0; i < itensDaLista.length; i += 1) {
+      itensDaLista[i].style.backgroundColor = "";
+    }
+    evento.target.style.backgroundColor = "rgb(128, 128, 128)";
+  });
+}
+selecionaItemDaLista();
